65a9a82e099347457bfcec2118708831a14d9645
aystarik <aystarik>
2005-06-29 19:27:29 +0000

    version
    date	2004.07.14.21.44.00;	author rmoore1;	state Exp;

--- source/include/acconfig.h
+++ source/include/acconfig.h
@@ -1,7 +1,7 @@
 /******************************************************************************
  *
  * Name: acconfig.h - Global configuration constants
- *       $Revision: 1.159 $
+ *       $Revision: 1.160 $
  *
  *****************************************************************************/
 
@@ -137,7 +137,7 @@
 
 /* Version string */
 
-#define ACPI_CA_VERSION                 0x20040615
+#define ACPI_CA_VERSION                 0x20040714
 
 /*
  * OS name, used for the _OS object.  The _OS object is essentially obsolete,
