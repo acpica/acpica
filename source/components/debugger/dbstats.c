160f25dcab9475affdf3466816d2e68ac5e15a31
aystarik <aystarik>
2005-06-29 16:26:29 +0000

    Fourth pass to include lint changes/comments/cleanup (64-bit)
    date	2002.04.11.22.57.00;	author rmoore1;	state Exp;

--- source/components/debugger/dbstats.c
+++ source/components/debugger/dbstats.c
@@ -1,7 +1,7 @@
 /*******************************************************************************
  *
  * Module Name: dbstats - Generation and display of ACPI table statistics
- *              $Revision: 1.53 $
+ *              $Revision: 1.59 $
  *
  ******************************************************************************/
 
@@ -117,19 +117,17 @@
 
 #include <acpi.h>
 #include <acdebug.h>
-#include <amlcode.h>
-#include <acparser.h>
 #include <acnamesp.h>
 
 #ifdef ENABLE_DEBUGGER
 
 #define _COMPONENT          ACPI_DEBUGGER
-        MODULE_NAME         ("dbstats")
+        ACPI_MODULE_NAME    ("dbstats")
 
 /*
  * Statistics subcommands
  */
-ARGUMENT_INFO               AcpiDbStatTypes [] =
+static ARGUMENT_INFO        AcpiDbStatTypes [] =
 {
     {"ALLOCATIONS"},
     {"OBJECTS"},
@@ -141,13 +139,13 @@ ARGUMENT_INFO               AcpiDbStatTypes [] =
     {NULL}           /* Must be null terminated */
 };
 
-#define CMD_ALLOCATIONS     0
-#define CMD_OBJECTS         1
-#define CMD_MEMORY          2
-#define CMD_MISC            3
-#define CMD_TABLES          4
-#define CMD_SIZES           5
-#define CMD_STACK           6
+#define CMD_STAT_ALLOCATIONS     0
+#define CMD_STAT_OBJECTS         1
+#define CMD_STAT_MEMORY          2
+#define CMD_STAT_MISC            3
+#define CMD_STAT_TABLES          4
+#define CMD_STAT_SIZES           5
+#define CMD_STAT_STACK           6
 
 
 /*******************************************************************************
@@ -234,6 +232,9 @@ AcpiDbEnumerateObject (
         AcpiDbEnumerateObject (ObjDesc->ThermalZone.SysHandler);
         AcpiDbEnumerateObject (ObjDesc->ThermalZone.DrvHandler);
         AcpiDbEnumerateObject (ObjDesc->ThermalZone.AddrHandler);
+        break;
+
+    default:
         break;
     }
 }
@@ -324,7 +325,7 @@ AcpiDbClassifyOneObject (
  *
  ******************************************************************************/
 
-ACPI_STATUS
+void
 AcpiDbCountNamespaceObjects (
     void)
 {
@@ -341,10 +342,8 @@ AcpiDbCountNamespaceObjects (
         AcpiGbl_NodeTypeCount [i] = 0;
     }
 
-    AcpiNsWalkNamespace (ACPI_TYPE_ANY, ACPI_ROOT_OBJECT, ACPI_UINT32_MAX,
+    (void) AcpiNsWalkNamespace (ACPI_TYPE_ANY, ACPI_ROOT_OBJECT, ACPI_UINT32_MAX,
                         FALSE, AcpiDbClassifyOneObject, NULL, NULL);
-
-    return (AE_OK);
 }
 
 #endif
@@ -385,7 +384,7 @@ AcpiDbDisplayStatistics (
         return (AE_OK);
     }
 
-    STRUPR (TypeArg);
+    ACPI_STRUPR (TypeArg);
     Type = AcpiDbMatchArgument (TypeArg, AcpiDbStatTypes);
     if (Type == (UINT32) -1)
     {
@@ -397,14 +396,14 @@ AcpiDbDisplayStatistics (
     switch (Type)
     {
 #ifndef PARSER_ONLY
-    case CMD_ALLOCATIONS:
+    case CMD_STAT_ALLOCATIONS:
 #ifdef ACPI_DBG_TRACK_ALLOCATIONS
         AcpiUtDumpAllocationInfo ();
 #endif
         break;
 #endif
 
-    case CMD_TABLES:
+    case CMD_STAT_TABLES:
 
         AcpiOsPrintf ("ACPI Table Information:\n\n");
         if (AcpiGbl_DSDT)
@@ -413,7 +412,7 @@ AcpiDbDisplayStatistics (
         }
         break;
 
-    case CMD_OBJECTS:
+    case CMD_STAT_OBJECTS:
 
 #ifndef PARSER_ONLY
 
@@ -437,7 +436,7 @@ AcpiDbDisplayStatistics (
 #endif
         break;
 
-    case CMD_MEMORY:
+    case CMD_STAT_MEMORY:
 
 #ifdef ACPI_DBG_TRACK_ALLOCATIONS
         AcpiOsPrintf ("\n----Object and Cache Statistics---------------------------------------------\n");
@@ -467,11 +466,11 @@ AcpiDbDisplayStatistics (
 
             if (AcpiGbl_MemoryLists[i].ObjectSize)
             {
-                Size = ROUND_UP_TO_1K (Outstanding * AcpiGbl_MemoryLists[i].ObjectSize);
+                Size = ACPI_ROUND_UP_TO_1K (Outstanding * AcpiGbl_MemoryLists[i].ObjectSize);
             }
             else
             {
-                Size = ROUND_UP_TO_1K (AcpiGbl_MemoryLists[i].CurrentTotalSize);
+                Size = ACPI_ROUND_UP_TO_1K (AcpiGbl_MemoryLists[i].CurrentTotalSize);
             }
 
             AcpiOsPrintf ("    Mem:   [Alloc Free Outstanding Size]  % 7d % 7d % 7d % 7d Kb\n",
@@ -483,7 +482,7 @@ AcpiDbDisplayStatistics (
 
         break;
 
-    case CMD_MISC:
+    case CMD_STAT_MISC:
 
         AcpiOsPrintf ("\nMiscellaneous Statistics:\n\n");
         AcpiOsPrintf ("Calls to AcpiPsFind:..  ........% 7ld\n", AcpiGbl_PsFindCount);
@@ -499,7 +498,7 @@ AcpiDbDisplayStatistics (
         break;
 
 
-    case CMD_SIZES:
+    case CMD_STAT_SIZES:
 
         AcpiOsPrintf ("\nInternal object sizes:\n\n");
 
@@ -528,23 +527,29 @@ AcpiDbDisplayStatistics (
 
         AcpiOsPrintf ("\n");
 
-        AcpiOsPrintf ("ParseObject      %3d\n", sizeof (ACPI_PARSE_OBJECT));
-        AcpiOsPrintf ("Parse2Object     %3d\n", sizeof (ACPI_PARSE2_OBJECT));
+        AcpiOsPrintf ("ParseObject      %3d\n", sizeof (ACPI_PARSE_OBJ_COMMON));
+        AcpiOsPrintf ("ParseObjectNamed %3d\n", sizeof (ACPI_PARSE_OBJ_NAMED));
+        AcpiOsPrintf ("ParseObjectAsl   %3d\n", sizeof (ACPI_PARSE_OBJ_ASL));
         AcpiOsPrintf ("OperandObject    %3d\n", sizeof (ACPI_OPERAND_OBJECT));
         AcpiOsPrintf ("NamespaceNode    %3d\n", sizeof (ACPI_NAMESPACE_NODE));
 
         break;
 
 
-    case CMD_STACK:
+    case CMD_STAT_STACK:
+#if defined(ACPI_DEBUG)
 
-        Size = AcpiGbl_EntryStackPointer - AcpiGbl_LowestStackPointer;
+        Size = (UINT32) (AcpiGbl_EntryStackPointer - AcpiGbl_LowestStackPointer);
 
         AcpiOsPrintf ("\nSubsystem Stack Usage:\n\n");
         AcpiOsPrintf ("Entry Stack Pointer          %X\n", AcpiGbl_EntryStackPointer);
         AcpiOsPrintf ("Lowest Stack Pointer         %X\n", AcpiGbl_LowestStackPointer);
         AcpiOsPrintf ("Stack Use                    %X (%d)\n", Size, Size);
         AcpiOsPrintf ("Deepest Procedure Nesting    %d\n", AcpiGbl_DeepestNesting);
+#endif
+        break;
+
+    default:
         break;
     }
 
