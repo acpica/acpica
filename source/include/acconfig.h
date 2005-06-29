2e30ca9f6ce3f77bc18a85ff284b1cdce7d44ed9
aystarik <aystarik>
2005-06-29 19:26:37 +0000

    version
    date	2002.08.29.17.53.00;	author rmoore1;	state Exp;

--- source/include/acconfig.h
+++ source/include/acconfig.h
@@ -1,7 +1,7 @@
 /******************************************************************************
  *
  * Name: acconfig.h - Global configuration constants
- *       $Revision: 1.108 $
+ *       $Revision: 1.109 $
  *
  *****************************************************************************/
 
@@ -145,7 +145,7 @@
 
 /* Version string */
 
-#define ACPI_CA_VERSION                 0x20020815
+#define ACPI_CA_VERSION                 0x20020829
 
 /* Version of ACPI supported */
 
