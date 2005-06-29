a30a40780a0729161156a133521eff6704f9ca4d
aystarik <aystarik>
2005-06-29 16:55:11 +0000

    date	2000.10.04.23.58.00;	author agrover;	state Exp;

--- source/components/hardware/hwregs.c
+++ source/components/hardware/hwregs.c
@@ -3,7 +3,7 @@
  *
  * Module Name: hwregs - Read/write access functions for the various ACPI
  *                       control and status registers.
- *              $Revision: 1.68 $
+ *              $Revision: 1.69 $
  *
  ******************************************************************************/
 
