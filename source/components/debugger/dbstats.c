0072673879065e4b527a3011e57fda65ce286f07
aystarik <aystarik>
2005-06-29 16:26:23 +0000

    Moved variables that are unused if DBG_TRACK_ALLOCATIONS is set
    date	2002.02.06.23.40.00;	author rmoore1;	state Exp;

--- source/components/debugger/dbstats.c
+++ source/components/debugger/dbstats.c
@@ -1,7 +1,7 @@
 /*******************************************************************************
  *
  * Module Name: dbstats - Generation and display of ACPI table statistics
- *              $Revision: 1.51 $
+ *              $Revision: 1.53 $
  *
  ******************************************************************************/
 
@@ -9,7 +9,7 @@
  *
  * 1. Copyright Notice
  *
- * Some or all of this work - Copyright (c) 1999, 2000, 2001, Intel Corp.
+ * Some or all of this work - Copyright (c) 1999 - 2002, Intel Corp.
  * All rights reserved.
  *
  * 2. License
@@ -368,8 +368,10 @@ AcpiDbDisplayStatistics (
 {
     UINT32                  i;
     UINT32                  Type;
-    UINT32                  Outstanding;
     UINT32                  Size;
+#ifdef ACPI_DBG_TRACK_ALLOCATIONS
+    UINT32                  Outstanding;
+#endif
 
 
     if (!AcpiGbl_DSDT)
