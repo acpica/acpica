fc59da0f7c36bcade51dfb2e7e47bba066e5c7cb
aystarik <aystarik>
2005-06-29 17:32:10 +0000

    Deployed UtGetDescriptorName for better debug output
    date	2003.10.22.20.51.00;	author rmoore1;	state Exp;

--- source/components/executer/exmisc.c
+++ source/components/executer/exmisc.c
@@ -2,7 +2,7 @@
 /******************************************************************************
  *
  * Module Name: exmisc - ACPI AML (p-code) execution - specific opcodes
- *              $Revision: 1.114 $
+ *              $Revision: 1.116 $
  *
  *****************************************************************************/
 
@@ -10,7 +10,7 @@
  *
  * 1. Copyright Notice
  *
- * Some or all of this work - Copyright (c) 1999 - 2002, Intel Corp.
+ * Some or all of this work - Copyright (c) 1999 - 2003, Intel Corp.
  * All rights reserved.
  *
  * 2. License
@@ -199,8 +199,8 @@ AcpiExGetObjectReference (
 
     default:
 
-        ACPI_DEBUG_PRINT ((ACPI_DB_ERROR, "Invalid descriptor type %X in %p\n",
-            ACPI_GET_DESCRIPTOR_TYPE (ObjDesc), ObjDesc));
+        ACPI_DEBUG_PRINT ((ACPI_DB_ERROR, "%p has invalid descriptor [%s]\n",
+                ObjDesc, AcpiUtGetDescriptorName (ObjDesc)));
         return_ACPI_STATUS (AE_TYPE);
     }
 
@@ -218,7 +218,7 @@ AcpiExGetObjectReference (
     *ReturnDesc = ReferenceObj;
 
     ACPI_DEBUG_PRINT ((ACPI_DB_EXEC, "Object %p Type [%s], returning Reference %p\n",
-        ObjDesc, AcpiUtGetObjectTypeName (ObjDesc), *ReturnDesc));
+            ObjDesc, AcpiUtGetObjectTypeName (ObjDesc), *ReturnDesc));
 
     return_ACPI_STATUS (AE_OK);
 }
