b534d48b6da698a1c26212e276d8f23d6a71d00b
aystarik <aystarik>
2005-06-29 17:21:33 +0000

    New ACPI_SIZE type for all "size" parameters and pointer diffs
    date	2001.12.10.22.44.00;	author rmoore1;	state Exp;

--- source/components/executer/excreate.c
+++ source/components/executer/excreate.c
@@ -1,7 +1,7 @@
 /******************************************************************************
  *
  * Module Name: excreate - Named object creation
- *              $Revision: 1.82 $
+ *              $Revision: 1.83 $
  *
  *****************************************************************************/
 
@@ -471,7 +471,7 @@ AcpiExCreateTableRegion (
     /* Init the region from the operands */
 
     ObjDesc->Region.SpaceId = REGION_DATA_TABLE;
-    ObjDesc->Region.Address = (NATIVE_UINT) Table;
+    ObjDesc->Region.Address = (ACPI_PHYSICAL_ADDRESS) Table;
     ObjDesc->Region.Length  = Table->Length;
     ObjDesc->Region.Node    = Node;
     ObjDesc->Region.Flags   = AOPOBJ_DATA_VALID;
