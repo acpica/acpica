ebfa15a14b0038ff34f1d4daed1102a7e00e4904
aystarik <aystarik>
2005-06-29 18:24:04 +0000

    Add support for device _CIDs (Compatibility IDs) with multiple strings
    date	2003.05.21.20.59.00;	author rmoore1;	state Exp;

--- source/components/namespace/nsdumpdv.c
+++ source/components/namespace/nsdumpdv.c
@@ -1,7 +1,7 @@
 /******************************************************************************
  *
  * Module Name: nsdump - table dumping routines for debug
- *              $Revision: 1.1 $
+ *              $Revision: 1.6 $
  *
  *****************************************************************************/
 
@@ -9,7 +9,7 @@
  *
  * 1. Copyright Notice
  *
- * Some or all of this work - Copyright (c) 1999 - 2002, Intel Corp.
+ * Some or all of this work - Copyright (c) 1999 - 2003, Intel Corp.
  * All rights reserved.
  *
  * 2. License
@@ -119,14 +119,13 @@
 
 #include "acpi.h"
 #include "acnamesp.h"
-#include "acparser.h"
 
 
 #define _COMPONENT          ACPI_NAMESPACE
         ACPI_MODULE_NAME    ("nsdumpdv")
 
 
-#if defined(ACPI_DEBUG) || defined(ENABLE_DEBUGGER)
+#if defined(ACPI_DEBUG_OUTPUT) || defined(ACPI_DEBUGGER)
 
 /*******************************************************************************
  *
@@ -148,7 +147,8 @@ AcpiNsDumpOneDevice (
     void                    *Context,
     void                    **ReturnValue)
 {
-    ACPI_DEVICE_INFO        Info;
+    ACPI_BUFFER             Buffer;
+    ACPI_DEVICE_INFO        *Info;
     ACPI_STATUS             Status;
     UINT32                  i;
 
@@ -158,18 +158,20 @@ AcpiNsDumpOneDevice (
 
     Status = AcpiNsDumpOneObject (ObjHandle, Level, Context, ReturnValue);
 
-    Status = AcpiGetObjectInfo (ObjHandle, &Info);
+    Status = AcpiGetObjectInfo (ObjHandle, &Buffer);
     if (ACPI_SUCCESS (Status))
     {
+        Info = Buffer.Pointer;
         for (i = 0; i < Level; i++)
         {
             ACPI_DEBUG_PRINT_RAW ((ACPI_DB_TABLES, " "));
         }
 
         ACPI_DEBUG_PRINT_RAW ((ACPI_DB_TABLES, "    HID: %s, ADR: %8.8X%8.8X, Status: %X\n",
-                        Info.HardwareId,
-                        ACPI_HIDWORD (Info.Address), ACPI_LODWORD (Info.Address),
-                        Info.CurrentStatus));
+                        &Info->HardwareId,
+                        ACPI_HIDWORD (Info->Address), ACPI_LODWORD (Info->Address),
+                        Info->CurrentStatus));
+        ACPI_MEM_FREE (Info);
     }
 
     return (Status);
@@ -211,7 +213,7 @@ AcpiNsDumpRootDevices (void)
 
     ACPI_DEBUG_PRINT ((ACPI_DB_TABLES, "Display of all devices in the namespace:\n"));
 
-    Status = AcpiNsWalkNamespace (ACPI_TYPE_DEVICE, SysBusHandle, 
+    Status = AcpiNsWalkNamespace (ACPI_TYPE_DEVICE, SysBusHandle,
                 ACPI_UINT32_MAX, ACPI_NS_WALK_NO_UNLOCK,
                 AcpiNsDumpOneDevice, NULL, NULL);
 }
