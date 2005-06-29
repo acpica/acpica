367d5c30c41b00c02b42283bde205105f7e6cfdd
aystarik <aystarik>
2005-06-29 19:19:56 +0000

    All table descriptors are now dynamic.  Fixes multiple SSDT deletion problem. Updated some global names and defines
    date	2003.05.08.23.28.00;	author rmoore1;	state Exp;

--- source/components/utilities/utmisc.c
+++ source/components/utilities/utmisc.c
@@ -1,7 +1,7 @@
 /*******************************************************************************
  *
  * Module Name: utmisc - common utility procedures
- *              $Revision: 1.95 $
+ *              $Revision: 1.96 $
  *
  ******************************************************************************/
 
@@ -656,7 +656,7 @@ AcpiUtMutexInitialize (
     /*
      * Create each of the predefined mutex objects
      */
-    for (i = 0; i < NUM_MTX; i++)
+    for (i = 0; i < NUM_MUTEX; i++)
     {
         Status = AcpiUtCreateMutex (i);
         if (ACPI_FAILURE (Status))
@@ -697,7 +697,7 @@ AcpiUtMutexTerminate (
     /*
      * Delete each predefined mutex object
      */
-    for (i = 0; i < NUM_MTX; i++)
+    for (i = 0; i < NUM_MUTEX; i++)
     {
         (void) AcpiUtDeleteMutex (i);
     }
@@ -729,17 +729,17 @@ AcpiUtCreateMutex (
     ACPI_FUNCTION_TRACE_U32 ("UtCreateMutex", MutexId);
 
 
-    if (MutexId > MAX_MTX)
+    if (MutexId > MAX_MUTEX)
     {
         return_ACPI_STATUS (AE_BAD_PARAMETER);
     }
 
-    if (!AcpiGbl_AcpiMutexInfo[MutexId].Mutex)
+    if (!AcpiGbl_MutexInfo[MutexId].Mutex)
     {
         Status = AcpiOsCreateSemaphore (1, 1,
-                        &AcpiGbl_AcpiMutexInfo[MutexId].Mutex);
-        AcpiGbl_AcpiMutexInfo[MutexId].OwnerId = ACPI_MUTEX_NOT_ACQUIRED;
-        AcpiGbl_AcpiMutexInfo[MutexId].UseCount = 0;
+                        &AcpiGbl_MutexInfo[MutexId].Mutex);
+        AcpiGbl_MutexInfo[MutexId].OwnerId = ACPI_MUTEX_NOT_ACQUIRED;
+        AcpiGbl_MutexInfo[MutexId].UseCount = 0;
     }
 
     return_ACPI_STATUS (Status);
@@ -768,15 +768,15 @@ AcpiUtDeleteMutex (
     ACPI_FUNCTION_TRACE_U32 ("UtDeleteMutex", MutexId);
 
 
-    if (MutexId > MAX_MTX)
+    if (MutexId > MAX_MUTEX)
     {
         return_ACPI_STATUS (AE_BAD_PARAMETER);
     }
 
-    Status = AcpiOsDeleteSemaphore (AcpiGbl_AcpiMutexInfo[MutexId].Mutex);
+    Status = AcpiOsDeleteSemaphore (AcpiGbl_MutexInfo[MutexId].Mutex);
 
-    AcpiGbl_AcpiMutexInfo[MutexId].Mutex = NULL;
-    AcpiGbl_AcpiMutexInfo[MutexId].OwnerId = ACPI_MUTEX_NOT_ACQUIRED;
+    AcpiGbl_MutexInfo[MutexId].Mutex = NULL;
+    AcpiGbl_MutexInfo[MutexId].OwnerId = ACPI_MUTEX_NOT_ACQUIRED;
 
     return_ACPI_STATUS (Status);
 }
@@ -806,7 +806,7 @@ AcpiUtAcquireMutex (
     ACPI_FUNCTION_NAME ("UtAcquireMutex");
 
 
-    if (MutexId > MAX_MTX)
+    if (MutexId > MAX_MUTEX)
     {
         return (AE_BAD_PARAMETER);
     }
@@ -819,9 +819,9 @@ AcpiUtAcquireMutex (
      * the mutex ordering rule.  This indicates a coding error somewhere in
      * the ACPI subsystem code.
      */
-    for (i = MutexId; i < MAX_MTX; i++)
+    for (i = MutexId; i < MAX_MUTEX; i++)
     {
-        if (AcpiGbl_AcpiMutexInfo[i].OwnerId == ThisThreadId)
+        if (AcpiGbl_MutexInfo[i].OwnerId == ThisThreadId)
         {
             if (i == MutexId)
             {
@@ -845,15 +845,15 @@ AcpiUtAcquireMutex (
                 "Thread %X attempting to acquire Mutex [%s]\n",
                 ThisThreadId, AcpiUtGetMutexName (MutexId)));
 
-    Status = AcpiOsWaitSemaphore (AcpiGbl_AcpiMutexInfo[MutexId].Mutex,
+    Status = AcpiOsWaitSemaphore (AcpiGbl_MutexInfo[MutexId].Mutex,
                                     1, ACPI_WAIT_FOREVER);
     if (ACPI_SUCCESS (Status))
     {
         ACPI_DEBUG_PRINT ((ACPI_DB_MUTEX, "Thread %X acquired Mutex [%s]\n",
                     ThisThreadId, AcpiUtGetMutexName (MutexId)));
 
-        AcpiGbl_AcpiMutexInfo[MutexId].UseCount++;
-        AcpiGbl_AcpiMutexInfo[MutexId].OwnerId = ThisThreadId;
+        AcpiGbl_MutexInfo[MutexId].UseCount++;
+        AcpiGbl_MutexInfo[MutexId].OwnerId = ThisThreadId;
     }
     else
     {
@@ -895,7 +895,7 @@ AcpiUtReleaseMutex (
         "Thread %X releasing Mutex [%s]\n", ThisThreadId,
         AcpiUtGetMutexName (MutexId)));
 
-    if (MutexId > MAX_MTX)
+    if (MutexId > MAX_MUTEX)
     {
         return (AE_BAD_PARAMETER);
     }
@@ -903,7 +903,7 @@ AcpiUtReleaseMutex (
     /*
      * Mutex must be acquired in order to release it!
      */
-    if (AcpiGbl_AcpiMutexInfo[MutexId].OwnerId == ACPI_MUTEX_NOT_ACQUIRED)
+    if (AcpiGbl_MutexInfo[MutexId].OwnerId == ACPI_MUTEX_NOT_ACQUIRED)
     {
         ACPI_DEBUG_PRINT ((ACPI_DB_ERROR,
                 "Mutex [%s] is not acquired, cannot release\n",
@@ -918,9 +918,9 @@ AcpiUtReleaseMutex (
      * ordering rule.  This indicates a coding error somewhere in
      * the ACPI subsystem code.
      */
-    for (i = MutexId; i < MAX_MTX; i++)
+    for (i = MutexId; i < MAX_MUTEX; i++)
     {
-        if (AcpiGbl_AcpiMutexInfo[i].OwnerId == ThisThreadId)
+        if (AcpiGbl_MutexInfo[i].OwnerId == ThisThreadId)
         {
             if (i == MutexId)
             {
@@ -937,9 +937,9 @@ AcpiUtReleaseMutex (
 
     /* Mark unlocked FIRST */
 
-    AcpiGbl_AcpiMutexInfo[MutexId].OwnerId = ACPI_MUTEX_NOT_ACQUIRED;
+    AcpiGbl_MutexInfo[MutexId].OwnerId = ACPI_MUTEX_NOT_ACQUIRED;
 
-    Status = AcpiOsSignalSemaphore (AcpiGbl_AcpiMutexInfo[MutexId].Mutex, 1);
+    Status = AcpiOsSignalSemaphore (AcpiGbl_MutexInfo[MutexId].Mutex, 1);
 
     if (ACPI_FAILURE (Status))
     {
