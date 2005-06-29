d90cd3f6ac9465beebe662b94b54ae013dc8dc4e
aystarik <aystarik>
2005-06-29 19:06:43 +0000

    Moved common code from other directories to here
    date	99.04.21.22.18.00;	author rmoore1;	state Exp;

--- source/components/utilities/utdebug.c
+++ source/components/utilities/utdebug.c
@@ -1,7 +1,7 @@
 
 /******************************************************************************
  * 
- * Module Name: excommon - Debug and error reporting routines
+ * Module Name: cmdebug - Debug print routines
  *
  *****************************************************************************/
 
@@ -95,15 +95,12 @@
  *****************************************************************************/
 
 
-#define __EXCOMMON_C__
+#define __CMDEBUG_C__
 
 #include <acpi.h>
-#include <acpiobj.h>
-#include <interpreter.h>
-#include <stdarg.h>
 
 
-#define _THIS_MODULE        "excommon.c"
+#define _THIS_MODULE        "cmdebug.c"
 #define _COMPONENT          MISCELLANEOUS
 
 
@@ -113,6 +110,7 @@
  * FUNCTION:    Get/Set debug level
  *
  * DESCRIPTION: Get or set value of the debug flag
+ *              TBD: Overkill?? remove??
  *
  ****************************************************************************/
 
@@ -217,279 +215,6 @@ DebugPrintRaw (char *Format, ...)
 }
 
 
-/*****************************************************************************
- * 
- * FUNCTION:    _ReportError
- *
- * DESCRIPTION: Print error message from KD table
- *
- ****************************************************************************/
-
-void
-_ReportError (char *ModuleName, INT32 LineNumber, INT32 ComponentId, ST_KEY_DESC_TABLE *KdtEntry)
-{
-
-    DebugPrint (ModuleName, LineNumber, ComponentId, ACPI_ERROR, 
-                "*** Error [%s]: %s\n", KdtEntry->Key, KdtEntry->Description);
-
-    _Kinc_error (KdtEntry->Key, 
-                    PACRLF, LineNumber, ModuleName, 0, 0);
-}
-
-
-/*****************************************************************************
- * 
- * FUNCTION:    _ReportWarning
- *
- * DESCRIPTION: Print warning message from KD table
- *
- ****************************************************************************/
-
-void
-_ReportWarning (char *ModuleName, INT32 LineNumber, INT32 ComponentId, ST_KEY_DESC_TABLE *KdtEntry)
-{
-
-    DebugPrint (ModuleName, LineNumber, ComponentId, ACPI_WARN, 
-                "*** Warning [%s]: %s\n", KdtEntry->Key, KdtEntry->Description);
-
-    _Kinc_warning (KdtEntry->Key, 
-                    PACRLF, LineNumber, ModuleName, 0, 0);
-}
-
-
-/*****************************************************************************
- * 
- * FUNCTION:    _ReportSuccess
- *
- * DESCRIPTION: Print warning message from KD table
- *
- ****************************************************************************/
-
-void
-_ReportSuccess (char *ModuleName, INT32 LineNumber, INT32 ComponentId, ST_KEY_DESC_TABLE *KdtEntry)
-{
-
-    DebugPrint (ModuleName, LineNumber, ComponentId, ACPI_SUCCESS, 
-                "*** Success [%s] %s\n", KdtEntry->Key, KdtEntry->Description);
-}
-
-
-/*****************************************************************************
- * 
- * FUNCTION:    _ReportInfo
- *
- * DESCRIPTION: Print information message from KD table
- *
- ****************************************************************************/
-
-void
-_ReportInfo (char *ModuleName, INT32 LineNumber, INT32 ComponentId, ST_KEY_DESC_TABLE *KdtEntry)
-{
-
-    DebugPrint (ModuleName, LineNumber, ComponentId, ACPI_INFO, 
-                "*** Info [%s], %s\n", KdtEntry->Key, KdtEntry->Description);
-
-    _Kinc_info (KdtEntry->Key, 
-                    PACRLF, LineNumber, ModuleName, 0, 0);
-}
-
-
-/*****************************************************************************
- * 
- * FUNCTION:    _AllocateObjectDesc
- *
- * RETURN:      Pointer to newly allocated object descriptor
- *
- * DESCRIPTION: Allocate a new object descriptor.  Gracefully handle
- *              error conditions.
- *
- ****************************************************************************/
-
-void *
-_AllocateObjectDesc (char *ModuleName, INT32 LineNumber, INT32 ComponentId, ST_KEY_DESC_TABLE *KdtEntry)
-{
-    OBJECT_DESCRIPTOR       *NewDesc;
-
-
-    /* Attempt to allocate new descriptor */
-
-    NewDesc = NEW (OBJECT_DESCRIPTOR);
-    if (!NewDesc)
-    {
-        /* Allocation failed */
-        
-        _REPORT_ERROR (ModuleName, LineNumber, ComponentId, KdtEntry);
-        OutOfMemory = TRUE;
-    }
-
-    return NewDesc;
-}
-
-
-/*****************************************************************************
- * 
- * FUNCTION:    _LocalAllocate 
- *
- * RETURN:      Pointer to newly allocated memory
- *
- * DESCRIPTION: Allocate memory.  Gracefully handle
- *              error conditions.
- *
- ****************************************************************************/
-
-void *
-_LocalAllocate (char *ModuleName, INT32 LineNumber, INT32 ComponentId, INT32 AllocSize)
-{
-    ST_KEY_DESC_TABLE   AKDT = {"0000", '1', "LocalAllocate: Memory allocation failure", 
-                                              "LocalAllocate: Memory allocation failure"};
-    void                *Block;
-
-
-    Block = OsdAllocate ((size_t) AllocSize);
-    if (!Block)
-    {
-        /* Report allocation error */
-
-        _REPORT_ERROR (ModuleName, LineNumber, ComponentId, &AKDT);
-        OutOfMemory = TRUE;
-    }
-
-    return Block;
-}
-
-
-/*****************************************************************************
- * 
- * FUNCTION:    _LocalCallocate 
- *
- * RETURN:      Pointer to newly allocated memory
- *
- * DESCRIPTION: Allocate memory via calloc (initialized to zero).  
- *              Gracefully handle error conditions.
- *
- ****************************************************************************/
-
-void *
-_LocalCallocate (char *ModuleName, INT32 LineNumber, INT32 ComponentId, INT32 AllocSize)
-{
-    ST_KEY_DESC_TABLE   AKDT = {"0000", '1', "LocalCallocate: Memory allocation failure", 
-                                              "LocalCallocate: Memory allocation failure"};
-    void                *Block;
-
-
-    Block = OsdCallocate ((size_t) AllocSize);
-    if (!Block)
-    {
-        /* Report allocation error */
-
-        _REPORT_ERROR (ModuleName, LineNumber, ComponentId, &AKDT);
-        OutOfMemory = TRUE;
-    }
-
-    return Block;
-}
-
-
-/*****************************************************************************
- * 
- * FUNCTION:    Kinc functions
- *
- * RETURN:      none
- *
- * DESCRIPTION: Increment global counters ?? TBD
- *              No error message printed here, done by the REPORT* functions
- *
- ****************************************************************************/
-
-
-void
-_Kinc_error (char *a, INT32 b, INT32 c, char * d, INT32 e, INT32 f)
-{
-
-/*    OsdPrintf (NULL, "%10s(%04d): *** Error %s\n", d, c, a);  */
-
-    return;
-}
-
-
-
-void
-_Kinc_info (char *a, INT32 b, INT32 c, char * d, INT32 e, INT32 f)
-{
-
-/*    OsdPrintf (NULL, "%10s(%04d): *** Info %s\n", d, c, a); */
-
-    return;
-}
-
-void
-_Kinc_warning (char *a, INT32 b, INT32 c, char * d, INT32 e, INT32 f)
-{
-
-/*    OsdPrintf (NULL, "%10s(%04d): *** Warning %s\n", d, c, a); */
-
-    return;
-}
-
-
-/* Obsolete interfaces?? (but the simpler interface is what we want.*/
-
-/*
-void
-Kinc_error (char *a, INT32 b)
-{
-
-    OsdPrintf (NULL, "*** Error %s\n", a);  
-
-    return;
-}
-*/
-/*
-void
-Kinc_info (char *a, INT32 b, INT32 c, char * d, INT32 e, INT32 f)
-{
-
-    OsdPrintf (NULL, "%10s(%04d): *** Info %s\n", d, c, a);
-
-    return;
-}
-*/
-
-/*
-void
-Kinc_warning (char *a, INT32 b)
-{
-
-     OsdPrintf (NULL, "*** Warning %s\n", a); 
-    return;
-}
-*/
-
-/*
-void
-KFatalError (char * a, char * b, ...)
-{
-    /* Optional parameters (...) not implemented
-
-
-    OsdPrintf (NULL, "*** Fatal Error %s: %s\n", a, b);
-    return;
-}
-*/
-
-
-/* Misc stuff */
-
-/* TBD: Need a real checksum routine here !! */
-
-
-UINT8
-checksum (void *pvBuf,UINT32 stBufSize)
-{
-    return 0;
-}
-
-
 
 /* TBD: Potentially obsolete stuff!! */
 
@@ -506,6 +231,7 @@ IncIndent (void)
 {
     return 0;
 }
+
 
 
 
