50a6ca46d6df07646672af4b49ae17d9c218321c
aystarik <aystarik>
2005-06-29 20:24:43 +0000

    pragma
    date	2002.04.17.16.27.00;	author rmoore1;	state Exp;

--- source/include/platform/acintel.h
+++ source/include/platform/acintel.h
@@ -1,7 +1,7 @@
 /******************************************************************************
  *
  * Name: acintel.h - VC specific defines, etc.
- *       $Revision: 1.2 $
+ *       $Revision: 1.7 $
  *
  *****************************************************************************/
 
@@ -9,7 +9,7 @@
  *
  * 1. Copyright Notice
  *
- * Some or all of this work - Copyright (c) 1999, 2000, 2001, Intel Corp.
+ * Some or all of this work - Copyright (c) 1999 - 2002, Intel Corp.
  * All rights reserved.
  *
  * 2. License
@@ -117,7 +117,22 @@
 #ifndef __ACINTEL_H__
 #define __ACINTEL_H__
 
-#define COMPILER_DEPENDENT_UINT64   unsigned __int64
+
+#define COMPILER_DEPENDENT_INT64   __int64
+#define COMPILER_DEPENDENT_UINT64  unsigned __int64
+
+/*
+ * Calling conventions:
+ *
+ * ACPI_SYSTEM_XFACE        - Interfaces to host OS (handlers, threads)
+ * ACPI_EXTERNAL_XFACE      - External ACPI interfaces 
+ * ACPI_INTERNAL_XFACE      - Internal ACPI interfaces
+ * ACPI_INTERNAL_VAR_XFACE  - Internal variable-parameter list interfaces
+ */
+#define ACPI_SYSTEM_XFACE
+#define ACPI_EXTERNAL_XFACE
+#define ACPI_INTERNAL_XFACE
+#define ACPI_INTERNAL_VAR_XFACE
 
 /*
  * Math helper functions
@@ -133,8 +148,8 @@
     n <<= 1; \
 }
 
-
-#pragma warning(disable:810)
+/* remark 981 - operands evaluated in no particular order */
+#pragma warning(disable:981)
 
 /* warn C4100: unreferenced formal parameter */
 #pragma warning(disable:4100)
