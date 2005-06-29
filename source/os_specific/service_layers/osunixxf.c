04948d6936ead7c0d768b9f370f10b571dad16d7
aystarik <aystarik>
2005-06-29 20:33:44 +0000

    added gettimer interface
    date	2001.06.15.20.12.00;	author agrover;	state Exp;

--- source/os_specific/service_layers/osunixxf.c
+++ source/os_specific/service_layers/osunixxf.c
@@ -122,6 +122,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <unistd.h>
+#include <sys/time.h>
 
 #undef LOWORD
 #undef HIWORD
@@ -704,8 +706,30 @@ AcpiOsSleep (
     UINT32                  milliseconds)
 {
 
-//    Sleep ((seconds * 1000) + milliseconds);
+    sleep ((seconds * 1000) + milliseconds);
     return;
+}
+
+/******************************************************************************
+ *
+ * FUNCTION:    AcpiOsGetTimer
+ *
+ * PARAMETERS:  None
+ *
+ * RETURN:      Current time in milliseconds
+ *
+ * DESCRIPTION: Get the current system time (in milliseconds).
+ *
+ *****************************************************************************/
+
+UINT32
+AcpiOsGetTimer (void)
+{
+    struct timeval	time;
+
+    gettimeofday(&time, NULL);
+
+    return ((time.tv_sec/1000) + (time.tv_usec*1000));
 }
 
 
