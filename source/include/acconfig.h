06fd78183915de007d8b136f6426417e1d8bb9fd
aystarik <aystarik>
2005-06-29 19:25:57 +0000

    date	2001.09.20.16.53.00;	author agrover;	state Exp;

--- source/include/acconfig.h
+++ source/include/acconfig.h
@@ -1,7 +1,7 @@
 /******************************************************************************
  *
  * Name: acconfig.h - Global configuration constants
- *       $Revision: 1.69 $
+ *       $Revision: 1.70 $
  *
  *****************************************************************************/
 
@@ -144,7 +144,7 @@
 
 /* Version string */
 
-#define ACPI_CA_VERSION             0x20010831
+#define ACPI_CA_VERSION             0x20010920
 
 
 /* Maximum objects in the various object caches */
