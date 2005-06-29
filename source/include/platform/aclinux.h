570215f8d8093998f601426aef055ab457d32d99
aystarik <aystarik>
2005-06-29 20:25:03 +0000

    Added 2000 to the copyright in the legal header
    date	2000.12.08.20.56.00;	author rmoore1;	state Exp;

--- source/include/platform/aclinux.h
+++ source/include/platform/aclinux.h
@@ -1,7 +1,7 @@
 /******************************************************************************
  *
  * Name: aclinux.h - OS specific defines, etc.
- *       $Revision: 1.4 $
+ *       $Revision: 1.5 $
  *
  *****************************************************************************/
 
@@ -9,8 +9,8 @@
  *
  * 1. Copyright Notice
  *
- * Some or all of this work - Copyright (c) 1999, Intel Corp.  All rights
- * reserved.
+ * Some or all of this work - Copyright (c) 1999, 2000, Intel Corp.
+ * All rights reserved.
  *
  * 2. License
  *
