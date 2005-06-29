da4cf1648b8796b1fd26a4b265f9b14d89b4dd5a
aystarik <aystarik>
2005-06-29 20:09:56 +0000

    Changed common constants to uppercase
    date	99.04.23.15.36.00;	author rmoore1;	state Exp;

--- source/include/actypes.h
+++ source/include/actypes.h
@@ -1,18 +1,99 @@
-/*__________________________________________________________________________
- |
- |
- |           Copyright (C) Intel Corporation 1994-1996
- |
- | All rights reserved.  No part of this program or publication may be
- | reproduced, transmitted, transcribed, stored in a retrieval system, or
- | translated into any language or computer language, in any form or by any
- | means, electronic, mechanical, magnetic, optical, chemical, manual, or
- | otherwise, without the prior written permission of Intel Corporation.
- |__________________________________________________________________________
- |
- | FILENAME: datatypes.h - Common data types for the ACPI subsystem
- |__________________________________________________________________________
- */
+
+/******************************************************************************
+ * 
+ * Name: datatypes.h - Common data types for the entire ACPI subsystem
+ *
+ *****************************************************************************/
+
+/******************************************************************************
+ *
+ * 1. Copyright Notice
+ *
+ * Some or all of this work - Copyright (c) 1999, Intel Corp.  All rights 
+ * reserved.
+ *
+ * 2. License
+ * 
+ * 2.1. Intel grants, free of charge, to any person ("Licensee") obtaining a 
+ * copy of the source code appearing in this file ("Covered Code") a license 
+ * under Intel's copyrights in the base code distributed originally by Intel 
+ * ("Original Intel Code") to copy, make derivatives, distribute, use and 
+ * display any portion of the Covered Code in any form; and
+ *
+ * 2.2. Intel grants Licensee a non-exclusive and non-transferable patent 
+ * license (without the right to sublicense), under only those claims of Intel
+ * patents that are infringed by the Original Intel Code, to make, use, sell, 
+ * offer to sell, and import the Covered Code and derivative works thereof 
+ * solely to the minimum extent necessary to exercise the above copyright 
+ * license, and in no event shall the patent license extend to any additions to
+ * or modifications of the Original Intel Code.  No other license or right is 
+ * granted directly or by implication, estoppel or otherwise;
+ *
+ * the above copyright and patent license is granted only if the following 
+ * conditions are met:
+ *
+ * 3. Conditions 
+ *
+ * 3.1. Redistribution of source code of any substantial portion of the Covered 
+ * Code or modification must include the above Copyright Notice, the above 
+ * License, this list of Conditions, and the following Disclaimer and Export 
+ * Compliance provision.  In addition, Licensee must cause all Covered Code to 
+ * which Licensee contributes to contain a file documenting the changes 
+ * Licensee made to create that Covered Code and the date of any change.  
+ * Licensee must include in that file the documentation of any changes made by
+ * any predecessor Licensee.  Licensee must include a prominent statement that
+ * the modification is derived, directly or indirectly, from Original Intel 
+ * Code.
+ *
+ * 3.2. Redistribution in binary form of any substantial portion of the Covered 
+ * Code or modification must reproduce the above Copyright Notice, and the 
+ * following Disclaimer and Export Compliance provision in the documentation 
+ * and/or other materials provided with the distribution.
+ *
+ * 3.3. Intel retains all right, title, and interest in and to the Original 
+ * Intel Code.
+ *
+ * 3.4. Neither the name Intel nor any other trademark owned or controlled by 
+ * Intel shall be used in advertising or otherwise to promote the sale, use or 
+ * other dealings in products derived from or relating to the Covered Code 
+ * without prior written authorization from Intel.
+ *
+ * 4. Disclaimer and Export Compliance
+ *
+ * 4.1. INTEL MAKES NO WARRANTY OF ANY KIND REGARDING ANY SOFTWARE PROVIDED 
+ * HERE.  ANY SOFTWARE ORIGINATING FROM INTEL OR DERIVED FROM INTEL SOFTWARE 
+ * IS PROVIDED "AS IS," AND INTEL WILL NOT PROVIDE ANY SUPPORT,  ASSISTANCE, 
+ * INSTALLATION, TRAINING OR OTHER SERVICES.  INTEL WILL NOT PROVIDE ANY 
+ * UPDATES, ENHANCEMENTS OR EXTENSIONS.  INTEL SPECIFICALLY DISCLAIMS ANY 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY, NONINFRINGEMENT AND FITNESS FOR A 
+ * PARTICULAR PURPOSE. 
+ *
+ * 4.2. IN NO EVENT SHALL INTEL HAVE ANY LIABILITY TO LICENSEE, ITS LICENSEES 
+ * OR ANY OTHER THIRD PARTY, FOR ANY LOST PROFITS, LOST DATA, LOSS OF USE OR 
+ * COSTS OF PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES, OR FOR ANY INDIRECT, 
+ * SPECIAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THIS AGREEMENT, UNDER ANY 
+ * CAUSE OF ACTION OR THEORY OF LIABILITY, AND IRRESPECTIVE OF WHETHER INTEL 
+ * HAS ADVANCE NOTICE OF THE POSSIBILITY OF SUCH DAMAGES.  THESE LIMITATIONS 
+ * SHALL APPLY NOTWITHSTANDING THE FAILURE OF THE ESSENTIAL PURPOSE OF ANY 
+ * LIMITED REMEDY.
+ *
+ * 4.3. Licensee shall not export, either directly or indirectly, any of this 
+ * software or system incorporating such software without first obtaining any 
+ * required license or other approval from the U. S. Department of Commerce or 
+ * any other agency or department of the United States Government.  In the 
+ * event Licensee exports any such software from the United States or re-
+ * exports any such software from a foreign destination, Licensee shall ensure
+ * that the distribution and export/re-export of the software is in compliance 
+ * with all laws, regulations, orders, or other restrictions of the U.S. Export 
+ * Administration Regulations. Licensee agrees that neither it nor any of its 
+ * subsidiaries will export/re-export any technical data, process, software, or 
+ * service, directly or indirectly, to any country for which the United States 
+ * government or any agency thereof requires an export license, other 
+ * governmental approval, or letter of assurance, without first obtaining such
+ * license, approval or letter.
+ *
+ *****************************************************************************/
+
 
 #ifndef _DATATYPES_H
 #define _DATATYPES_H
@@ -147,9 +228,9 @@ typedef struct
 /* Operational mode of AML scanner */
 
 typedef enum {
-    Load    = 0,
-    Exec    = 1,
-    Load1   = 2
+    MODE_Load    = 0,
+    MODE_Exec    = 1,
+    MODE_Load1   = 2
 } OpMode;
 
 
@@ -174,21 +255,21 @@ typedef void*           NsHandle;
  */
 
 typedef enum {
-   Any              =  0,
-   Number           =  1, /* Byte/Word/Dword/Zero/One/Ones */
-   String           =  2,
-   Buffer           =  3,
-   Package          =  4, /* ByteConst, multiple DataTerm/Constant/SuperName */
-   FieldUnit        =  5,
-   Device           =  6, /* Name, multiple NamedObject */
-   Event            =  7,
-   Method           =  8, /* Name, ByteConst, multiple Code */
-   Mutex            =  9,
-   Region           = 10,
-   Power            = 11, /* Name,ByteConst,WordConst,multi NamedObject */
-   Processor        = 12, /* Name,ByteConst,DWordConst,ByteConst,multi NmO */
-   Thermal          = 13, /* Name, multiple NamedObject */
-   Alias            = 14,
+   TYPE_Any              =  0,
+   TYPE_Number           =  1, /* Byte/Word/Dword/Zero/One/Ones */
+   TYPE_String           =  2,
+   TYPE_Buffer           =  3,
+   TYPE_Package          =  4, /* ByteConst, multiple DataTerm/Constant/SuperName */
+   TYPE_FieldUnit        =  5,
+   TYPE_Device           =  6, /* Name, multiple NamedObject */
+   TYPE_Event            =  7,
+   TYPE_Method           =  8, /* Name, ByteConst, multiple Code */
+   TYPE_Mutex            =  9,
+   TYPE_Region           = 10,
+   TYPE_Power            = 11, /* Name,ByteConst,WordConst,multi NamedObject */
+   TYPE_Processor        = 12, /* Name,ByteConst,DWordConst,ByteConst,multi NmO */
+   TYPE_Thermal          = 13, /* Name, multiple NamedObject */
+   TYPE_Alias            = 14,
 
 /* 
  * The remaining values do not relate to the ObjectType operator and are
@@ -196,18 +277,18 @@ typedef enum {
  * official ObjectType's are added in the future.  Also, values exceeding
  * the largest ObjectType need to not overlap with defined AML opcodes.
  */
-   DefField         = 25,
-   BankField        = 26,
-   IndexField       = 27,
-   DefFieldDefn     = 28, /* Name, ByteConst, multiple FieldElement */
-   BankFieldDefn    = 29, /* 2 Name,DWordConst,ByteConst,multi FieldElement */
-   IndexFieldDefn   = 30, /* 2 Name, ByteConst, multiple FieldElement */
-   If               = 31, /* OpCode, multiple Code */
-   Else             = 32, /* multiple Code */
-   While            = 33, /* OpCode, multiple Code */
-   Scope            = 34, /* Name, multiple NamedObject */
-   DefAny           = 35, /* type is Any, suppress search of enclosing scopes */
-   Lvalue           = 36  /* Arg#, Local#, Name, Debug; used only in descriptors */
+   TYPE_DefField         = 25,
+   TYPE_BankField        = 26,
+   TYPE_IndexField       = 27,
+   TYPE_DefFieldDefn     = 28, /* Name, ByteConst, multiple FieldElement */
+   TYPE_BankFieldDefn    = 29, /* 2 Name,DWordConst,ByteConst,multi FieldElement */
+   TYPE_IndexFieldDefn   = 30, /* 2 Name, ByteConst, multiple FieldElement */
+   TYPE_If               = 31, /* OpCode, multiple Code */
+   TYPE_Else             = 32, /* multiple Code */
+   TYPE_While            = 33, /* OpCode, multiple Code */
+   TYPE_Scope            = 34, /* Name, multiple NamedObject */
+   TYPE_DefAny           = 35, /* type is Any, suppress search of enclosing scopes */
+   TYPE_Lvalue           = 36  /* Arg#, Local#, Name, Debug; used only in descriptors */
 } NsType;
 
 
