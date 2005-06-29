69f95d841d3c4b6d7afce305fff6c7578ce7db7f
aystarik <aystarik>
2005-06-29 20:24:09 +0000

    fixed ACPI_SHIFT macro
    date	2001.09.17.20.57.00;	author agrover;	state Exp;

--- source/include/platform/acgcc.h
+++ source/include/platform/acgcc.h
@@ -1,7 +1,7 @@
 /******************************************************************************
  *
  * Name: acgcc.h - GCC specific defines, etc.
- *       $Revision: 1.10 $
+ *       $Revision: 1.11 $
  *
  *****************************************************************************/
 
@@ -237,15 +237,17 @@
  * Math helper asm macros
  */
 #define ACPI_DIV_64_BY_32(n_hi, n_lo, d32, q32, r32) \
-		asm("divl %2;"            \
+		asm("divl %2;"        \
 		:"=a"(q32), "=d"(r32) \
-		:"r"(d32), \
+		:"r"(d32),            \
 		"0"(n_hi), "1"(n_lo));
 
 
 #define ACPI_SHIFT_RIGHT_64(n_hi, n_lo) \
-        asm("shrl   $1,%1;"          \
-            "rcrl   $1,%2;");
+        asm("shrl   $1,%2;"             \
+	    "rcrl   $1,%3;"             \
+	    :"=r"(n_hi), "=r"(n_lo)     \
+	    :"0"(n_hi), "1"(n_lo));
 
 /*! [End] no source code translation !*/
 
