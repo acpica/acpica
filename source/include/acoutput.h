187bbb68af77008640ae7c94d23349639e440265
aystarik <aystarik>
2005-06-29 19:55:55 +0000

    date	2000.02.04.23.43.00;	author psdiefen;	state Exp;

--- source/include/acoutput.h
+++ source/include/acoutput.h
@@ -136,9 +136,15 @@
 #define HARDWARE                    0x00000100
 #define MISCELLANEOUS               0x00000200
 #define OS_DEPENDENT                0x00000400
+
 #define DEVICE_MANAGER              0x00000800
 
-#define ALL_COMPONENTS              0x00000FFF
+#define EMBEDDED_CONTROLLER         0x00001000
+#define PROCESSOR_CONTROL           0x00002000
+#define SYSTEM_CONTROL              0x00004000
+#define THERMAL_CONTROL             0x00008000
+
+#define ALL_COMPONENTS              0x0000FFFF
 
 
 /* Exception level -- used in the global "DebugLevel" */
