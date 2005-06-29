9a848d58ede357c8279ac31a6e85d14df031672a
aystarik <aystarik>
2005-06-29 19:38:58 +0000

    Cleanup and consolidation
    date	99.04.29.22.04.00;	author rmoore1;	state Exp;

--- source/include/acinterp.h
+++ source/include/acinterp.h
@@ -1,27 +1,99 @@
-/*
-  __________________________________________________________________________
- |
- |
- |               Copyright (C) Intel Corporation 1994-1997
- |
- | All rights reserved.  No part of this program or publication may be
- | reproduced, transmitted, transcribed, stored in a retrieval system, or
- | translated into any language or computer language, in any form or by any
- | means, electronic, mechanical, magnetic, optical, chemical, manual, or
- | otherwise, without the prior written permission of Intel Corporation.
- |__________________________________________________________________________
- |
- | FILENAME: aml.h - ACPI AML (p-code) parse and dump and execution
- |                   I/O for ACPI AML scanner and dumper
- |                   OS interfaces
- |__________________________________________________________________________
- |
-//
-//    Rev 1.0   13 May 1997 17:34:48   phutchis
-// Initial revision.
- |__________________________________________________________________________
 
-*/
+/******************************************************************************
+ * 
+ * Name: interpreter.h - Interpreter subcomponent prototypes and defines
+ *
+ *****************************************************************************/
+
+/******************************************************************************
+ *
+ * 1. Copyright Notice
+ *
+ * Some or all of this work - Copyright (c) 1999, Intel Corp.  All rights 
+ * reserved.
+ *
+ * 2. License
+ * 
+ * 2.1. Intel grants, free of charge, to any person ("Licensee") obtaining a 
+ * copy of the source code appearing in this file ("Covered Code") a license 
+ * under Intel's copyrights in the base code distributed originally by Intel 
+ * ("Original Intel Code") to copy, make derivatives, distribute, use and 
+ * display any portion of the Covered Code in any form; and
+ *
+ * 2.2. Intel grants Licensee a non-exclusive and non-transferable patent 
+ * license (without the right to sublicense), under only those claims of Intel
+ * patents that are infringed by the Original Intel Code, to make, use, sell, 
+ * offer to sell, and import the Covered Code and derivative works thereof 
+ * solely to the minimum extent necessary to exercise the above copyright 
+ * license, and in no event shall the patent license extend to any additions to
+ * or modifications of the Original Intel Code.  No other license or right is 
+ * granted directly or by implication, estoppel or otherwise;
+ *
+ * the above copyright and patent license is granted only if the following 
+ * conditions are met:
+ *
+ * 3. Conditions 
+ *
+ * 3.1. Redistribution of source code of any substantial portion of the Covered 
+ * Code or modification must include the above Copyright Notice, the above 
+ * License, this list of Conditions, and the following Disclaimer and Export 
+ * Compliance provision.  In addition, Licensee must cause all Covered Code to 
+ * which Licensee contributes to contain a file documenting the changes 
+ * Licensee made to create that Covered Code and the date of any change.  
+ * Licensee must include in that file the documentation of any changes made by
+ * any predecessor Licensee.  Licensee must include a prominent statement that
+ * the modification is derived, directly or indirectly, from Original Intel 
+ * Code.
+ *
+ * 3.2. Redistribution in binary form of any substantial portion of the Covered 
+ * Code or modification must reproduce the above Copyright Notice, and the 
+ * following Disclaimer and Export Compliance provision in the documentation 
+ * and/or other materials provided with the distribution.
+ *
+ * 3.3. Intel retains all right, title, and interest in and to the Original 
+ * Intel Code.
+ *
+ * 3.4. Neither the name Intel nor any other trademark owned or controlled by 
+ * Intel shall be used in advertising or otherwise to promote the sale, use or 
+ * other dealings in products derived from or relating to the Covered Code 
+ * without prior written authorization from Intel.
+ *
+ * 4. Disclaimer and Export Compliance
+ *
+ * 4.1. INTEL MAKES NO WARRANTY OF ANY KIND REGARDING ANY SOFTWARE PROVIDED 
+ * HERE.  ANY SOFTWARE ORIGINATING FROM INTEL OR DERIVED FROM INTEL SOFTWARE 
+ * IS PROVIDED "AS IS," AND INTEL WILL NOT PROVIDE ANY SUPPORT,  ASSISTANCE, 
+ * INSTALLATION, TRAINING OR OTHER SERVICES.  INTEL WILL NOT PROVIDE ANY 
+ * UPDATES, ENHANCEMENTS OR EXTENSIONS.  INTEL SPECIFICALLY DISCLAIMS ANY 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY, NONINFRINGEMENT AND FITNESS FOR A 
+ * PARTICULAR PURPOSE. 
+ *
+ * 4.2. IN NO EVENT SHALL INTEL HAVE ANY LIABILITY TO LICENSEE, ITS LICENSEES 
+ * OR ANY OTHER THIRD PARTY, FOR ANY LOST PROFITS, LOST DATA, LOSS OF USE OR 
+ * COSTS OF PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES, OR FOR ANY INDIRECT, 
+ * SPECIAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THIS AGREEMENT, UNDER ANY 
+ * CAUSE OF ACTION OR THEORY OF LIABILITY, AND IRRESPECTIVE OF WHETHER INTEL 
+ * HAS ADVANCE NOTICE OF THE POSSIBILITY OF SUCH DAMAGES.  THESE LIMITATIONS 
+ * SHALL APPLY NOTWITHSTANDING THE FAILURE OF THE ESSENTIAL PURPOSE OF ANY 
+ * LIMITED REMEDY.
+ *
+ * 4.3. Licensee shall not export, either directly or indirectly, any of this 
+ * software or system incorporating such software without first obtaining any 
+ * required license or other approval from the U. S. Department of Commerce or 
+ * any other agency or department of the United States Government.  In the 
+ * event Licensee exports any such software from the United States or re-
+ * exports any such software from a foreign destination, Licensee shall ensure
+ * that the distribution and export/re-export of the software is in compliance 
+ * with all laws, regulations, orders, or other restrictions of the U.S. Export 
+ * Administration Regulations. Licensee agrees that neither it nor any of its 
+ * subsidiaries will export/re-export any technical data, process, software, or 
+ * service, directly or indirectly, to any country for which the United States 
+ * government or any agency thereof requires an export license, other 
+ * governmental approval, or letter of assurance, without first obtaining such
+ * license, approval or letter.
+ *
+ *****************************************************************************/
+
 
 #ifndef __AML_H__
 #define __AML_H__
@@ -64,11 +136,12 @@ extern INT32        OBJECT_DESCRIPTOR_IS_VOID;
 /* Macros to allocate and deallocate structures */
 /* GET RID OF THESE, confusing!! TBD!! */
 
-#define  NEW(type)   ((type *)OsdCallocate (sizeof(type)))
 #ifdef DELETE
 #undef DELETE
 #endif
-#define  DELETE(ptr) OsdFree ((void *)(ptr)), (ptr) = 0
+
+#define  NEW(type)          ((type *)OsdCallocate (sizeof(type)))
+#define  DELETE(ptr)        OsdFree ((void *)(ptr)), (ptr) = 0
 
 /* 
  * Macro to test for out-of-range subscript
@@ -112,102 +185,75 @@ extern OBJECT_DESCRIPTOR    *MethodStack[AML_METHOD_MAX_NEST][ARGBASE+NUMARG];
 extern INT32                MethodStackTop;
 
 
-/* |-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
- * Functions which Load/Dump/Execute Interpreted Data
- * |-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
- */
-
-INT32
-DoMatchOp (
-    OpMode              iLE);
-
-void
-DoOpByte (
-    INT32               nl, 
-    UINT16              op, 
-    OpMode              iLE);
 
 
-/* |-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
- * Functions which Load/Dump/Execute Names
- * |-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
- */
-
-char *
-LastFQN (void);
-
-void
-AllocateNameString (
-    INT32               PrefixCount,
-    INT32               NSeg);
-
-INT32
-NcOK (
-    INT32               c);
-
-void
-GenLabel (
-    char                *Name);
-
-/* |-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
- * Functions which Load/Dump/Execute Atoms
- * |-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
- */
-
-/* |-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
- * Functions which Load/Dump/Execute Expressions
- * |-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
+/*
+ * iefield - ACPI AML (p-code) execution - field manipulation
  */
 
 
 INT32
-DoOpCode (
-    OpMode              iLE);
+SetNamedFieldValue (
+    NsHandle            NamedField, 
+    UINT32              Value);
 
 INT32
-DoCode (
-    OpMode              iLE);
+GetNamedFieldValue (
+    NsHandle            NamedField, 
+    UINT32              *Value);
 
-UINT32 
-AmlBufSeq (void);
 
-INT32
-DoPkg (
-    NsType              Type, 
-    OpMode              iLE);
-
+/*
+ * ieopexec - ACPI AML (p-code) execution - specific opcodes
+ */
 
 INT32
-DoDefinitionBlock (
-    char                *name, 
-    UINT8               *addr, 
-    INT32               len,
-    LogHandle           iLstHandle, 
-    OSD_FILE            *fAsmF);
-
-void
-AmlMarkPackage (
-    OBJECT_DESCRIPTOR   *pOD);
-
-void
-vAmlMarkObject (
-    OBJECT_DESCRIPTOR   *pOD);
-
-void
-MarkObjectStack (
-    INT32               *piCount);
-
-
-
-
-/* P-code execution Prototypes */
+ExecCreateField (
+    UINT16              op);
 
 INT32
-GetMethodDepth (void);
+ExecFatal (
+    void);
+
+INT32
+ExecIndex (
+    void);
+
+INT32
+ExecMatch (
+    void);
 
 INT32
 GetRvalue (
     OBJECT_DESCRIPTOR   **StackPtr);
+
+INT32
+IsMethodValue (
+    OBJECT_DESCRIPTOR   *pOD);
+
+
+/*
+ * ieprep - ACPI AML (p-code) execution - prep utilities
+ */
+
+INT32
+GetMethodDepth (
+    void);
+
+NsType
+GetMethodValTyp (
+    INT32               Index);
+
+INT32
+GetMethodValue (
+    INT32               Index, 
+    OBJECT_DESCRIPTOR   *ObjDesc);
+
+INT32
+SetMethodValue (
+    INT32               Index, 
+    OBJECT_DESCRIPTOR   *ObjDesc, 
+    OBJECT_DESCRIPTOR   *ObjDesc2);
 
 INT32
 PrepDefFieldValue (
@@ -237,12 +283,58 @@ INT32
 PrepStack (
     char                *Types);
 
+
+/*
+ * iesystem - Interface to OS services
+ */
+
+UINT16 
+ThreadId (
+    void);
+
+INT32
+DoNotifyOp (
+    OBJECT_DESCRIPTOR   *val, 
+    OBJECT_DESCRIPTOR   *obj);
+
 void
-AmlAppendOperandDiag(
-    char                *Name, 
-    INT32               Line, 
-    UINT16              OpCode, 
-    INT32               NOperands);
+DoSuspend(
+    UINT32              Time);
+
+INT32
+AcquireOpRqst(
+    OBJECT_DESCRIPTOR   *Time, 
+    OBJECT_DESCRIPTOR   *sOb);
+
+INT32
+ReleaseOpRqst(
+    OBJECT_DESCRIPTOR   *sOb);
+
+INT32
+SignalOpRqst(
+    OBJECT_DESCRIPTOR   *sOb);
+
+INT32
+WaitOpRqst(
+    OBJECT_DESCRIPTOR   *Time, 
+    OBJECT_DESCRIPTOR   *sOb);
+
+INT32
+ResetOpRqst(
+    OBJECT_DESCRIPTOR   *sOb);
+
+INT32
+GetGlobalLock(
+    void);
+
+void
+ReleaseGlobalLock(
+    void);
+
+
+/*
+ * iexecute - ACPI AML (p-code) execution
+ */
 
 INT32
 ExecStore (
@@ -274,19 +366,6 @@ ExecDyadic2S (
     UINT16              op);
 
 INT32
-ExecCreateField (
-    UINT16              op);
-
-INT32
-ExecFatal (void);
-
-INT32
-ExecIndex (void);
-
-INT32
-ExecMatch (void);
-
-INT32
 ExecDyadic2 (
     UINT16              op);
 
@@ -296,20 +375,56 @@ AmlExec (
     INT32               Len, 
     OBJECT_DESCRIPTOR   **Params);
 
-void
-MarkMethodValues (
-    INT32               *Count);
+
+/*
+ * isatoms - interpreter/scanner atom load/execute
+ */
+
+INT32 
+DoSuperName (
+    OpMode              LoadExecMode, 
+    NsType              Define);
+
+INT32 
+DoLiteral (
+    OpMode              LoadExecMode);
+
+/*
+ * iscode - Scanner AML code manipulation routines
+ */
 
 INT32
-IsMethodValue (
-    OBJECT_DESCRIPTOR   *pOD);
+avail (
+    size_t              n);
 
+INT32
+Peek (
+    void);
 
-/* I/O Prototypes */
-/* Prototypes */
+INT32
+GetPCodeByte (
+    ptrdiff_t           Offset);
+
+INT32
+PeekOp (
+    void);
+
+UINT8 *
+ConsumeAMLByte (
+    size_t              Bytes);
+
+size_t
+ConsumeAMLStreamBytes (
+    size_t              BytesToGet, 
+    UINT8               *AmlBuffer);
 
 void
-ClearPkgStack (void);
+ConsumeAMLPackage (
+    OpMode              LoadExecMode);
+
+INT32
+IsInPCodeBlock (
+    UINT8               *Where);
 
 void
 SetPCodeInput (
@@ -326,7 +441,8 @@ AmlPrepExec (
     INT32               Len);
 
 ACPI_OBJECT_HANDLE
-PCodeHandle (void);
+PCodeHandle (
+    void);
 
 void
 GetCurrentLoc (
@@ -336,19 +452,164 @@ void
 SetCurrentLoc (
     meth                *Method);
 
-INT32
-DigitsNeeded (
-    INT32               val, 
-    INT32               base);
+
+/*
+ * isdump - Scanner debug output routines
+ */
 
 void
-LineSet (
-    INT32               n, 
+DumpAMLBuffer (
+    size_t              Length);
+
+
+INT32
+DumpStackEntry (
+    OBJECT_DESCRIPTOR   *EntryDesc);
+
+void
+DumpStack (
+    OpMode              LoadExecMode, 
+    char                *pcIdent, 
+    INT32               NumLevels, 
+    char                *pcNote);
+        
+
+/*
+ * isexprs - interpreter/scanner expression load/execute
+ */
+
+INT32 
+DoDataTerm (
+    OpMode              LoadExecMode);
+
+INT32
+DoCode (
+    OpMode              iLE);
+
+INT32
+DoPkg (
+    NsType              Type, 
+    OpMode              iLE);
+
+INT32
+DoDefinitionBlock (
+    char                *name, 
+    UINT8               *addr, 
+    INT32               len,
+    LogHandle           iLstHandle, 
+    OSD_FILE            *fAsmF);
+
+
+/*
+ * isidata - interpreter/scanner interpreted data load/execute
+ */
+
+INT32 
+DoFieldFlag (
+    OpMode              LoadExecMode);
+
+INT32
+DoMatchOp (
+    OpMode              iLE);
+
+void
+DoOpByte (
+    INT32               nl, 
+    UINT16              op, 
+    OpMode              iLE);
+
+
+/*
+ * isnames - interpreter/scanner name load/execute
+ */
+
+char *
+LastFullyQualifiedName (
+    void);
+
+void
+AllocateNameString (
+    INT32               PrefixCount,
+    INT32               NSeg);
+
+INT32
+NcOK (
+    INT32               c);
+
+INT32 
+GetEncodedPkgLen (
+    INT32               LastPkgLen);
+
+INT32 
+DoName (
+    NsType              DataType, 
     OpMode              LoadExecMode);
 
 void
-Indent (
-    INT32               n);
+GenLabel (
+    char                *Name);
+
+
+/*
+ * isopcode - interpreter/scanner expression(opcode) load/execute
+ */
+
+INT32
+DoOpCode (
+    OpMode              iLE);
+
+INT32 
+DoRefOpCode (
+    OpMode              LoadExecMode);
+
+INT32 
+DoType1OpCode (
+    OpMode              LoadExecMode);
+
+INT32 
+DoType2OpCode (
+    OpMode              LoadExecMode);
+
+
+/*
+ * isrdata - interpreter/scanner raw data load/execute
+ */
+
+void 
+ShowHexValue (
+    INT32               ByteCount, 
+    UINT8               *AmlPtr, 
+    OpMode              LoadExecMode, 
+    INT32               LeadSpace);
+
+INT32 
+DoPkgLength (
+    INT32               DoPush, 
+    OpMode              LoadExecMode);
+
+INT32 
+DoByteConst (
+    OpMode              LoadExecMode, 
+    INT32               LeadSpace);
+
+INT32 
+DoWordConst (
+    OpMode              LoadExecMode, 
+    INT32               LeadSpace);
+
+INT32 
+DoDWordConst (
+    OpMode              LoadExecMode, 
+    INT32               LeadSpace);
+
+
+/*
+ * isstack - Scanner stack routines
+ */
+
+void
+ClearPkgStack (
+    void);
 
 INT32
 PushLen (
@@ -370,212 +631,79 @@ PopLen (
     OpMode              LoadExecMode);
 
 INT32
-PopExecLen (void);
+PopExecLen (
+    void);
 
 INT32
-PopExec (void);
+PopExec (
+    void);
 
-INT32
-PkgNested (void);
-
-INT32
-avail (
-    size_t              n);
-
-INT32
-Peek (void);
-
-INT32
-GetPCodeByte (
-    ptrdiff_t           Offset);
-
-INT32
-PeekOp (void);
-
-size_t
-ConsumeAMLStreamBytes (
-    size_t              BytesToGet, 
-    UINT8               *AmlBuffer);
-
-void
-ConsumeAMLPackage (
-    OpMode              LoadExecMode);
-
-void
-DumpAMLBuffer (
-    size_t              Length);
-
-
-INT32
-DumpStackEntry (
-    OBJECT_DESCRIPTOR   *EntryDesc);
-
-void
-DumpStack (
-    OpMode              LoadExecMode, 
-    char                *pcIdent, 
-    INT32               NumLevels, 
-    char                *pcNote);
-        
 INT32
 PushIfExec (
     OpMode              LoadExecMode);
 
 INT32
-IsInPCodeBlock (
-    UINT8               *Where);
-
-void
-DeleteObject (
-    OBJECT_DESCRIPTOR   **ppOD);
+PkgNested (
+     void);
 
 
-/* OS interfaces */
-
-
-void
-DoSuspend(
-    UINT32              Time);
-
-INT32
-DoNotifyOp (
-    OBJECT_DESCRIPTOR   *val, 
-    OBJECT_DESCRIPTOR   *obj);
-
-INT32
-AcquireOpRqst(
-    OBJECT_DESCRIPTOR   *Time, 
-    OBJECT_DESCRIPTOR   *sOb);
-
-INT32
-WaitOpRqst(
-    OBJECT_DESCRIPTOR   *Time, 
-    OBJECT_DESCRIPTOR   *sOb);
-
-INT32
-ReleaseOpRqst(
-    OBJECT_DESCRIPTOR   *sOb);
-
-INT32
-ResetOpRqst(
-    OBJECT_DESCRIPTOR   *sOb);
-
-INT32
-SignalOpRqst(
-    OBJECT_DESCRIPTOR   *sOb);
-
-INT32
-GetGlobalLock(void);
-
-void
-ReleaseGlobalLock(void);
-
-
-
-INT32 
-DoByteConst (
-    OpMode              LoadExecMode, 
-    INT32               LeadSpace);
-
-INT32 
-DoWordConst (
-    OpMode              LoadExecMode, 
-    INT32               LeadSpace);
-
-INT32 
-DoDWordConst (
-    OpMode              LoadExecMode, 
-    INT32               LeadSpace);
-
-INT32 
-DoName (
-    NsType              DataType, 
-    OpMode              LoadExecMode);
-
-
-INT32 
-DoPkgLength (
-    INT32               DoPush, 
-    OpMode              LoadExecMode);
-
-INT32 
-DoFieldFlag (
-    OpMode              LoadExecMode);
-
-INT32 
-GetEncodedPkgLen (
-    INT32               LastPkgLen);
-
-INT32 
-DoType2OpCode (
-    OpMode              LoadExecMode);
-
-
-INT32 
-DoSuperName (
-    OpMode              LoadExecMode, 
-    NsType              Define);
-
-INT32 
-DoRefOpCode (
-    OpMode              LoadExecMode);
-
-INT32 
-DoLiteral (
-    OpMode              LoadExecMode);
-
-char *
-LastFullyQualifiedName (
-    void);
-
-INT32 
-DoDataTerm (
-    OpMode              LoadExecMode);
-
-INT32 
-DoType1OpCode (
-    OpMode              LoadExecMode);
-
-INT32
-SetNamedFieldValue (
-    NsHandle            NamedField, 
-    UINT32              Value);
-
-INT32
-GetNamedFieldValue (
-    NsHandle            NamedField, 
-    UINT32              *Value);
-
-INT32
-SetMethodValue (
-    INT32               Index, 
-    OBJECT_DESCRIPTOR   *ObjDesc, 
-    OBJECT_DESCRIPTOR   *ObjDesc2);
-
-INT32
-GetMethodValue (
-    INT32               Index, 
-    OBJECT_DESCRIPTOR   *ObjDesc);
-
-NsType
-GetMethodValTyp (
-    INT32               Index);
+/*
+ * isutils - interpreter/scanner utilities
+ */
 
 void
 AmlAppendBlockOwner (
     void                *Owner);
 
-void 
-ShowHexValue (
-    INT32               ByteCount, 
-    UINT8               *AmlPtr, 
-    OpMode              LoadExecMode, 
-    INT32               LeadSpace);
+void
+AmlAppendOperandDiag(
+    char                *Name, 
+    INT32               Line, 
+    UINT16              OpCode, 
+    INT32               NOperands);
 
-UINT8 *
-ConsumeAMLByte (
-    size_t              Bytes);
+UINT32 
+AmlBufSeq (
+    void);
+
+void
+DeleteObject (
+    OBJECT_DESCRIPTOR   **ppOD);
+
+INT32
+DigitsNeeded (
+    INT32               val, 
+    INT32               base);
+
+void
+LineSet (
+    INT32               n, 
+    OpMode              LoadExecMode);
+
+void
+Indent (
+    INT32               n);
+
+
+/* More PLUMBER stuff (in isutils) TBD: what is this? */
+
+void
+AmlMarkPackage (
+    OBJECT_DESCRIPTOR   *pOD);
+
+void
+vAmlMarkObject (
+    OBJECT_DESCRIPTOR   *pOD);
+
+void
+MarkObjectStack (
+    INT32               *piCount);
+
+
+void
+MarkMethodValues (
+    INT32               *Count);
 
 
 
-#endif /* __AML_H__ */
+#endif
