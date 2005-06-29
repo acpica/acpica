2bfd21152bd4797626a0c7184325b6560a07ec5f
aystarik <aystarik>
2005-06-29 17:56:34 +0000

    First BeOS compatible check-in.
    date	99.01.13.18.50.00;	author grsmith1;	state Exp;

--- source/components/executer/exsystem.c
+++ source/components/executer/exsystem.c
@@ -14,15 +14,18 @@
  | FILENAME: amlopsys.c
  |__________________________________________________________________________
  |
- | $Revision: 1.2 $
- | $Date: 2005/06/29 17:56:33 $
+ | $Revision: 1.3 $
+ | $Date: 2005/06/29 17:56:34 $
  | $Log: exsystem.c,v $
- | Revision 1.2  2005/06/29 17:56:33  aystarik
- | Detabified.
+ | Revision 1.3  2005/06/29 17:56:34  aystarik
+ | First BeOS compatible check-in.
  |
  | 
- | date	99.01.12.00.17.00;	author grsmith1;	state Exp;
+ | date	99.01.13.18.50.00;	author grsmith1;	state Exp;
  |
+ * 
+ * 3     1/13/99 10:50a Grsmith1
+ * First BeOS compatible check-in.
  * 
  * 2     1/11/99 4:17p Grsmith1
  * Detabified.
