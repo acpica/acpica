0d6ffa5cc5d2e9fc9ad1b9d5cababe0ae5c2301b
aystarik <aystarik>
2005-06-29 19:29:17 +0000

    New prototype
    date	2004.05.14.20.38.00;	author rmoore1;	state Exp;

--- source/include/acdebug.h
+++ source/include/acdebug.h
@@ -1,7 +1,7 @@
 /******************************************************************************
  *
  * Name: acdebug.h - ACPI/AML debugger
- *       $Revision: 1.72 $
+ *       $Revision: 1.74 $
  *
  *****************************************************************************/
 
@@ -9,7 +9,7 @@
  *
  * 1. Copyright Notice
  *
- * Some or all of this work - Copyright (c) 1999 - 2003, Intel Corp.
+ * Some or all of this work - Copyright (c) 1999 - 2004, Intel Corp.
  * All rights reserved.
  *
  * 2. License
@@ -238,6 +238,10 @@ AcpiDbFindNameInNamespace (
 void
 AcpiDbSetScope (
     char                    *Name);
+
+ACPI_STATUS
+AcpiDbSleep (
+    char                    *ObjectArg);
 
 void
 AcpiDbFindReferences (
