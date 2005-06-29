492e2dd27cfaef238efba227a07ded48a0602bd2
aystarik <aystarik>
2005-06-29 18:29:24 +0000

    Now returns required buffer size
    date	99.08.11.20.28.00;	author rmoore1;	state Exp;

--- source/components/namespace/nsnames.c
+++ source/components/namespace/nsnames.c
@@ -9,91 +9,110 @@
  *
  * 1. Copyright Notice
  *
- * Some or all of this work - Copyright (c) 1999, Intel Corp.  All rights 
+ * Some or all of this work - Copyright (c) 1999, Intel Corp.  All rights
  * reserved.
  *
  * 2. License
- * 
- * 2.1. Intel grants, free of charge, to any person ("Licensee") obtaining a 
- * copy of the source code appearing in this file ("Covered Code") a license 
- * under Intel's copyrights in the base code distributed originally by Intel 
- * ("Original Intel Code") to copy, make derivatives, distribute, use and 
- * display any portion of the Covered Code in any form; and
  *
- * 2.2. Intel grants Licensee a non-exclusive and non-transferable patent 
- * license (without the right to sublicense), under only those claims of Intel
- * patents that are infringed by the Original Intel Code, to make, use, sell, 
- * offer to sell, and import the Covered Code and derivative works thereof 
- * solely to the minimum extent necessary to exercise the above copyright 
- * license, and in no event shall the patent license extend to any additions to
- * or modifications of the Original Intel Code.  No other license or right is 
- * granted directly or by implication, estoppel or otherwise;
+ * 2.1. This is your license from Intel Corp. under its intellectual property
+ * rights.  You may have additional license terms from the party that provided
+ * you this software, covering your right to use that party's intellectual
+ * property rights.
  *
- * the above copyright and patent license is granted only if the following 
+ * 2.2. Intel grants, free of charge, to any person ("Licensee") obtaining a
+ * copy of the source code appearing in this file ("Covered Code") an
+ * irrevocable, perpetual, worldwide license under Intel's copyrights in the
+ * base code distributed originally by Intel ("Original Intel Code") to copy,
+ * make derivatives, distribute, use and display any portion of the Covered
+ * Code in any form, with the right to sublicense such rights; and
+ *
+ * 2.3. Intel grants Licensee a non-exclusive and non-transferable patent
+ * license (with the right to sublicense), under only those claims of Intel
+ * patents that are infringed by the Original Intel Code, to make, use, sell,
+ * offer to sell, and import the Covered Code and derivative works thereof
+ * solely to the minimum extent necessary to exercise the above copyright
+ * license, and in no event shall the patent license extend to any additions
+ * to or modifications of the Original Intel Code.  No other license or right
+ * is granted directly or by implication, estoppel or otherwise;
+ *
+ * The above copyright and patent license is granted only if the following
  * conditions are met:
  *
  * 3. Conditions 
  *
- * 3.1. Redistribution of source code of any substantial portion of the Covered 
- * Code or modification must include the above Copyright Notice, the above 
- * License, this list of Conditions, and the following Disclaimer and Export 
- * Compliance provision.  In addition, Licensee must cause all Covered Code to 
- * which Licensee contributes to contain a file documenting the changes 
- * Licensee made to create that Covered Code and the date of any change.  
- * Licensee must include in that file the documentation of any changes made by
- * any predecessor Licensee.  Licensee must include a prominent statement that
- * the modification is derived, directly or indirectly, from Original Intel 
- * Code.
+ * 3.1. Redistribution of Source with Rights to Further Distribute Source.  
+ * Redistribution of source code of any substantial portion of the Covered
+ * Code or modification with rights to further distribute source must include
+ * the above Copyright Notice, the above License, this list of Conditions,
+ * and the following Disclaimer and Export Compliance provision.  In addition,
+ * Licensee must cause all Covered Code to which Licensee contributes to
+ * contain a file documenting the changes Licensee made to create that Covered
+ * Code and the date of any change.  Licensee must include in that file the
+ * documentation of any changes made by any predecessor Licensee.  Licensee 
+ * must include a prominent statement that the modification is derived,
+ * directly or indirectly, from Original Intel Code.
  *
- * 3.2. Redistribution in binary form of any substantial portion of the Covered 
- * Code or modification must reproduce the above Copyright Notice, and the 
- * following Disclaimer and Export Compliance provision in the documentation 
- * and/or other materials provided with the distribution.
+ * 3.2. Redistribution of Source with no Rights to Further Distribute Source.  
+ * Redistribution of source code of any substantial portion of the Covered
+ * Code or modification without rights to further distribute source must
+ * include the following Disclaimer and Export Compliance provision in the
+ * documentation and/or other materials provided with distribution.  In
+ * addition, Licensee may not authorize further sublicense of source of any
+ * portion of the Covered Code, and must include terms to the effect that the
+ * license from Licensee to its licensee is limited to the intellectual
+ * property embodied in the software Licensee provides to its licensee, and
+ * not to intellectual property embodied in modifications its licensee may
+ * make.
  *
- * 3.3. Intel retains all right, title, and interest in and to the Original 
+ * 3.3. Redistribution of Executable. Redistribution in executable form of any
+ * substantial portion of the Covered Code or modification must reproduce the
+ * above Copyright Notice, and the following Disclaimer and Export Compliance
+ * provision in the documentation and/or other materials provided with the
+ * distribution.
+ *
+ * 3.4. Intel retains all right, title, and interest in and to the Original
  * Intel Code.
  *
- * 3.4. Neither the name Intel nor any other trademark owned or controlled by 
- * Intel shall be used in advertising or otherwise to promote the sale, use or 
- * other dealings in products derived from or relating to the Covered Code 
+ * 3.5. Neither the name Intel nor any other trademark owned or controlled by
+ * Intel shall be used in advertising or otherwise to promote the sale, use or
+ * other dealings in products derived from or relating to the Covered Code
  * without prior written authorization from Intel.
  *
  * 4. Disclaimer and Export Compliance
  *
- * 4.1. INTEL MAKES NO WARRANTY OF ANY KIND REGARDING ANY SOFTWARE PROVIDED 
- * HERE.  ANY SOFTWARE ORIGINATING FROM INTEL OR DERIVED FROM INTEL SOFTWARE 
- * IS PROVIDED "AS IS," AND INTEL WILL NOT PROVIDE ANY SUPPORT,  ASSISTANCE, 
- * INSTALLATION, TRAINING OR OTHER SERVICES.  INTEL WILL NOT PROVIDE ANY 
- * UPDATES, ENHANCEMENTS OR EXTENSIONS.  INTEL SPECIFICALLY DISCLAIMS ANY 
- * IMPLIED WARRANTIES OF MERCHANTABILITY, NONINFRINGEMENT AND FITNESS FOR A 
+ * 4.1. INTEL MAKES NO WARRANTY OF ANY KIND REGARDING ANY SOFTWARE PROVIDED
+ * HERE.  ANY SOFTWARE ORIGINATING FROM INTEL OR DERIVED FROM INTEL SOFTWARE
+ * IS PROVIDED "AS IS," AND INTEL WILL NOT PROVIDE ANY SUPPORT,  ASSISTANCE,
+ * INSTALLATION, TRAINING OR OTHER SERVICES.  INTEL WILL NOT PROVIDE ANY
+ * UPDATES, ENHANCEMENTS OR EXTENSIONS.  INTEL SPECIFICALLY DISCLAIMS ANY
+ * IMPLIED WARRANTIES OF MERCHANTABILITY, NONINFRINGEMENT AND FITNESS FOR A
  * PARTICULAR PURPOSE. 
  *
- * 4.2. IN NO EVENT SHALL INTEL HAVE ANY LIABILITY TO LICENSEE, ITS LICENSEES 
- * OR ANY OTHER THIRD PARTY, FOR ANY LOST PROFITS, LOST DATA, LOSS OF USE OR 
- * COSTS OF PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES, OR FOR ANY INDIRECT, 
- * SPECIAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THIS AGREEMENT, UNDER ANY 
- * CAUSE OF ACTION OR THEORY OF LIABILITY, AND IRRESPECTIVE OF WHETHER INTEL 
- * HAS ADVANCE NOTICE OF THE POSSIBILITY OF SUCH DAMAGES.  THESE LIMITATIONS 
- * SHALL APPLY NOTWITHSTANDING THE FAILURE OF THE ESSENTIAL PURPOSE OF ANY 
+ * 4.2. IN NO EVENT SHALL INTEL HAVE ANY LIABILITY TO LICENSEE, ITS LICENSEES
+ * OR ANY OTHER THIRD PARTY, FOR ANY LOST PROFITS, LOST DATA, LOSS OF USE OR
+ * COSTS OF PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES, OR FOR ANY INDIRECT,
+ * SPECIAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THIS AGREEMENT, UNDER ANY
+ * CAUSE OF ACTION OR THEORY OF LIABILITY, AND IRRESPECTIVE OF WHETHER INTEL
+ * HAS ADVANCE NOTICE OF THE POSSIBILITY OF SUCH DAMAGES.  THESE LIMITATIONS
+ * SHALL APPLY NOTWITHSTANDING THE FAILURE OF THE ESSENTIAL PURPOSE OF ANY
  * LIMITED REMEDY.
  *
- * 4.3. Licensee shall not export, either directly or indirectly, any of this 
- * software or system incorporating such software without first obtaining any 
- * required license or other approval from the U. S. Department of Commerce or 
- * any other agency or department of the United States Government.  In the 
- * event Licensee exports any such software from the United States or re-
- * exports any such software from a foreign destination, Licensee shall ensure
- * that the distribution and export/re-export of the software is in compliance 
- * with all laws, regulations, orders, or other restrictions of the U.S. Export 
- * Administration Regulations. Licensee agrees that neither it nor any of its 
- * subsidiaries will export/re-export any technical data, process, software, or 
- * service, directly or indirectly, to any country for which the United States 
- * government or any agency thereof requires an export license, other 
- * governmental approval, or letter of assurance, without first obtaining such
- * license, approval or letter.
+ * 4.3. Licensee shall not export, either directly or indirectly, any of this
+ * software or system incorporating such software without first obtaining any
+ * required license or other approval from the U. S. Department of Commerce or
+ * any other agency or department of the United States Government.  In the
+ * event Licensee exports any such software from the United States or
+ * re-exports any such software from a foreign destination, Licensee shall
+ * ensure that the distribution and export/re-export of the software is in
+ * compliance with all laws, regulations, orders, or other restrictions of the
+ * U.S. Export Administration Regulations. Licensee agrees that neither it nor
+ * any of its subsidiaries will export/re-export any technical data, process,
+ * software, or service, directly or indirectly, to any country for which the
+ * United States government or any agency thereof requires an export license,
+ * other governmental approval, or letter of assurance, without first obtaining
+ * such license, approval or letter.
  *
  *****************************************************************************/
-
 
 #define __NSNAMES_C__
 
@@ -106,6 +125,7 @@
 #define _THIS_MODULE        "nsnames.c"
 #define _COMPONENT          NAMESPACE
 
+#define PATH_SEPARATOR      '.'
 
 
 /****************************************************************************
@@ -156,7 +176,9 @@ NsNameOfScope (
     }
 
 
-    NameBuffer = LocalCallocate (Size + 1);
+    /* Allocate the buffer */
+
+    NameBuffer = CmCallocate (Size + 1);
     if (!NameBuffer)
     {
         REPORT_ERROR ("NsNameOfScope: allocation failure");
@@ -171,8 +193,7 @@ NsNameOfScope (
     while ((Size > ACPI_NAME_SIZE) && EntryToSearch->ParentScope)
     {
         Size -= ACPI_NAME_SIZE;
-        *(UINT32 *) (NameBuffer + Size) = 
-                        *(UINT32 *) NsFindParentName (EntryToSearch, 0);
+        *(UINT32 *) (NameBuffer + Size) = NsFindParentName (EntryToSearch);
         EntryToSearch = EntryToSearch->ParentScope;
     }
 
@@ -234,14 +255,14 @@ NsNameOfCurrentScope (void)
 ACPI_STATUS
 NsHandleToPathname (
     ACPI_HANDLE             TargetHandle, 
-    UINT32                  BufSize, 
+    UINT32                  *BufSize, 
     char                    *UserBuffer)
 {
     NAME_TABLE_ENTRY        *EntryToSearch = NULL;
     NAME_TABLE_ENTRY        *Temp = NULL;
-    ACPI_SIZE               FqnSize = 0;
+    ACPI_SIZE               PathLength = 0;
     ACPI_SIZE               Size;
-    INT32                   TraceFQN = 0;
+    UINT32                  UserBufSize;
 
 
     FUNCTION_TRACE ("NsHandleToPathname");
@@ -264,60 +285,59 @@ NsHandleToPathname (
         return AE_BAD_PARAMETER;
     }
 
-    /* Compute length of pathname as 5 * number of name segments */
-    
-    for (Size = 5, Temp = EntryToSearch;
+    /* 
+     * Compute length of pathname as 5 * number of name segments.
+     * Go back up the parent tree to the root
+     */
+    for (Size = PATH_SEGMENT_LENGTH, Temp = EntryToSearch;
           Temp->ParentScope;
           Temp = Temp->ParentScope)
     {
-        Size += 5;
+        Size += PATH_SEGMENT_LENGTH;
     }
 
-    
+    /* Set return length to the required path length */
+
+    PathLength = Size + 1;
+    UserBufSize = *BufSize;
+    *BufSize = PathLength;
+
     /* Check if the user buffer is sufficiently large */
 
-    FqnSize = Size + 1;
-    if (FqnSize > BufSize)
+    if (PathLength > UserBufSize)
     {
         FUNCTION_STATUS_EXIT (AE_BUFFER_OVERFLOW);
         return AE_BUFFER_OVERFLOW;
     }
-
           
-    /* Store null termination */
+    /* Store null terminator */
     
     UserBuffer[Size] = '\0';
-    Size -= 4;
-    
-    if (TraceFQN)
-    {
-        DEBUG_PRINT (ACPI_INFO, ("%d:%08lx \n", Size, EntryToSearch->Name));
-    }
-    
+    Size -= ACPI_NAME_SIZE;
+
+    /* Put the original ACPI name at the end of the path */
+
     *(UINT32 *) (UserBuffer + Size) = EntryToSearch->Name;
-    UserBuffer[--Size] = '.';
+    UserBuffer[--Size] = PATH_SEPARATOR;
 
     /* Build name backwards, putting "." between segments */
     
-    while ((Size > 4) && EntryToSearch)
+    while ((Size > ACPI_NAME_SIZE) && EntryToSearch)
     {
-        Size -= 4;
-        *(UINT32 *) (UserBuffer + Size) = 
-                        *(UINT32 *) NsFindParentName (EntryToSearch, TraceFQN);
+        Size -= ACPI_NAME_SIZE;
+        *(UINT32 *) (UserBuffer + Size) = NsFindParentName (EntryToSearch);
         
-        if (TraceFQN)
-        {
-            DEBUG_PRINT (ACPI_INFO,
-                            ("%d:%08lx \n", Size, *(UINT32 *)(UserBuffer + Size)));
-        }
-
-        UserBuffer[--Size] = '.';
+        UserBuffer[--Size] = PATH_SEPARATOR;
         EntryToSearch = EntryToSearch->ParentScope;
     }
 
-    /* Overlay the "." preceding the first segment with root name "\" */
+    /* Overlay the "." preceding the first segment with the root name "\" */
     
     UserBuffer[Size] = '\\';
+
+    DEBUG_PRINT (ACPI_INFO, ("NsHandleToPathname: Len=%d, %s \n", 
+                                PathLength, UserBuffer));
+    
     FUNCTION_STATUS_EXIT (AE_OK);
     return AE_OK;
 }
@@ -389,11 +409,11 @@ NsNameCompare (
 
     if (NsPatternMatch ((NAME_TABLE_ENTRY *) ObjHandle, Find->SearchFor))
     {
-        /* name matches pattern */
+        /* Name matches pattern */
         
         if (Find->List)
         {
-            DEBUG_PRINT (TRACE_NAMES, ("FindName: match found: %.4s\n",
+            DEBUG_PRINT (TRACE_NAMES, ("FindName: Match found: %.4s\n",
                             &((NAME_TABLE_ENTRY *) ObjHandle)->Name));
 
             Find->List[*(Find->Count)] = ObjHandle;
@@ -438,7 +458,6 @@ NsLowFindNames (
     FIND_CONTEXT            Find;
 
 
-
     FUNCTION_TRACE ("NsLowFindNames");
 
 
@@ -478,6 +497,7 @@ NsLowFindNames (
 /****************************************************************************
  *
  * FUNCTION:    NsFindNames
+
  *
  * PARAMETERS:  *SearchFor          - pattern to be found.
  *                                    4 bytes, ? matches any character.
@@ -522,14 +542,14 @@ NsFindNames (
 
     if (NS_ALL == StartHandle)
     {
-        /*  base is root    */
+        /* base is root */
 
         StartHandle = RootObject;
     }
     
     else if (((NAME_TABLE_ENTRY *) StartHandle)->Scope)
     {
-        /*  base has children to search */
+        /* base has children to search */
 
         StartHandle = ((NAME_TABLE_ENTRY *) StartHandle)->Scope;
     }
@@ -546,7 +566,7 @@ NsFindNames (
 
     if (!SearchFor)
     {
-        /*  search name not specified   */
+        /* Search name not specified */
 
         SearchFor = "????";
     }
@@ -564,7 +584,7 @@ NsFindNames (
     }
 
     Count++;                                            /* Allow for trailing null */
-    List = LocalCallocate (Count * sizeof(ACPI_HANDLE));
+    List = CmCallocate (Count * sizeof(ACPI_HANDLE));
     if (!List)
     {
         REPORT_ERROR ("NsFindNames: allocation failure");
