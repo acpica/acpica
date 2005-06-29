1b0b04844ef1b564e7320c969a4236ac5e6dfa89
aystarik <aystarik>
2005-06-29 16:13:33 +0000

    New error message
    date	2001.12.07.18.34.00;	author rmoore1;	state Exp;

--- source/compiler/asltypes.h
+++ source/compiler/asltypes.h
@@ -2,7 +2,7 @@
 /******************************************************************************
  *
  * Module Name: asltypes.h - compiler data types and struct definitions
- *              $Revision: 1.23 $
+ *              $Revision: 1.25 $
  *
  *****************************************************************************/
 
@@ -389,11 +389,14 @@ typedef enum
     ASL_MSG_SEEK,
     ASL_MSG_CLOSE,
     ASL_MSG_FIELD_ACCESS_WIDTH,
+    ASL_MSG_REGION_BYTE_ACCESS,
+    ASL_MSG_REGION_BUFFER_ACCESS,
     ASL_MSG_FIELD_UNIT_OFFSET,
     ASL_MSG_RESOURCE_FIELD,
     ASL_MSG_BYTES_TO_BITS,
     ASL_MSG_BITS_TO_BYTES,
     ASL_MSG_AML_NOT_IMPLEMENTED,
+    ASL_MSG_NO_WHILE,
 
 } ASL_MESSAGE_IDS;
 
