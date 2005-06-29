a6ff653737a88952e86c0967e93db5b6aba9d4f0
aystarik <aystarik>
2005-06-29 20:21:44 +0000

    Renamed most header files
    date	2000.07.10.21.26.00;	author rmoore1;	state Exp;

--- source/include/platform/acenv.h
+++ source/include/platform/acenv.h
@@ -127,7 +127,7 @@
  * 2) ACPI_USE_STANDARD_HEADERS - Define this if linking to a C library and
  *      the standard header files may be used.
  *
- * The ACPI subsystem only uses low level C library functions that do not call 
+ * The ACPI subsystem only uses low level C library functions that do not call
  * operating system services and may therefore be inlined in the code.
  *
  * It may be necessary to tailor these include files to the target
