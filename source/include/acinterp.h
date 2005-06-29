efedfc129ce42e681f01d8d79a5e6c7466ec4844
aystarik <aystarik>
2005-06-29 19:39:19 +0000

    Added default opregion handlers
    date	99.07.22.17.48.00;	author rmoore1;	state Exp;

--- source/include/acinterp.h
+++ source/include/acinterp.h
@@ -9,117 +9,124 @@
  *
  * 1. Copyright Notice
  *
- * Some or all of this work - Copyright (c) 1999, Intel Corp.  All rights 
+ * Some or all of this work - Copyright (c) 1999, Intel Corp.  All rights
  * reserved.
  *
  * 2. License
- * 
- * 2.1. Intel grants, free of charge, to any person ("Licensee") obtaining a 
- * copy of the source code appearing in this file ("Covered Code") a license 
- * under Intel's copyrights in the base code distributed originally by Intel 
- * ("Original Intel Code") to copy, make derivatives, distribute, use and 
- * display any portion of the Covered Code in any form; and
  *
- * 2.2. Intel grants Licensee a non-exclusive and non-transferable patent 
+ * 2.1. This is your license from Intel Corp. under its intellectual property
+ * rights.  You may have additional license terms from the party that provided
+ * you this software, covering your right to use that party's intellectual
+ * property rights.
+ *
+ * 2.2. Intel grants, free of charge, to any person ("Licensee") obtaining a
+ * copy of the source code appearing in this file ("Covered Code") an
+ * irrevocable, perpetual, worldwide license under Intel's copyrights in the
+ * base code distributed originally by Intel ("Original Intel Code") to copy,
+ * make derivatives, distribute, use and display any portion of the Covered
+ * Code in any form, with the right to sublicense such rights; and
+ *
+ * 2.3. Intel grants Licensee a non-exclusive and non-transferable patent
  * license (without the right to sublicense), under only those claims of Intel
- * patents that are infringed by the Original Intel Code, to make, use, sell, 
- * offer to sell, and import the Covered Code and derivative works thereof 
- * solely to the minimum extent necessary to exercise the above copyright 
- * license, and in no event shall the patent license extend to any additions to
- * or modifications of the Original Intel Code.  No other license or right is 
- * granted directly or by implication, estoppel or otherwise;
+ * patents that are infringed by the Original Intel Code, to make, use, sell,
+ * offer to sell, and import the Covered Code and derivative works thereof
+ * solely to the minimum extent necessary to exercise the above copyright
+ * license, and in no event shall the patent license extend to any additions
+ * to or modifications of the Original Intel Code.  No other license or right
+ * is granted directly or by implication, estoppel or otherwise;
  *
- * the above copyright and patent license is granted only if the following 
+ * The above copyright and patent license is granted only if the following
  * conditions are met:
  *
  * 3. Conditions 
  *
- * 3.1. Redistribution of source code of any substantial portion of the Covered 
- * Code or modification must include the above Copyright Notice, the above 
- * License, this list of Conditions, and the following Disclaimer and Export 
- * Compliance provision.  In addition, Licensee must cause all Covered Code to 
- * which Licensee contributes to contain a file documenting the changes 
- * Licensee made to create that Covered Code and the date of any change.  
- * Licensee must include in that file the documentation of any changes made by
- * any predecessor Licensee.  Licensee must include a prominent statement that
- * the modification is derived, directly or indirectly, from Original Intel 
- * Code.
+ * 3.1. Redistribution of Source with Rights to Further Distribute Source.  
+ * Redistribution of source code of any substantial portion of the Covered
+ * Code or modification with rights to further distribute source must include
+ * the above Copyright Notice, the above License, this list of Conditions,
+ * and the following Disclaimer and Export Compliance provision.  In addition,
+ * Licensee must cause all Covered Code to which Licensee contributes to
+ * contain a file documenting the changes Licensee made to create that Covered
+ * Code and the date of any change.  Licensee must include in that file the
+ * documentation of any changes made by any predecessor Licensee.  Licensee 
+ * must include a prominent statement that the modification is derived,
+ * directly or indirectly, from Original Intel Code.
  *
- * 3.2. Redistribution in binary form of any substantial portion of the Covered 
- * Code or modification must reproduce the above Copyright Notice, and the 
- * following Disclaimer and Export Compliance provision in the documentation 
- * and/or other materials provided with the distribution.
+ * 3.2. Redistribution of Source with no Rights to Further Distribute Source.  
+ * Redistribution of source code of any substantial portion of the Covered
+ * Code or modification without rights to further distribute source must
+ * include the following Disclaimer and Export Compliance provision in the
+ * documentation and/or other materials provided with distribution.  In
+ * addition, Licensee may not authorize further sublicense of source of any
+ * portion of the Covered Code, and must include terms to the effect that the
+ * license from Licensee to its licensee is limited to the intellectual
+ * property embodied in the software Licensee provides to its licensee, and
+ * not to intellectual property embodied in modifications its licensee may
+ * make.
  *
- * 3.3. Intel retains all right, title, and interest in and to the Original 
+ * 3.3. Redistribution of Executable. Redistribution in executable form of any
+ * substantial portion of the Covered Code or modification must reproduce the
+ * above Copyright Notice, and the following Disclaimer and Export Compliance
+ * provision in the documentation and/or other materials provided with the
+ * distribution.
+ *
+ * 3.4. Intel retains all right, title, and interest in and to the Original
  * Intel Code.
  *
- * 3.4. Neither the name Intel nor any other trademark owned or controlled by 
- * Intel shall be used in advertising or otherwise to promote the sale, use or 
- * other dealings in products derived from or relating to the Covered Code 
+ * 3.5. Neither the name Intel nor any other trademark owned or controlled by
+ * Intel shall be used in advertising or otherwise to promote the sale, use or
+ * other dealings in products derived from or relating to the Covered Code
  * without prior written authorization from Intel.
  *
  * 4. Disclaimer and Export Compliance
  *
- * 4.1. INTEL MAKES NO WARRANTY OF ANY KIND REGARDING ANY SOFTWARE PROVIDED 
- * HERE.  ANY SOFTWARE ORIGINATING FROM INTEL OR DERIVED FROM INTEL SOFTWARE 
- * IS PROVIDED "AS IS," AND INTEL WILL NOT PROVIDE ANY SUPPORT,  ASSISTANCE, 
- * INSTALLATION, TRAINING OR OTHER SERVICES.  INTEL WILL NOT PROVIDE ANY 
- * UPDATES, ENHANCEMENTS OR EXTENSIONS.  INTEL SPECIFICALLY DISCLAIMS ANY 
- * IMPLIED WARRANTIES OF MERCHANTABILITY, NONINFRINGEMENT AND FITNESS FOR A 
+ * 4.1. INTEL MAKES NO WARRANTY OF ANY KIND REGARDING ANY SOFTWARE PROVIDED
+ * HERE.  ANY SOFTWARE ORIGINATING FROM INTEL OR DERIVED FROM INTEL SOFTWARE
+ * IS PROVIDED "AS IS," AND INTEL WILL NOT PROVIDE ANY SUPPORT,  ASSISTANCE,
+ * INSTALLATION, TRAINING OR OTHER SERVICES.  INTEL WILL NOT PROVIDE ANY
+ * UPDATES, ENHANCEMENTS OR EXTENSIONS.  INTEL SPECIFICALLY DISCLAIMS ANY
+ * IMPLIED WARRANTIES OF MERCHANTABILITY, NONINFRINGEMENT AND FITNESS FOR A
  * PARTICULAR PURPOSE. 
  *
- * 4.2. IN NO EVENT SHALL INTEL HAVE ANY LIABILITY TO LICENSEE, ITS LICENSEES 
- * OR ANY OTHER THIRD PARTY, FOR ANY LOST PROFITS, LOST DATA, LOSS OF USE OR 
- * COSTS OF PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES, OR FOR ANY INDIRECT, 
- * SPECIAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THIS AGREEMENT, UNDER ANY 
- * CAUSE OF ACTION OR THEORY OF LIABILITY, AND IRRESPECTIVE OF WHETHER INTEL 
- * HAS ADVANCE NOTICE OF THE POSSIBILITY OF SUCH DAMAGES.  THESE LIMITATIONS 
- * SHALL APPLY NOTWITHSTANDING THE FAILURE OF THE ESSENTIAL PURPOSE OF ANY 
+ * 4.2. IN NO EVENT SHALL INTEL HAVE ANY LIABILITY TO LICENSEE, ITS LICENSEES
+ * OR ANY OTHER THIRD PARTY, FOR ANY LOST PROFITS, LOST DATA, LOSS OF USE OR
+ * COSTS OF PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES, OR FOR ANY INDIRECT,
+ * SPECIAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THIS AGREEMENT, UNDER ANY
+ * CAUSE OF ACTION OR THEORY OF LIABILITY, AND IRRESPECTIVE OF WHETHER INTEL
+ * HAS ADVANCE NOTICE OF THE POSSIBILITY OF SUCH DAMAGES.  THESE LIMITATIONS
+ * SHALL APPLY NOTWITHSTANDING THE FAILURE OF THE ESSENTIAL PURPOSE OF ANY
  * LIMITED REMEDY.
  *
- * 4.3. Licensee shall not export, either directly or indirectly, any of this 
- * software or system incorporating such software without first obtaining any 
- * required license or other approval from the U. S. Department of Commerce or 
- * any other agency or department of the United States Government.  In the 
- * event Licensee exports any such software from the United States or re-
- * exports any such software from a foreign destination, Licensee shall ensure
- * that the distribution and export/re-export of the software is in compliance 
- * with all laws, regulations, orders, or other restrictions of the U.S. Export 
- * Administration Regulations. Licensee agrees that neither it nor any of its 
- * subsidiaries will export/re-export any technical data, process, software, or 
- * service, directly or indirectly, to any country for which the United States 
- * government or any agency thereof requires an export license, other 
- * governmental approval, or letter of assurance, without first obtaining such
- * license, approval or letter.
+ * 4.3. Licensee shall not export, either directly or indirectly, any of this
+ * software or system incorporating such software without first obtaining any
+ * required license or other approval from the U. S. Department of Commerce or
+ * any other agency or department of the United States Government.  In the
+ * event Licensee exports any such software from the United States or
+ * re-exports any such software from a foreign destination, Licensee shall
+ * ensure that the distribution and export/re-export of the software is in
+ * compliance with all laws, regulations, orders, or other restrictions of the
+ * U.S. Export Administration Regulations. Licensee agrees that neither it nor
+ * any of its subsidiaries will export/re-export any technical data, process,
+ * software, or service, directly or indirectly, to any country for which the
+ * United States government or any agency thereof requires an export license,
+ * other governmental approval, or letter of assurance, without first obtaining
+ * such license, approval or letter.
  *
  *****************************************************************************/
-
 
 #ifndef __INTERPRETER_H__
 #define __INTERPRETER_H__
 
 
-#define AML_END_OF_BLOCK    -1
-
 #include <datatypes.h>
 #include <acpiobj.h>
 
 
-/* 
- * Macro to test for out-of-range subscript
- * No parens needed around Array in expansion since the actual parameter
- * must be the name of a declared array and cannot be an expression.
- */
+/* Interpreter constants */
 
-#define OUTRANGE(Subscript, Array) \
-    (sizeof(Array[0]) == 0 ||\
-    (UINT32)(Subscript) >= (sizeof(Array)/sizeof(Array[0])))
-
-
+#define AML_END_OF_BLOCK        -1
 #define PUSH_PKG_LENGTH         1
 #define DO_NOT_PUSH_PKG_LENGTH  0
-
-
 
 
 /*
@@ -163,13 +170,13 @@ AmlExecMatch (
  * ievalue - ACPI AML (p-code) execution - get value functions
  */
 
-INT32
+ACPI_STATUS
 AmlGetRvalue (
-    ACPI_OBJECT             **StackPtr);
+    ACPI_OBJECT_INTERNAL    **StackPtr);
 
 BOOLEAN
 AmlIsMethodValue (
-    ACPI_OBJECT             *ObjDesc);
+    ACPI_OBJECT_INTERNAL    *ObjDesc);
 
 
 /*
@@ -216,13 +223,13 @@ AmlMthStackGetType (
 ACPI_STATUS
 AmlMthStackGetValue (
     INT32                   Index, 
-    ACPI_OBJECT             *ObjDesc);
+    ACPI_OBJECT_INTERNAL    *ObjDesc);
 
 ACPI_STATUS
 AmlMthStackSetValue (
     INT32                   Index, 
-    ACPI_OBJECT             *ObjDesc, 
-    ACPI_OBJECT             *ObjDesc2);
+    ACPI_OBJECT_INTERNAL    *ObjDesc, 
+    ACPI_OBJECT_INTERNAL    *ObjDesc2);
 
 
 /*
@@ -296,8 +303,8 @@ OsThreadId (
 
 ACPI_STATUS
 OsDoNotifyOp (
-    ACPI_OBJECT             *Value, 
-    ACPI_OBJECT             *ObjDesc);
+    ACPI_OBJECT_INTERNAL    *Value, 
+    ACPI_OBJECT_INTERNAL    *ObjDesc);
 
 void
 OsDoSuspend(
@@ -305,25 +312,25 @@ OsDoSuspend(
 
 ACPI_STATUS
 OsAcquireOpRqst(
-    ACPI_OBJECT             *Time, 
-    ACPI_OBJECT             *ObjDesc);
+    ACPI_OBJECT_INTERNAL    *Time, 
+    ACPI_OBJECT_INTERNAL    *ObjDesc);
 
 ACPI_STATUS
 OsReleaseOpRqst(
-    ACPI_OBJECT             *ObjDesc);
+    ACPI_OBJECT_INTERNAL    *ObjDesc);
 
 ACPI_STATUS
 OsSignalOpRqst(
-    ACPI_OBJECT             *ObjDesc);
+    ACPI_OBJECT_INTERNAL    *ObjDesc);
 
 ACPI_STATUS
 OsWaitOpRqst(
-    ACPI_OBJECT             *Time, 
-    ACPI_OBJECT             *ObjDesc);
+    ACPI_OBJECT_INTERNAL    *Time, 
+    ACPI_OBJECT_INTERNAL    *ObjDesc);
 
 ACPI_STATUS
 OsResetOpRqst(
-    ACPI_OBJECT             *ObjDesc);
+    ACPI_OBJECT_INTERNAL    *ObjDesc);
 
 ACPI_STATUS
 OsGetGlobalLock(
@@ -342,12 +349,12 @@ ACPI_STATUS
 AmlExecuteMethod (
     INT32                   Offset, 
     INT32                   Len, 
-    ACPI_OBJECT             **Params);
+    ACPI_OBJECT_INTERNAL    **Params);
 
 ACPI_STATUS
 AmlExecStore (
-    ACPI_OBJECT             *op1, 
-    ACPI_OBJECT             *res);
+    ACPI_OBJECT_INTERNAL    *op1, 
+    ACPI_OBJECT_INTERNAL    *res);
 
 
 /*
@@ -417,7 +424,7 @@ INT32
 AmlGetPCodeByte (
     ACPI_PTRDIFF            Offset);
 
-INT32
+UINT16
 AmlPeekOp (
     void);
 
@@ -452,7 +459,7 @@ AmlPrepExec (
     ACPI_PTRDIFF            Offset, 
     INT32                   Len);
 
-ACPI_OBJECT_HANDLE
+ACPI_HANDLE
 AmlGetPCodeHandle (
     void);
 
@@ -476,7 +483,7 @@ AmlDumpBuffer (
 
 ACPI_STATUS
 AmlDumpObjStackEntry (
-    ACPI_OBJECT             *EntryDesc);
+    ACPI_OBJECT_INTERNAL    *EntryDesc);
 
 void
 AmlDumpObjStack (
@@ -487,7 +494,7 @@ AmlDumpObjStack (
 
 void
 AmlDumpObjectDescriptor (
-	ACPI_OBJECT             *ObjDesc);
+	ACPI_OBJECT_INTERNAL    *ObjDesc);
 
 
 /*
@@ -567,7 +574,7 @@ AmlAllocateNameString (
 
 INT32
 AmlGoodChar (
-    INT32                   c);
+    INT32                   Character);
 
 ACPI_STATUS
 AmlDoSeg (
@@ -674,6 +681,53 @@ INT32
 AmlDigitsNeeded (
     INT32                   Value, 
     INT32                   Base);
+
+
+/*
+ * ieregion - default OpRegion handlers
+ */
+
+ACPI_STATUS
+AmlSystemMemorySpaceHandler (
+    UINT32                  Function,
+    UINT32                  Address,
+    UINT32                  BitWidth,
+    UINT32                  *Value,
+    void                    *Context);
+
+ACPI_STATUS
+AmlSystemIoSpaceHandler (
+    UINT32                  Function,
+    UINT32                  Address,
+    UINT32                  BitWidth,
+    UINT32                  *Value,
+    void                    *Context);
+
+ACPI_STATUS
+AmlPciConfigSpaceHandler (
+    UINT32                  Function,
+    UINT32                  Address,
+    UINT32                  BitWidth,
+    UINT32                  *Value,
+    void                    *Context);
+
+ACPI_STATUS
+AmlEmbeddedControllerSpaceHandler (
+    UINT32                  Function,
+    UINT32                  Address,
+    UINT32                  BitWidth,
+    UINT32                  *Value,
+    void                    *Context);
+
+ACPI_STATUS
+AmlSmBusSpaceHandler (
+    UINT32                  Function,
+    UINT32                  Address,
+    UINT32                  BitWidth,
+    UINT32                  *Value,
+    void                    *Context);
+
+
 
 
 #endif /* __INTERPRETER_H__ */
