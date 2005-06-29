dd04ac063aff3c4d9125a57635d198c8f5bbd1f1
aystarik <aystarik>
2005-06-29 19:34:20 +0000

    fixed code max
    date	2004.07.30.17.02.00;	author rmoore1;	state Exp;

--- source/include/acexcep.h
+++ source/include/acexcep.h
@@ -1,7 +1,7 @@
 /******************************************************************************
  *
  * Name: acexcep.h - Exception codes returned by the ACPI subsystem
- *       $Revision: 1.71 $
+ *       $Revision: 1.72 $
  *
  *****************************************************************************/
 
@@ -239,7 +239,7 @@
 #define AE_AML_CIRCULAR_REFERENCE       (ACPI_STATUS) (0x0020 | AE_CODE_AML)
 #define AE_AML_BAD_RESOURCE_LENGTH      (ACPI_STATUS) (0x0021 | AE_CODE_AML)
 
-#define AE_CODE_AML_MAX                 0x0020
+#define AE_CODE_AML_MAX                 0x0021
 
 /*
  * Internal exceptions used for control
