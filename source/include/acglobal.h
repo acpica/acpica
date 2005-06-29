1901744e1abb71c869d4ccc653ac7579c3661eeb
aystarik <aystarik>
2005-06-29 19:36:07 +0000

    Updated 16-bit tools to 64-bits and ACPI 2.0 tables
    date	2000.12.20.23.10.00;	author rmoore1;	state Exp;

--- source/include/acglobal.h
+++ source/include/acglobal.h
@@ -1,7 +1,7 @@
 /******************************************************************************
  *
  * Name: acglobal.h - Declarations for global variables
- *       $Revision: 1.92 $
+ *       $Revision: 1.94 $
  *
  *****************************************************************************/
 
@@ -9,8 +9,8 @@
  *
  * 1. Copyright Notice
  *
- * Some or all of this work - Copyright (c) 1999, Intel Corp.  All rights
- * reserved.
+ * Some or all of this work - Copyright (c) 1999, 2000, Intel Corp.
+ * All rights reserved.
  *
  * 2. License
  *
@@ -253,7 +253,7 @@ ACPI_EXTERN ACPI_OBJECT_NOTIFY_HANDLER  AcpiGbl_SysNotify;
 extern      BOOLEAN                     AcpiGbl_Shutdown;
 extern      UINT32                      AcpiGbl_SystemFlags;
 extern      UINT32                      AcpiGbl_StartupFlags;
-extern      UINT8                       AcpiGbl_DecodeTo8bit[];
+extern      UINT8                       AcpiGbl_DecodeTo8bit[8];
 
 
 /*****************************************************************************
