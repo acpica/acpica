0ab723c9b7f2e368c470f541d9b45c5e98d7b613
aystarik <aystarik>
2005-06-29 20:24:11 +0000

    remove wbinvd
    date	2001.09.17.23.42.00;	author agrover;	state Exp;

--- source/include/platform/acgcc.h
+++ source/include/platform/acgcc.h
@@ -1,7 +1,7 @@
 /******************************************************************************
  *
  * Name: acgcc.h - GCC specific defines, etc.
- *       $Revision: 1.12 $
+ *       $Revision: 1.13 $
  *
  *****************************************************************************/
 
@@ -130,7 +130,6 @@
 #define BREAKPOINT3
 #define disable() __cli()
 #define enable()  __sti()
-#define wbinvd()
 
 /*! [Begin] no source code translation */
 
@@ -193,7 +192,6 @@
 #define disable() __cli()
 #define enable()  __sti()
 #define halt()    __asm__ __volatile__ ("sti; hlt":::"memory")
-#define wbinvd()  __asm__ __volatile__ ("wbinvd":::"memory")
 
 /*! [Begin] no source code translation
  *
