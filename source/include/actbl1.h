5ac807a5761fe5c016ae657be76c746b7e82fde6
aystarik <aystarik>
2005-06-29 20:07:45 +0000

    Tables specific to IA32
    date	99.10.21.17.09.00;	author rmoore1;	state Exp;

--- source/include/actbl1.h
+++ source/include/actbl1.h
@@ -1,7 +1,6 @@
 /******************************************************************************
- *
- * Name: actbl1.h - ACPI 1.0 tables
- *       $Revision: 1.27 $
+ * 
+ * Name: acpitbl32.h - ACPI tables specific to IA32
  *
  *****************************************************************************/
 
@@ -9,8 +8,8 @@
  *
  * 1. Copyright Notice
  *
- * Some or all of this work - Copyright (c) 1999 - 2003, Intel Corp.
- * All rights reserved.
+ * Some or all of this work - Copyright (c) 1999, Intel Corp.  All rights
+ * reserved.
  *
  * 2. License
  *
@@ -38,9 +37,9 @@
  * The above copyright and patent license is granted only if the following
  * conditions are met:
  *
- * 3. Conditions
+ * 3. Conditions 
  *
- * 3.1. Redistribution of Source with Rights to Further Distribute Source.
+ * 3.1. Redistribution of Source with Rights to Further Distribute Source.  
  * Redistribution of source code of any substantial portion of the Covered
  * Code or modification with rights to further distribute source must include
  * the above Copyright Notice, the above License, this list of Conditions,
@@ -48,11 +47,11 @@
  * Licensee must cause all Covered Code to which Licensee contributes to
  * contain a file documenting the changes Licensee made to create that Covered
  * Code and the date of any change.  Licensee must include in that file the
- * documentation of any changes made by any predecessor Licensee.  Licensee
+ * documentation of any changes made by any predecessor Licensee.  Licensee 
  * must include a prominent statement that the modification is derived,
  * directly or indirectly, from Original Intel Code.
  *
- * 3.2. Redistribution of Source with no Rights to Further Distribute Source.
+ * 3.2. Redistribution of Source with no Rights to Further Distribute Source.  
  * Redistribution of source code of any substantial portion of the Covered
  * Code or modification without rights to further distribute source must
  * include the following Disclaimer and Export Compliance provision in the
@@ -86,7 +85,7 @@
  * INSTALLATION, TRAINING OR OTHER SERVICES.  INTEL WILL NOT PROVIDE ANY
  * UPDATES, ENHANCEMENTS OR EXTENSIONS.  INTEL SPECIFICALLY DISCLAIMS ANY
  * IMPLIED WARRANTIES OF MERCHANTABILITY, NONINFRINGEMENT AND FITNESS FOR A
- * PARTICULAR PURPOSE.
+ * PARTICULAR PURPOSE. 
  *
  * 4.2. IN NO EVENT SHALL INTEL HAVE ANY LIABILITY TO LICENSEE, ITS LICENSEES
  * OR ANY OTHER THIRD PARTY, FOR ANY LOST PROFITS, LOST DATA, LOSS OF USE OR
@@ -114,98 +113,99 @@
  *
  *****************************************************************************/
 
-#ifndef __ACTBL1_H__
-#define __ACTBL1_H__
-
-#pragma pack(1)
-
-/*
- * ACPI 1.0 Root System Description Table (RSDT)
- */
-typedef struct rsdt_descriptor_rev1
-{
-    ACPI_TABLE_HEADER_DEF                           /* ACPI common table header */
-    UINT32                  TableOffsetEntry [1];   /* Array of pointers to other */
-                                                    /* ACPI tables */
-} RSDT_DESCRIPTOR_REV1;
+#ifndef __ACPITBL32_H__
+#define __ACPITBL32_H__
 
 
-/*
- * ACPI 1.0 Firmware ACPI Control Structure (FACS)
- */
-typedef struct facs_descriptor_rev1
-{
-    char                    Signature[4];           /* ACPI Signature */
-    UINT32                  Length;                 /* Length of structure, in bytes */
-    UINT32                  HardwareSignature;      /* Hardware configuration signature */
+
+
+/* IA32 Root System Description Table */
+
+typedef struct  
+{                                                   
+    ACPI_TABLE_HEADER       header;                 /* Table header */
+    void                    *TableOffsetEntry [1];  /* Array of pointers to other */
+                                                    /* tables' headers */
+} ROOT_SYSTEM_DESCRIPTION_TABLE;
+
+
+/* IA32 Firmware ACPI Control Structure */
+
+typedef struct  
+{                                           
+    char                    Signature[4];           /* signature "FACS" */
+    UINT32                  Length;                 /* length of structure, in bytes */
+    UINT32                  HardwareSignature;      /* hardware configuration signature */
     UINT32                  FirmwareWakingVector;   /* ACPI OS waking vector */
     UINT32                  GlobalLock;             /* Global Lock */
-    UINT32_BIT              S4Bios_f        : 1;    /* Indicates if S4BIOS support is present */
-    UINT32_BIT              Reserved1       : 31;   /* Must be 0 */
-    UINT8                   Resverved3 [40];        /* Reserved - must be zero */
+    UINT16_BIT              S4Bios_f        : 1;    /* Indicates if S4BIOS support is present */
+    UINT16_BIT              Reserved1       : 15;   /* must be 0 */
+    UINT16                  Reserved2;              /* must be 0 */
+    UINT8                   Resverved3 [40];        /* reserved - must be zero */
 
-} FACS_DESCRIPTOR_REV1;
+} FIRMWARE_ACPI_CONTROL_STRUCTURE;
 
 
-/*
- * ACPI 1.0 Fixed ACPI Description Table (FADT)
- */
-typedef struct fadt_descriptor_rev1
-{
-    ACPI_TABLE_HEADER_DEF                           /* ACPI common table header */
-    UINT32                  FirmwareCtrl;           /* Physical address of FACS */
-    UINT32                  Dsdt;                   /* Physical address of DSDT */
+/* IA32 Fixed ACPI Description Table */
+
+typedef struct  
+{                                           
+    ACPI_TABLE_HEADER       header;                 /* table header */
+    ACPI_TBLPTR             FirmwareCtrl;           /* Physical address of FACS */
+    ACPI_TBLPTR             Dsdt;                   /* Physical address of DSDT */
     UINT8                   Model;                  /* System Interrupt Model */
-    UINT8                   Reserved1;              /* Reserved */
+    UINT8                   Reserved1;              /* reserved */
     UINT16                  SciInt;                 /* System vector of SCI interrupt */
-    UINT32                  SmiCmd;                 /* Port address of SMI command port */
-    UINT8                   AcpiEnable;             /* Value to write to smi_cmd to enable ACPI */
-    UINT8                   AcpiDisable;            /* Value to write to smi_cmd to disable ACPI */
+    IO_ADDRESS              SmiCmd;                 /* Port address of SMI command port */
+    UINT8                   AcpiEnable;             /* value to write to smi_cmd to enable ACPI */
+    UINT8                   AcpiDisable;            /* value to write to smi_cmd to disable ACPI */
     UINT8                   S4BiosReq;              /* Value to write to SMI CMD to enter S4BIOS state */
-    UINT8                   Reserved2;              /* Reserved - must be zero */
-    UINT32                  Pm1aEvtBlk;             /* Port address of Power Mgt 1a AcpiEvent Reg Blk */
-    UINT32                  Pm1bEvtBlk;             /* Port address of Power Mgt 1b AcpiEvent Reg Blk */
-    UINT32                  Pm1aCntBlk;             /* Port address of Power Mgt 1a Control Reg Blk */
-    UINT32                  Pm1bCntBlk;             /* Port address of Power Mgt 1b Control Reg Blk */
-    UINT32                  Pm2CntBlk;              /* Port address of Power Mgt 2 Control Reg Blk */
-    UINT32                  PmTmrBlk;               /* Port address of Power Mgt Timer Ctrl Reg Blk */
-    UINT32                  Gpe0Blk;                /* Port addr of General Purpose AcpiEvent 0 Reg Blk */
-    UINT32                  Gpe1Blk;                /* Port addr of General Purpose AcpiEvent 1 Reg Blk */
+    UINT8                   Reserved2;              /* reserved - must be zero */
+    IO_ADDRESS              Pm1aEvtBlk;             /* Port address of Power Mgt 1a Event Reg Blk */
+    IO_ADDRESS              Pm1bEvtBlk;             /* Port address of Power Mgt 1b Event Reg Blk */
+    IO_ADDRESS              Pm1aCntBlk;             /* Port address of Power Mgt 1a Control Reg Blk */
+    IO_ADDRESS              Pm1bCntBlk;             /* Port address of Power Mgt 1b Control Reg Blk */
+    IO_ADDRESS              Pm2CntBlk;              /* Port address of Power Mgt 2 Control Reg Blk */
+    IO_ADDRESS              PmTmrBlk;               /* Port address of Power Mgt Timer Ctrl Reg Blk */
+    IO_ADDRESS              Gpe0Blk;                /* Port addr of General Purpose Event 0 Reg Blk */
+    IO_ADDRESS              Gpe1Blk;                /* Port addr of General Purpose Event 1 Reg Blk */
     UINT8                   Pm1EvtLen;              /* Byte Length of ports at pm1X_evt_blk */
     UINT8                   Pm1CntLen;              /* Byte Length of ports at pm1X_cnt_blk */
     UINT8                   Pm2CntLen;              /* Byte Length of ports at pm2_cnt_blk */
     UINT8                   PmTmLen;                /* Byte Length of ports at pm_tm_blk */
     UINT8                   Gpe0BlkLen;             /* Byte Length of ports at gpe0_blk */
     UINT8                   Gpe1BlkLen;             /* Byte Length of ports at gpe1_blk */
-    UINT8                   Gpe1Base;               /* Offset in gpe model where gpe1 events start */
-    UINT8                   Reserved3;              /* Reserved */
-    UINT16                  Plvl2Lat;               /* Worst case HW latency to enter/exit C2 state */
-    UINT16                  Plvl3Lat;               /* Worst case HW latency to enter/exit C3 state */
+    UINT8                   Gpe1Base;               /* offset in gpe model where gpe1 events start */
+    UINT8                   Reserved3;              /* reserved */
+    UINT16                  PLvl2Lat;               /* worst case HW latency to enter/exit C2 state */
+    UINT16                  PLvl3Lat;               /* worst case HW latency to enter/exit C3 state */
     UINT16                  FlushSize;              /* Size of area read to flush caches */
     UINT16                  FlushStride;            /* Stride used in flushing caches */
-    UINT8                   DutyOffset;             /* Bit location of duty cycle field in p_cnt reg */
-    UINT8                   DutyWidth;              /* Bit width of duty cycle field in p_cnt reg */
-    UINT8                   DayAlrm;                /* Index to day-of-month alarm in RTC CMOS RAM */
-    UINT8                   MonAlrm;                /* Index to month-of-year alarm in RTC CMOS RAM */
-    UINT8                   Century;                /* Index to century in RTC CMOS RAM */
-    UINT8                   Reserved4;              /* Reserved */
-    UINT8                   Reserved4a;             /* Reserved */
-    UINT8                   Reserved4b;             /* Reserved */
-    UINT32_BIT              WbInvd          : 1;    /* The wbinvd instruction works properly */
-    UINT32_BIT              WbInvdFlush     : 1;    /* The wbinvd flushes but does not invalidate */
-    UINT32_BIT              ProcC1          : 1;    /* All processors support C1 state */
-    UINT32_BIT              Plvl2Up         : 1;    /* C2 state works on MP system */
-    UINT32_BIT              PwrButton       : 1;    /* Power button is handled as a generic feature */
-    UINT32_BIT              SleepButton     : 1;    /* Sleep button is handled as a generic feature, or not present */
-    UINT32_BIT              FixedRTC        : 1;    /* RTC wakeup stat not in fixed register space */
-    UINT32_BIT              Rtcs4           : 1;    /* RTC wakeup stat not possible from S4 */
-    UINT32_BIT              TmrValExt       : 1;    /* The tmr_val width is 32 bits (0 = 24 bits) */
-    UINT32_BIT              Reserved5       : 23;   /* Reserved - must be zero */
+    UINT8                   DutyOffset;             /* bit location of duty cycle field in p_cnt reg */
+    UINT8                   DutyWidth;              /* bit width of duty cycle field in p_cnt reg */
+    UINT8                   DayAlrm;                /* index to day-of-month alarm in RTC CMOS RAM */
+    UINT8                   MonAlrm;                /* index to month-of-year alarm in RTC CMOS RAM */
+    UINT8                   Century;                /* index to century in RTC CMOS RAM */
+    UINT8                   Reserved4;              /* reserved */
+    UINT8                   Reserved4a;             /* reserved */
+    UINT8                   Reserved4b;             /* reserved */
+    UINT16_BIT              WBInvd          : 1;    /* wbinvd instruction works properly */
+    UINT16_BIT              WBInvdFlush     : 1;    /* wbinvd flushes but does not invalidate */
+    UINT16_BIT              ProcC1          : 1;    /* all processors support C1 state */
+    UINT16_BIT              PLvl2Up         : 1;    /* C2 state works on MP system */
+    UINT16_BIT              PwrButton       : 1;    /* Power button is handled as a generic feature */
+    UINT16_BIT              SleepButton     : 1;    /* Sleep button is handled as a generic feature, or not present */
+    UINT16_BIT              FixedRTC        : 1;    /* RTC wakeup stat not in fixed register space */
+    UINT16_BIT              RTCS4           : 1;    /* RTC wakeup stat not possible from S4 */
+    UINT16_BIT              TmrValExt       : 1;    /* tmr_val is 32 bits */
+    UINT16_BIT              Reserved5       : 7;    /* reserved - must be zero */
+    UINT16                  Reserved6;              /* reserved - must be zero */
 
-} FADT_DESCRIPTOR_REV1;
+}  FIXED_ACPI_DESCRIPTION_TABLE;
 
-#pragma pack()
 
-#endif /* __ACTBL1_H__ */
+
+
+#endif /* __ACPITBL32_H__ */
 
 
