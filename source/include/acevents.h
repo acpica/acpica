5699e33a0aa7ee92ae9d6aa11cb010d129430335
aystarik <aystarik>
2005-06-29 19:32:10 +0000

    remove no longer neede sci counting code
    date	2001.02.09.21.54.00;	author agrover;	state Exp;

--- source/include/acevents.h
+++ source/include/acevents.h
@@ -1,7 +1,7 @@
 /******************************************************************************
  *
  * Name: acevents.h - Event subcomponent prototypes and defines
- *       $Revision: 1.62 $
+ *       $Revision: 1.64 $
  *
  *****************************************************************************/
 
@@ -9,7 +9,7 @@
  *
  * 1. Copyright Notice
  *
- * Some or all of this work - Copyright (c) 1999, 2000, Intel Corp.
+ * Some or all of this work - Copyright (c) 1999, 2000, 2001, Intel Corp.
  * All rights reserved.
  *
  * 2. License
@@ -286,22 +286,6 @@ AcpiEvRestoreAcpiState (
 void
 AcpiEvTerminate (
     void);
-
-
-/* Debug support */
-
-#ifdef ACPI_DEBUG
-
-UINT32
-AcpiEvSciCount (
-    UINT32                  AcpiEvent);
-
-#define DEBUG_INCREMENT_EVENT_COUNT(a)   AcpiGbl_EventCount[a]++;
-
-#else
-
-#define DEBUG_INCREMENT_EVENT_COUNT(a)
-#endif
 
 
 #endif  /* __ACEVENTS_H__  */
