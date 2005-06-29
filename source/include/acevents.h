552e7d2ea801f890102867610aa63b3c08eeaae8
aystarik <aystarik>
2005-06-29 19:32:48 +0000

    Pass ISR flag to internal interfaces
    date	2004.07.14.21.44.00;	author rmoore1;	state Exp;

--- source/include/acevents.h
+++ source/include/acevents.h
@@ -1,7 +1,7 @@
 /******************************************************************************
  *
  * Name: acevents.h - Event subcomponent prototypes and defines
- *       $Revision: 1.95 $
+ *       $Revision: 1.96 $
  *
  *****************************************************************************/
 
@@ -184,7 +184,8 @@ AcpiEvNotifyDispatch (
 
 ACPI_STATUS
 AcpiEvWalkGpeList (
-    ACPI_GPE_CALLBACK       GpeWalkCallback);
+    ACPI_GPE_CALLBACK       GpeWalkCallback,
+    UINT32                  Flags);
 
 BOOLEAN
 AcpiEvValidGpeEvent (
