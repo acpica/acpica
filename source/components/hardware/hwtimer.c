fe3723fc0fc5cdc8d790c74aefc7ac6fa2604326
aystarik <aystarik>
2005-06-29 16:58:46 +0000

    speling fix
    date	2003.02.26.17.36.00;	author agrover;	state Exp;

--- source/components/hardware/hwtimer.c
+++ source/components/hardware/hwtimer.c
@@ -2,7 +2,7 @@
 /******************************************************************************
  *
  * Name: hwtimer.c - ACPI Power Management Timer Interface
- *              $Revision: 1.23 $
+ *              $Revision: 1.24 $
  *
  *****************************************************************************/
 
@@ -210,7 +210,7 @@ AcpiGetTimer (
  *              transitions (unlike many CPU timestamp counters) -- making it
  *              a versatile and accurate timer.
  *
- *              Note that this function accomodates only a single timer
+ *              Note that this function accommodates only a single timer
  *              rollover.  Thus for 24-bit timers, this function should only
  *              be used for calculating durations less than ~4.6 seconds
  *              (~20 minutes for 32-bit timers) -- calculations below
