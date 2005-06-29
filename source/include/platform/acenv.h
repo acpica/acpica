a418958cada3499dbdc95dc8bf4c59cf53efcf81
aystarik <aystarik>
2005-06-29 20:22:20 +0000

    remove tab
    date	2001.02.22.01.12.00;	author agrover;	state Exp;

--- source/include/platform/acenv.h
+++ source/include/platform/acenv.h
@@ -1,7 +1,7 @@
 /******************************************************************************
  *
  * Name: acenv.h - Generation environment specific items
- *       $Revision: 1.71 $
+ *       $Revision: 1.72 $
  *
  *****************************************************************************/
 
@@ -300,7 +300,7 @@ typedef char *va_list;
  */
 
 #define _Bnd(X, bnd)    (((sizeof (X)) + (bnd)) & (~(bnd)))
-#define va_arg(ap, T)	(*(T *)(((ap) += (_Bnd (T, _AUPBND))) - (_Bnd (T,_ADNBND))))
+#define va_arg(ap, T)   (*(T *)(((ap) += (_Bnd (T, _AUPBND))) - (_Bnd (T,_ADNBND))))
 #define va_end(ap)      (void) 0
 #define va_start(ap, A) (void) ((ap) = (((char *) &(A)) + (_Bnd (A,_AUPBND))))
 
