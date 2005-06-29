1f503c84035f82b3a10f25c5e0484dc5defc72e4
aystarik <aystarik>
2005-06-29 19:55:03 +0000

    date	99.04.21.22.19.00;	author rmoore1;	state Exp;

--- source/include/acoutput.h
+++ source/include/acoutput.h
@@ -1,19 +1,99 @@
-/*
-  __________________________________________________________________________
- |
- |
- |           Copyright (C) Intel Corporation 1994-1996
- |
- | All rights reserved.  No part of this program or publication may be
- | reproduced, transmitted, transcribed, stored in a retrieval system, or
- | translated into any language or computer language, in any form or by any
- | means, electronic, mechanical, magnetic, optical, chemical, manual, or
- | otherwise, without the prior written permission of Intel Corporation.
- |__________________________________________________________________________
- |
- | FILENAME: output.h -- debug macros and procedures
- |__________________________________________________________________________
-*/
+
+/******************************************************************************
+ * 
+ * Name: output.h -- debug macros and procedures
+ *
+ *****************************************************************************/
+
+/******************************************************************************
+ *
+ * 1. Copyright Notice
+ *
+ * Some or all of this work - Copyright (c) 1999, Intel Corp.  All rights 
+ * reserved.
+ *
+ * 2. License
+ * 
+ * 2.1. Intel grants, free of charge, to any person ("Licensee") obtaining a 
+ * copy of the source code appearing in this file ("Covered Code") a license 
+ * under Intel's copyrights in the base code distributed originally by Intel 
+ * ("Original Intel Code") to copy, make derivatives, distribute, use and 
+ * display any portion of the Covered Code in any form; and
+ *
+ * 2.2. Intel grants Licensee a non-exclusive and non-transferable patent 
+ * license (without the right to sublicense), under only those claims of Intel
+ * patents that are infringed by the Original Intel Code, to make, use, sell, 
+ * offer to sell, and import the Covered Code and derivative works thereof 
+ * solely to the minimum extent necessary to exercise the above copyright 
+ * license, and in no event shall the patent license extend to any additions to
+ * or modifications of the Original Intel Code.  No other license or right is 
+ * granted directly or by implication, estoppel or otherwise;
+ *
+ * the above copyright and patent license is granted only if the following 
+ * conditions are met:
+ *
+ * 3. Conditions 
+ *
+ * 3.1. Redistribution of source code of any substantial portion of the Covered 
+ * Code or modification must include the above Copyright Notice, the above 
+ * License, this list of Conditions, and the following Disclaimer and Export 
+ * Compliance provision.  In addition, Licensee must cause all Covered Code to 
+ * which Licensee contributes to contain a file documenting the changes 
+ * Licensee made to create that Covered Code and the date of any change.  
+ * Licensee must include in that file the documentation of any changes made by
+ * any predecessor Licensee.  Licensee must include a prominent statement that
+ * the modification is derived, directly or indirectly, from Original Intel 
+ * Code.
+ *
+ * 3.2. Redistribution in binary form of any substantial portion of the Covered 
+ * Code or modification must reproduce the above Copyright Notice, and the 
+ * following Disclaimer and Export Compliance provision in the documentation 
+ * and/or other materials provided with the distribution.
+ *
+ * 3.3. Intel retains all right, title, and interest in and to the Original 
+ * Intel Code.
+ *
+ * 3.4. Neither the name Intel nor any other trademark owned or controlled by 
+ * Intel shall be used in advertising or otherwise to promote the sale, use or 
+ * other dealings in products derived from or relating to the Covered Code 
+ * without prior written authorization from Intel.
+ *
+ * 4. Disclaimer and Export Compliance
+ *
+ * 4.1. INTEL MAKES NO WARRANTY OF ANY KIND REGARDING ANY SOFTWARE PROVIDED 
+ * HERE.  ANY SOFTWARE ORIGINATING FROM INTEL OR DERIVED FROM INTEL SOFTWARE 
+ * IS PROVIDED "AS IS," AND INTEL WILL NOT PROVIDE ANY SUPPORT,  ASSISTANCE, 
+ * INSTALLATION, TRAINING OR OTHER SERVICES.  INTEL WILL NOT PROVIDE ANY 
+ * UPDATES, ENHANCEMENTS OR EXTENSIONS.  INTEL SPECIFICALLY DISCLAIMS ANY 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY, NONINFRINGEMENT AND FITNESS FOR A 
+ * PARTICULAR PURPOSE. 
+ *
+ * 4.2. IN NO EVENT SHALL INTEL HAVE ANY LIABILITY TO LICENSEE, ITS LICENSEES 
+ * OR ANY OTHER THIRD PARTY, FOR ANY LOST PROFITS, LOST DATA, LOSS OF USE OR 
+ * COSTS OF PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES, OR FOR ANY INDIRECT, 
+ * SPECIAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THIS AGREEMENT, UNDER ANY 
+ * CAUSE OF ACTION OR THEORY OF LIABILITY, AND IRRESPECTIVE OF WHETHER INTEL 
+ * HAS ADVANCE NOTICE OF THE POSSIBILITY OF SUCH DAMAGES.  THESE LIMITATIONS 
+ * SHALL APPLY NOTWITHSTANDING THE FAILURE OF THE ESSENTIAL PURPOSE OF ANY 
+ * LIMITED REMEDY.
+ *
+ * 4.3. Licensee shall not export, either directly or indirectly, any of this 
+ * software or system incorporating such software without first obtaining any 
+ * required license or other approval from the U. S. Department of Commerce or 
+ * any other agency or department of the United States Government.  In the 
+ * event Licensee exports any such software from the United States or re-
+ * exports any such software from a foreign destination, Licensee shall ensure
+ * that the distribution and export/re-export of the software is in compliance 
+ * with all laws, regulations, orders, or other restrictions of the U.S. Export 
+ * Administration Regulations. Licensee agrees that neither it nor any of its 
+ * subsidiaries will export/re-export any technical data, process, software, or 
+ * service, directly or indirectly, to any country for which the United States 
+ * government or any agency thereof requires an export license, other 
+ * governmental approval, or letter of assurance, without first obtaining such
+ * license, approval or letter.
+ *
+ *****************************************************************************/
+
 
 #ifndef _OUTPUT_H
 #define _OUTPUT_H
@@ -85,18 +165,92 @@
 
 
 
+
+
 /*
- * Debug/Error reporting table definition
+ * Reporting macros that are never compiled out
  */
 
-typedef struct
-{
-    char            *Key;
-    char            Level;
-    char            *Description;
-    char            *Description2;
+/* Error reporting.  These versions add callers module and line# */
 
-} ST_KEY_DESC_TABLE;
+#define REPORT_INFO(a)                  _ReportInfo(_THIS_MODULE,__LINE__,_COMPONENT,a)
+#define REPORT_ERROR(a)                 _ReportError(_THIS_MODULE,__LINE__,_COMPONENT,a)
+#define REPORT_WARNING(a)               _ReportWarning(_THIS_MODULE,__LINE__,_COMPONENT,a)
+#define REPORT_SUCCESS(a)               _ReportSuccess(_THIS_MODULE,__LINE__,_COMPONENT,a)
+
+/* Error reporting.  These versions pass thru the module and line# */
+
+#define _REPORT_INFO(a,b,c,d)           _ReportInfo(a,b,c,d)
+#define _REPORT_ERROR(a,b,c,d)          _ReportError(a,b,c,d)
+#define _REPORT_WARNING(a,b,c,d)        _ReportWarning(a,b,c,d)
+
+
+/* Buffer dump macros */
+
+#define DUMP_BUFFER(a,b,c)              DumpBuffer((char *)a,b,c,_COMPONENT)
+
+
+/*
+ * Debug macros that are conditionally compiled
+ */
+
+#ifdef _DEBUG
+
+/* Conditional execution */
+
+#define DEBUG_EXEC(a)                   a;
+
+/* Function entry tracing */
+
+#define FUNCTION_TRACE(a)               FunctionTrace(_THIS_MODULE,__LINE__,_COMPONENT,a)
+
+/* Stack and buffer dumping */
+
+#define DUMP_STACK_ENTRY(a)             DumpStackEntry(a)
+#define DUMP_STACK(a,b,c,d)             DumpStack(a,b,c,d)
+#define DUMP_ENTRY(a)                   NsDumpEntry (a)
+#define DUMP_TABLES(a,b)                NsDumpTables(a,b)
+
+/* 
+ * Master debug print macros 
+ * Print iff:
+ *    1) Debug print for the current component is enabled
+ *    2) Debug error level or trace level for the print statement is enabled
+ *
+ */
+
+#define	PARAM_LIST(PL) PL
+#define TEST_DEBUG_SWITCH(lvl)          if (((lvl) & DebugLevel) && (_COMPONENT & DebugLayer))
+
+#define DEBUG_PRINT(lvl,fp)             TEST_DEBUG_SWITCH(lvl) {\
+                                            DebugPrintPrefix (_THIS_MODULE,__LINE__,_COMPONENT);\
+                                            DebugPrintRaw PARAM_LIST(fp);}
+
+#define DEBUG_PRINT_RAW(lvl,fp)         TEST_DEBUG_SWITCH(lvl) {\
+                                            DebugPrintRaw PARAM_LIST(fp);}
+
+
+
+
+#else
+/* 
+ * This is the non-debug case -- make everything go away,
+ * leaving no executable debug code!
+ */
+
+#define DEBUG_EXEC(a)                       
+#define FUNCTION_TRACE(a)
+#define DUMP_STACK_ENTRY(a)
+#define DUMP_STACK(a,b,c,d)
+#define DUMP_ENTRY(a)
+#define DUMP_TABLES(a,b)
+
+#define DEBUG_PRINT(l,f)
+#define DEBUG_PRINT_RAW(l,f) 
+
+#endif
+
+
 
 
 /* Prototypes */
@@ -170,7 +324,10 @@ DumpBuffer (
 
 void
 DumpObjectDescriptor (
-	OBJECT_DESCRIPTOR *Obj);
+	OBJECT_DESCRIPTOR   *Obj);
+
+
+/* TBD: simplify or remove entirely */
 
 void 
 _Kinc_error (char *, INT32, INT32, char *, INT32, INT32); 
@@ -181,7 +338,7 @@ _Kinc_warning (char *, INT32, INT32, char *, INT32, INT32);
 
 
 /*
- * Memory allocation functions.
+ * Memory allocation functions and related macros.
  * Macros that expand to include filename and line number
  *
  * TBD:  Move to a more appropriate header!!
@@ -215,81 +372,6 @@ _LocalCallocate (
 
 
 
-/*
- * Reporting macros that are never compiled out
- */
-
-/* Error reporting.  These versions add callers module/line# */
-
-#define REPORT_INFO(a)                  _ReportInfo(_THIS_MODULE,__LINE__,_COMPONENT,a)
-#define REPORT_ERROR(a)                 _ReportError(_THIS_MODULE,__LINE__,_COMPONENT,a)
-#define REPORT_WARNING(a)               _ReportWarning(_THIS_MODULE,__LINE__,_COMPONENT,a)
-#define REPORT_SUCCESS(a)               _ReportSuccess(_THIS_MODULE,__LINE__,_COMPONENT,a)
-
-/* Error reporting.  These versions pass thru the module/line# */
-
-#define _REPORT_INFO(a,b,c,d)           _ReportInfo(a,b,c,d)
-#define _REPORT_ERROR(a,b,c,d)          _ReportError(a,b,c,d)
-#define _REPORT_WARNING(a,b,c,d)        _ReportWarning(a,b,c,d)
-
-
-/* Buffer dump macros */
-
-#define DUMP_BUFFER(a,b,c)              DumpBuffer((char *)a,b,c,_COMPONENT)
-
-
-/*
- * Debug macros that are conditionally compiled
- */
-
-#ifdef _DEBUG
-
-/* Function entry tracing */
-
-#define FUNCTION_TRACE(a)               FunctionTrace(_THIS_MODULE,__LINE__,_COMPONENT,a)
-
-/* Stack and buffer dumping */
-
-#define DUMP_STACK_ENTRY(a)             DumpStackEntry(a)
-#define DUMP_STACK(a,b,c,d)             DumpStack(a,b,c,d)
-#define DUMP_ENTRY(a)                   NsDumpEntry (a)
-#define DUMP_TABLES(a,b)                NsDumpTables(a,b)
-
-/* 
- * Master debug print macros 
- * Print iff:
- *    1) Debug print for the current component is enabled
- *    2) Debug error level or trace level for the print statement is enabled
- *
- */
-
-#define	PARAM_LIST(PL) PL
-#define TEST_DEBUG_SWITCH(lvl)          if ((lvl & DebugLevel) && (_COMPONENT & DebugLayer))
-
-#define DEBUG_PRINT(lvl,fp)             TEST_DEBUG_SWITCH(lvl) {\
-                                            DebugPrintPrefix (_THIS_MODULE,__LINE__,_COMPONENT);\
-                                            DebugPrintRaw PARAM_LIST(fp);}
-
-#define DEBUG_PRINT_RAW(lvl,fp)         TEST_DEBUG_SWITCH(lvl) {\
-                                            DebugPrintRaw PARAM_LIST(fp);}
-
-#define DEBUG_EXEC(a)                   a;
-
-#else
-
-/* Non-debug case -- make everything go away */
-
-#define FUNCTION_TRACE(a)
-#define DUMP_STACK_ENTRY(a)
-#define DUMP_STACK(a,b,c,d)
-#define DUMP_ENTRY(a)
-#define DUMP_TABLES(a,b)
-
-#define DEBUG_PRINT(l,f)
-#define DEBUG_PRINT_RAW(l,f) 
-#define DEBUG_EXEC(a)                       
-
-#endif
 
 
 
@@ -306,7 +388,7 @@ _LocalCallocate (
 
 /*  
  * start using bits from the other end to minimize the possibility
- *  of additional flag bits from BU being defined that will conflict
+ * of additional flag bits from BU being defined that will conflict
  */
 #define OUTPUT_ERRORS       0x80
 
@@ -397,19 +479,6 @@ open_log (
 
 INT32
 close_log (LogHandle handle);                             /* LogHandle to close */
-
-
-/* Orphaned prototypes TBD:  move to appropriate header !!!*/
-
-
-UINT8
-checksum (
-    void                *Buf,
-    UINT32              BufSize);
-
-
-INT32
-AcpiInit (char          *FileName);
 
 
 
