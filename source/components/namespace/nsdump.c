69b3596c48677a69a070af2ba303d98fffe3fbfe
aystarik <aystarik>
2005-06-29 18:23:10 +0000

    ACPI_ALLOCATE_LOCAL_BUFFER now allocates via the local tracking interface (allows the allocation to be used in internal objects as well as tracks the allocation).
    date	2002.02.08.20.11.00;	author rmoore1;	state Exp;

--- source/components/namespace/nsdump.c
+++ source/components/namespace/nsdump.c
@@ -1,7 +1,7 @@
 /******************************************************************************
  *
  * Module Name: nsdump - table dumping routines for debug
- *              $Revision: 1.124 $
+ *              $Revision: 1.125 $
  *
  *****************************************************************************/
 
@@ -217,7 +217,7 @@ AcpiNsDumpPathname (
     if (ACPI_SUCCESS (Status))
     {
         AcpiOsPrintf ("%s %s (Node %p)\n", Msg, Buffer.Pointer, Handle);
-        AcpiOsFree (Buffer.Pointer);
+        ACPI_MEM_FREE (Buffer.Pointer);
     }
 
     return_ACPI_STATUS (Status);
