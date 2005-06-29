08ac6a3fc654c2d9392b78bfb5d66acfaa7b2ecf
aystarik <aystarik>
2005-06-29 20:01:04 +0000

    Added ACPI_SYSTEM_XFACE to specify calling convention for "system" interfaces and callbacks.
    date	2002.02.14.23.50.00;	author rmoore1;	state Exp;

--- source/include/acpiosxf.h
+++ source/include/acpiosxf.h
@@ -154,12 +154,12 @@ typedef struct AcpiFatalInfo
  * Types specific to the OS service interfaces
  */
 
-typedef
-UINT32 (*OSD_HANDLER) (
+typedef UINT32 
+(ACPI_SYSTEM_XFACE *OSD_HANDLER) (
     void                    *Context);
 
-typedef
-void (*OSD_EXECUTION_CALLBACK) (
+typedef void 
+(ACPI_SYSTEM_XFACE *OSD_EXECUTION_CALLBACK) (
     void                    *Context);
 
 
