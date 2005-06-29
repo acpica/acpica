a478d6722b5507bcc03175bbc8e207e742aeb6ca
aystarik <aystarik>
2005-06-29 20:26:19 +0000

    date	2003.07.13.22.12.00;	author agrover;	state Exp;

--- source/include/platform/acnetbsd.h
+++ source/include/platform/acnetbsd.h
@@ -1,7 +1,7 @@
 /******************************************************************************
  *
  * Name: acnetbsd.h - OS specific defines, etc.
- *       $Revision: 1.7 $
+ *       $Revision: 1.1 $
  *
  *****************************************************************************/
 
@@ -9,7 +9,7 @@
  *
  * 1. Copyright Notice
  *
- * Some or all of this work - Copyright (c) 1999 - 2005, Intel Corp.
+ * Some or all of this work - Copyright (c) 1999 - 2003, Intel Corp.
  * All rights reserved.
  *
  * 2. License
@@ -117,21 +117,31 @@
 #ifndef __ACNETBSD_H__
 #define __ACNETBSD_H__
 
+#if 0
+/*
+ * XXX this is technically correct, but will cause problems with some ASL
+ *     which only works if the string names a Microsoft operating system.
+ */
+#define ACPI_OS_NAME                "NetBSD"
+#else
+#define	ACPI_OS_NAME                "Microsoft Windows NT"
+#endif
+
 /* NetBSD uses GCC */
 
 #include "acgcc.h"
 
 #ifdef _LP64
-#define ACPI_MACHINE_WIDTH      64
+#define	ACPI_MACHINE_WIDTH	64
 #else
-#define ACPI_MACHINE_WIDTH      32
+#define	ACPI_MACHINE_WIDTH	32
 #endif
 
-#define COMPILER_DEPENDENT_INT64  int64_t
-#define COMPILER_DEPENDENT_UINT64 uint64_t
+#define	COMPILER_DEPENDENT_INT64  int64_t
+#define	COMPILER_DEPENDENT_UINT64 uint64_t
 
 #ifdef _KERNEL
-#include "opt_acpi.h"           /* collect build-time options here */
+#include "opt_acpi.h"		/* collect build-time options here */
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -140,14 +150,15 @@
 
 #define asm         __asm
 
-#define ACPI_USE_NATIVE_DIVIDE
+#define	ACPI_USE_NATIVE_DIVIDE
 
-#define ACPI_ASM_MACROS         /* tell acenv.h */
+#define ACPI_ASM_MACROS		/* tell acenv.h */
 
-#define ACPI_SYSTEM_XFACE
-#define ACPI_EXTERNAL_XFACE
-#define ACPI_INTERNAL_XFACE
-#define ACPI_INTERNAL_VAR_XFACE
+#define	ACPI_SYSTEM_XFACE       
+#define	ACPI_EXTERNAL_XFACE     
+#define	ACPI_INTERNAL_XFACE
+#define	ACPI_INTERNAL_VAR_XFACE
+#define	ACPI_DISASSEMBLER
 
 #ifdef ACPI_DEBUG
 #define ACPI_DEBUG_OUTPUT
@@ -155,10 +166,9 @@
 #ifdef DEBUGGER_THREADING
 #undef DEBUGGER_THREADING
 #endif /* DEBUGGER_THREADING */
-#define DEBUGGER_THREADING 0    /* integrated with DDB */
+#define DEBUGGER_THREADING 0	/* integrated with DDB */
 #include "opt_ddb.h"
 #ifdef DDB
-#define ACPI_DISASSEMBLER
 #define ACPI_DEBUGGER
 #endif /* DDB */
 #endif /* ACPI_DEBUG */
@@ -166,7 +176,7 @@
 static __inline int
 isprint(int ch)
 {
-        return(isspace(ch) || isascii(ch));
+	return(isspace(ch) || isascii(ch));
 }
 
 #else /* _KERNEL */
@@ -176,8 +186,8 @@ isprint(int ch)
 /* Not building kernel code, so use libc */
 #define ACPI_USE_STANDARD_HEADERS
 
-#define __cli()
-#define __sti()
+#define	__cli()
+#define	__sti()
 
 /* XXX */
 #define __inline inline
@@ -188,4 +198,15 @@ isprint(int ch)
 #define ACPI_USE_SYSTEM_CLIBRARY
 #define ACPI_USE_NATIVE_DIVIDE
 
+/* NetBSD doesn't have strupr, should be fixed. (move to libkern) */
+static __inline char *
+strupr(char *str)
+{
+    char *c = str;
+    while(*c) {
+	*c = toupper(*c);
+	c++;
+    }
+    return(str);
+}
 #endif /* __ACNETBSD_H__ */
