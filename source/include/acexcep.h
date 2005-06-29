58178ec39f6e5ab8a0a672a391263d4b931860b2
aystarik <aystarik>
2005-06-29 19:33:02 +0000

    Merged AML and ACPI exception codes
    date	99.05.12.21.57.00;	author rmoore1;	state Exp;

--- source/include/acexcep.h
+++ source/include/acexcep.h
@@ -1,76 +1,146 @@
-/*
-  __________________________________________________________________________
- |
- |
- |           Copyright (C) Intel Corporation 1994-1996
- |
- | All rights reserved.  No part of this program or publication may be
- | reproduced, transmitted, transcribed, stored in a retrieval system, or
- | translated into any language or computer language, in any form or by any
- | means, electronic, mechanical, magnetic, optical, chemical, manual, or
- | otherwise, without the prior written permission of Intel Corporation.
- |__________________________________________________________________________
- |
- | FILENAME: excep.h - local exception codes
- |__________________________________________________________________________
-*/
 
+/******************************************************************************
+ * 
+ * Name: excep.h - local exception codes
+ *
+ *****************************************************************************/
+
+/******************************************************************************
+ *
+ * 1. Copyright Notice
+ *
+ * Some or all of this work - Copyright (c) 1999, Intel Corp.  All rights 
+ * reserved.
+ *
+ * 2. License
+ * 
+ * 2.1. Intel grants, free of charge, to any person ("Licensee") obtaining a 
+ * copy of the source code appearing in this file ("Covered Code") a license 
+ * under Intel's copyrights in the base code distributed originally by Intel 
+ * ("Original Intel Code") to copy, make derivatives, distribute, use and 
+ * display any portion of the Covered Code in any form; and
+ *
+ * 2.2. Intel grants Licensee a non-exclusive and non-transferable patent 
+ * license (without the right to sublicense), under only those claims of Intel
+ * patents that are infringed by the Original Intel Code, to make, use, sell, 
+ * offer to sell, and import the Covered Code and derivative works thereof 
+ * solely to the minimum extent necessary to exercise the above copyright 
+ * license, and in no event shall the patent license extend to any additions to
+ * or modifications of the Original Intel Code.  No other license or right is 
+ * granted directly or by implication, estoppel or otherwise;
+ *
+ * the above copyright and patent license is granted only if the following 
+ * conditions are met:
+ *
+ * 3. Conditions 
+ *
+ * 3.1. Redistribution of source code of any substantial portion of the Covered 
+ * Code or modification must include the above Copyright Notice, the above 
+ * License, this list of Conditions, and the following Disclaimer and Export 
+ * Compliance provision.  In addition, Licensee must cause all Covered Code to 
+ * which Licensee contributes to contain a file documenting the changes 
+ * Licensee made to create that Covered Code and the date of any change.  
+ * Licensee must include in that file the documentation of any changes made by
+ * any predecessor Licensee.  Licensee must include a prominent statement that
+ * the modification is derived, directly or indirectly, from Original Intel 
+ * Code.
+ *
+ * 3.2. Redistribution in binary form of any substantial portion of the Covered 
+ * Code or modification must reproduce the above Copyright Notice, and the 
+ * following Disclaimer and Export Compliance provision in the documentation 
+ * and/or other materials provided with the distribution.
+ *
+ * 3.3. Intel retains all right, title, and interest in and to the Original 
+ * Intel Code.
+ *
+ * 3.4. Neither the name Intel nor any other trademark owned or controlled by 
+ * Intel shall be used in advertising or otherwise to promote the sale, use or 
+ * other dealings in products derived from or relating to the Covered Code 
+ * without prior written authorization from Intel.
+ *
+ * 4. Disclaimer and Export Compliance
+ *
+ * 4.1. INTEL MAKES NO WARRANTY OF ANY KIND REGARDING ANY SOFTWARE PROVIDED 
+ * HERE.  ANY SOFTWARE ORIGINATING FROM INTEL OR DERIVED FROM INTEL SOFTWARE 
+ * IS PROVIDED "AS IS," AND INTEL WILL NOT PROVIDE ANY SUPPORT,  ASSISTANCE, 
+ * INSTALLATION, TRAINING OR OTHER SERVICES.  INTEL WILL NOT PROVIDE ANY 
+ * UPDATES, ENHANCEMENTS OR EXTENSIONS.  INTEL SPECIFICALLY DISCLAIMS ANY 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY, NONINFRINGEMENT AND FITNESS FOR A 
+ * PARTICULAR PURPOSE. 
+ *
+ * 4.2. IN NO EVENT SHALL INTEL HAVE ANY LIABILITY TO LICENSEE, ITS LICENSEES 
+ * OR ANY OTHER THIRD PARTY, FOR ANY LOST PROFITS, LOST DATA, LOSS OF USE OR 
+ * COSTS OF PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES, OR FOR ANY INDIRECT, 
+ * SPECIAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THIS AGREEMENT, UNDER ANY 
+ * CAUSE OF ACTION OR THEORY OF LIABILITY, AND IRRESPECTIVE OF WHETHER INTEL 
+ * HAS ADVANCE NOTICE OF THE POSSIBILITY OF SUCH DAMAGES.  THESE LIMITATIONS 
+ * SHALL APPLY NOTWITHSTANDING THE FAILURE OF THE ESSENTIAL PURPOSE OF ANY 
+ * LIMITED REMEDY.
+ *
+ * 4.3. Licensee shall not export, either directly or indirectly, any of this 
+ * software or system incorporating such software without first obtaining any 
+ * required license or other approval from the U. S. Department of Commerce or 
+ * any other agency or department of the United States Government.  In the 
+ * event Licensee exports any such software from the United States or re-
+ * exports any such software from a foreign destination, Licensee shall ensure
+ * that the distribution and export/re-export of the software is in compliance 
+ * with all laws, regulations, orders, or other restrictions of the U.S. Export 
+ * Administration Regulations. Licensee agrees that neither it nor any of its 
+ * subsidiaries will export/re-export any technical data, process, software, or 
+ * service, directly or indirectly, to any country for which the United States 
+ * government or any agency thereof requires an export license, other 
+ * governmental approval, or letter of assurance, without first obtaining such
+ * license, approval or letter.
+ *
+ *****************************************************************************/
 
 
 #ifndef _EXCEP_H
 #define _EXCEP_H
 
 
-/* Error codes  */
+/* 
+ * Exceptions returned by external ACPI interfaces
+ */
 
-#ifndef EXIT_SUCCESS                                  /* also defined in stdlib.h as 0 */
-#define EXIT_SUCCESS        0
-#endif
+#define ACPI_SUCCESS(a)     (!(a))
+#define ACPI_FAILURE(a)     (a)
 
-#ifndef E_OK
-#define E_OK                0
-#endif
+#define AE_OK               (ACPI_STATUS) 0x0000
+#define AE_PENDING          (ACPI_STATUS) 0x0001
+#define AE_AML_ERROR        (ACPI_STATUS) 0x0002
+#define AE_RETURN_VALUE     (ACPI_STATUS) 0x0003
+#define AE_ERROR            (ACPI_STATUS) 0x0004
+#define AE_NO_ACPI_TABLES   (ACPI_STATUS) 0x0005
+#define AE_NO_NAMESPACE     (ACPI_STATUS) 0x0006
+#define AE_NO_MEMORY        (ACPI_STATUS) 0x0007
+#define AE_BAD_HEADER       (ACPI_STATUS) 0x0008
+#define AE_BAD_CHECKSUM     (ACPI_STATUS) 0x0009
+#define AE_NOT_FOUND        (ACPI_STATUS) 0x000A
+#define AE_BAD_PARAMETER    (ACPI_STATUS) 0x000B
+#define AE_NULL_ENTRY       (ACPI_STATUS) 0x000C
+#define AE_FILE_NOT_EXIST   (ACPI_STATUS) 0x000D
+#define AE_BUFFER_OVERFLOW  (ACPI_STATUS) 0x000E
+#define AE_BAD_CHARACTER    (ACPI_STATUS) 0x000F
+#define AE_CONTEXT          (ACPI_STATUS) 0x0010
 
-#ifndef NO_ERROR
-#define NO_ERROR            0
-#endif
 
-#ifndef E_ERROR
-#define E_ERROR             1
-#endif
+/*
+ * Return values used in the AML scanner
+ *
+ * Note that S_FAILURE is not an error, but indicates
+ * that other alternatives should be checked.
+ *
+ * S_RETURN is mostly handled like an error -- a caller which receives either
+ * one cleans up, terminates, and passes the received status value back to its
+ * own caller -- but S_RETURN is special-cased in appropriate places.
+ */
 
-#ifdef E_IRQ_ERROR
-#undef E_IRQ_ERROR
-#endif
-#define E_IRQ_ERROR         -1 /* MUST be negative */
+//#define S_SUCCESS   0       /* found what we were looking for */
+//#define S_FAILURE   1       /* did not find what we looked for */
+//#define S_ERROR     2       /* something is wrong */
+//#define S_RETURN    3       /* ReturnOp executed. */
 
-#ifndef E_FILE_NOT_EXIST
-#define E_FILE_NOT_EXIST    1
-#endif
-
-#ifndef E_ARGUMENT
-#define E_ARGUMENT          2
-#endif
-
-#ifndef E_MEMORY
-#define E_MEMORY            3
-#endif
-
-/* returned from parse_command_line() */
-
-#ifndef E_HELP_MESSAGE
-#define E_HELP_MESSAGE      4
-#endif
-
-#ifndef E_NO_ACPI_TBLS
-#define E_NO_ACPI_TBLS      5
-#endif
-
-/* returned from parse_command_line() */
-
-#ifndef E_NON_STANDARD_ARGUMENT
-#define E_NON_STANDARD_ARGUMENT 0xff
-#endif
 
 
 
