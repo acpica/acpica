9cb991cdc599e16190c4bcdb7efccb655923fe9f
aystarik <aystarik>
2005-06-29 17:40:32 +0000

    Cleanup
    date	2003.10.22.22.11.00;	author rmoore1;	state Exp;

--- source/components/executer/exoparg3.c
+++ source/components/executer/exoparg3.c
@@ -2,7 +2,7 @@
 /******************************************************************************
  *
  * Module Name: exoparg3 - AML execution - opcodes with 3 arguments
- *              $Revision: 1.16 $
+ *              $Revision: 1.18 $
  *
  *****************************************************************************/
 
@@ -10,7 +10,7 @@
  *
  * 1. Copyright Notice
  *
- * Some or all of this work - Copyright (c) 1999 - 2002, Intel Corp.
+ * Some or all of this work - Copyright (c) 1999 - 2003, Intel Corp.
  * All rights reserved.
  *
  * 2. License
@@ -177,14 +177,13 @@ AcpiExOpcode_3A_0T_0R (
 
     switch (WalkState->Opcode)
     {
-
     case AML_FATAL_OP:          /* Fatal (FatalType  FatalCode  FatalArg)    */
 
         ACPI_DEBUG_PRINT ((ACPI_DB_INFO,
             "FatalOp: Type %X Code %X Arg %X <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n",
-            (UINT32) Operand[0]->Integer.Value, (UINT32) Operand[1]->Integer.Value,
+            (UINT32) Operand[0]->Integer.Value,
+            (UINT32) Operand[1]->Integer.Value,
             (UINT32) Operand[2]->Integer.Value));
-
 
         Fatal = ACPI_MEM_ALLOCATE (sizeof (ACPI_SIGNAL_FATAL_INFO));
         if (Fatal)
