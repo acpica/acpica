a9e48c5235f9226eeb51ea6fae6b477354f53656
aystarik <aystarik>
2005-06-29 20:35:29 +0000

    detabbed
    date	2003.01.10.18.35.00;	author rmoore1;	state Exp;

--- source/os_specific/service_layers/oswinxf.c
+++ source/os_specific/service_layers/oswinxf.c
@@ -1,7 +1,7 @@
 /******************************************************************************
  *
  * Module Name: oswinxf - Windows OSL
- *              $Revision: 1.43 $
+ *              $Revision: 1.45 $
  *
  *****************************************************************************/
 
@@ -9,7 +9,7 @@
  *
  * 1. Copyright Notice
  *
- * Some or all of this work - Copyright (c) 1999 - 2002, Intel Corp.
+ * Some or all of this work - Copyright (c) 1999 - 2003, Intel Corp.
  * All rights reserved.
  *
  * 2. License
@@ -1171,9 +1171,9 @@ AcpiOsWritePciConfiguration (
 /* TEMPORARY STUB FUNCTION */
 void
 AcpiOsDerivePciId(
-    ACPI_HANDLE	            rhandle,
+    ACPI_HANDLE             rhandle,
     ACPI_HANDLE             chandle,
-    ACPI_PCI_ID	            **PciId)
+    ACPI_PCI_ID             **PciId)
 {
 
 }
