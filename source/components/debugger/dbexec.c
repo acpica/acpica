8b7c4407e00439ccc4da4ec084d2d4352a4f92bf
aystarik <aystarik>
2005-06-29 16:20:27 +0000

    Moved structure to aclocals.h
    date	2001.05.29.16.46.00;	author rmoore1;	state Exp;

--- source/components/debugger/dbexec.c
+++ source/components/debugger/dbexec.c
@@ -1,7 +1,7 @@
 /*******************************************************************************
  *
  * Module Name: dbexec - debugger control method execution
- *              $Revision: 1.24 $
+ *              $Revision: 1.26 $
  *
  ******************************************************************************/
 
@@ -130,18 +130,6 @@
 
 #define _COMPONENT          ACPI_DEBUGGER
         MODULE_NAME         ("dbexec")
-
-
-typedef struct dbmethodinfo
-{
-    ACPI_HANDLE             ThreadGate;
-    NATIVE_CHAR             *Name;
-    NATIVE_CHAR             **Args;
-    UINT32                  Flags;
-    UINT32                  NumLoops;
-    NATIVE_CHAR             Pathname[128];
-
-} DB_METHOD_INFO;
 
 
 DB_METHOD_INFO              Info;
@@ -333,7 +321,8 @@ AcpiDbExecute (
 
     if (ACPI_FAILURE (Status))
     {
-        AcpiOsPrintf ("Execution of %s failed with status %s\n", Info.Pathname, AcpiCmFormatException (Status));
+        AcpiOsPrintf ("Execution of %s failed with status %s\n", 
+            Info.Pathname, AcpiUtFormatException (Status));
     }
 
     else
@@ -440,7 +429,7 @@ AcpiDbCreateExecutionThreads (
     Status = AcpiOsCreateSemaphore (1, 0, &ThreadGate);
     if (ACPI_FAILURE (Status))
     {
-        AcpiOsPrintf ("Could not create semaphore, %s\n", AcpiCmFormatException (Status));
+        AcpiOsPrintf ("Could not create semaphore, %s\n", AcpiUtFormatException (Status));
         return;
     }
 
