a99188fe19ce4ea8ed66a1d0d766b819c4154caf
aystarik <aystarik>
2005-06-29 16:58:52 +0000

    Comment and header updates
    date	2004.07.23.16.21.00;	author rmoore1;	state Exp;

--- source/components/hardware/hwtimer.c
+++ source/components/hardware/hwtimer.c
@@ -2,7 +2,7 @@
 /******************************************************************************
  *
  * Name: hwtimer.c - ACPI Power Management Timer Interface
- *              $Revision: 1.24 $
+ *              $Revision: 1.27 $
  *
  *****************************************************************************/
 
@@ -10,7 +10,7 @@
  *
  * 1. Copyright Notice
  *
- * Some or all of this work - Copyright (c) 1999 - 2003, Intel Corp.
+ * Some or all of this work - Copyright (c) 1999 - 2004, Intel Corp.
  * All rights reserved.
  *
  * 2. License
@@ -125,11 +125,11 @@
  *
  * FUNCTION:    AcpiGetTimerResolution
  *
- * PARAMETERS:  none
+ * PARAMETERS:  Resolution          - Where the resolution is returned
  *
- * RETURN:      Number of bits of resolution in the PM Timer (24 or 32).
+ * RETURN:      Status and timer resolution
  *
- * DESCRIPTION: Obtains resolution of the ACPI PM Timer.
+ * DESCRIPTION: Obtains resolution of the ACPI PM Timer (24 or 32 bits).
  *
  ******************************************************************************/
 
@@ -162,11 +162,11 @@ AcpiGetTimerResolution (
  *
  * FUNCTION:    AcpiGetTimer
  *
- * PARAMETERS:  none
+ * PARAMETERS:  Ticks               - Where the timer value is returned
  *
- * RETURN:      Current value of the ACPI PM Timer (in ticks).
+ * RETURN:      Status and current ticks
  *
- * DESCRIPTION: Obtains current value of ACPI PM Timer.
+ * DESCRIPTION: Obtains current value of ACPI PM Timer (in ticks).
  *
  ******************************************************************************/
 
@@ -185,7 +185,7 @@ AcpiGetTimer (
         return_ACPI_STATUS (AE_BAD_PARAMETER);
     }
 
-    Status = AcpiHwLowLevelRead (32, Ticks, &AcpiGbl_FADT->XPmTmrBlk, 0);
+    Status = AcpiHwLowLevelRead (32, Ticks, &AcpiGbl_FADT->XPmTmrBlk);
 
     return_ACPI_STATUS (Status);
 }
@@ -195,11 +195,11 @@ AcpiGetTimer (
  *
  * FUNCTION:    AcpiGetTimerDuration
  *
- * PARAMETERS:  StartTicks
- *              EndTicks
- *              TimeElapsed
+ * PARAMETERS:  StartTicks          - Starting timestamp
+ *              EndTicks            - End timestamp
+ *              TimeElapsed         - Where the elapsed time is returned
  *
- * RETURN:      TimeElapsed
+ * RETURN:      Status and TimeElapsed
  *
  * DESCRIPTION: Computes the time elapsed (in microseconds) between two
  *              PM Timer time stamps, taking into account the possibility of
@@ -213,7 +213,7 @@ AcpiGetTimer (
  *              Note that this function accommodates only a single timer
  *              rollover.  Thus for 24-bit timers, this function should only
  *              be used for calculating durations less than ~4.6 seconds
- *              (~20 minutes for 32-bit timers) -- calculations below
+ *              (~20 minutes for 32-bit timers) -- calculations below:
  *
  *              2**24 Ticks / 3,600,000 Ticks/Sec = 4.66 sec
  *              2**32 Ticks / 3,600,000 Ticks/Sec = 1193 sec or 19.88 minutes
@@ -242,7 +242,6 @@ AcpiGetTimerDuration (
 
     /*
      * Compute Tick Delta:
-     * -------------------
      * Handle (max one) timer rollovers on 24- versus 32-bit timers.
      */
     if (StartTicks < EndTicks)
@@ -271,10 +270,7 @@ AcpiGetTimerDuration (
     }
 
     /*
-     * Compute Duration:
-     * -----------------
-     *
-     * Requires a 64-bit divide:
+     * Compute Duration (Requires a 64-bit divide):
      *
      * TimeElapsed = (DeltaTicks * 1000000) / PM_TIMER_FREQUENCY;
      */
