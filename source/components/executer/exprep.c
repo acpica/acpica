3c1aef550b7ee531c71b4e70baceedf08a9c3eed
aystarik <aystarik>
2005-06-29 17:42:31 +0000

    typo
    date	2001.03.24.00.13.00;	author agrover;	state Exp;

--- source/components/executer/exprep.c
+++ source/components/executer/exprep.c
@@ -2,7 +2,7 @@
 /******************************************************************************
  *
  * Module Name: amprep - ACPI AML (p-code) execution - field prep utilities
- *              $Revision: 1.72 $
+ *              $Revision: 1.75 $
  *
  *****************************************************************************/
 
@@ -10,7 +10,7 @@
  *
  * 1. Copyright Notice
  *
- * Some or all of this work - Copyright (c) 1999, 2000, Intel Corp.
+ * Some or all of this work - Copyright (c) 1999, 2000, 2001, Intel Corp.
  * All rights reserved.
  *
  * 2. License
@@ -124,7 +124,7 @@
 #include "acparser.h"
 
 
-#define _COMPONENT          INTERPRETER
+#define _COMPONENT          ACPI_EXECUTER
         MODULE_NAME         ("amprep")
 
 
@@ -192,7 +192,7 @@ AcpiAmlDecodeFieldAccessType (
 
 /*******************************************************************************
  *
- * FUNCTION:    AcpiAmlPrepCommonFieldObjec
+ * FUNCTION:    AcpiAmlPrepCommonFieldObject
  *
  * PARAMETERS:  ObjDesc             - The field object
  *              FieldFlags          - Access, LockRule, or UpdateRule.
