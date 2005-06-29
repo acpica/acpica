c9342838b497073692a54d607e4874c43427db72
aystarik <aystarik>
2005-06-29 19:20:01 +0000

    Return Status, not AE_OK
    date	2004.03.12.22.41.00;	author rmoore1;	state Exp;

--- source/components/utilities/utmisc.c
+++ source/components/utilities/utmisc.c
@@ -1,7 +1,7 @@
 /*******************************************************************************
  *
  * Module Name: utmisc - common utility procedures
- *              $Revision: 1.98 $
+ *              $Revision: 1.100 $
  *
  ******************************************************************************/
 
@@ -9,7 +9,7 @@
  *
  * 1. Copyright Notice
  *
- * Some or all of this work - Copyright (c) 1999 - 2003, Intel Corp.
+ * Some or all of this work - Copyright (c) 1999 - 2004, Intel Corp.
  * All rights reserved.
  *
  * 2. License
@@ -636,6 +636,7 @@ AcpiUtStrupr (
     return (SrcString);
 }
 
+
 /*******************************************************************************
  *
  * FUNCTION:    AcpiUtMutexInitialize
@@ -671,10 +672,8 @@ AcpiUtMutexInitialize (
         }
     }
 
-
     Status = AcpiOsCreateLock (&AcpiGbl_GpeLock);
-
-    return_ACPI_STATUS (AE_OK);
+    return_ACPI_STATUS (Status);
 }
 
 
