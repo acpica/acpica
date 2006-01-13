bdde086503123a137e49d67aee5c5af0224dca6f
rmoore1 <rmoore1>
2006-01-13 20:53:28 +0000

    version

--- source/include/acconfig.h
+++ source/include/acconfig.h
@@ -1,7 +1,7 @@
 /******************************************************************************
  *
  * Name: acconfig.h - Global configuration constants
- *       $Revision: 1.202 $
+ *       $Revision: 1.203 $
  *
  *****************************************************************************/
 
@@ -137,7 +137,7 @@
 
 /* Current ACPICA subsystem version in YYYYMMDD format */
 
-#define ACPI_CA_VERSION                 0x20051216
+#define ACPI_CA_VERSION                 0x20060113
 
 /*
  * OS name, used for the _OS object.  The _OS object is essentially obsolete,
