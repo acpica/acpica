dad54de3c63766f7a1ce97e2e11c4317a36cf774
rmoore1 <rmoore1>
2006-01-26 21:59:33 +0000

    Added default definitions for two macros

--- source/include/actypes.h
+++ source/include/actypes.h
@@ -1,7 +1,7 @@
 /******************************************************************************
  *
  * Name: actypes.h - Common data types for the entire ACPI subsystem
- *       $Revision: 1.298 $
+ *       $Revision: 1.299 $
  *
  *****************************************************************************/
 
@@ -343,6 +343,24 @@ typedef char                            *ACPI_PHYSICAL_ADDRESS;
 #ifndef ACPI_CACHE_T
 #define ACPI_CACHE_T                    ACPI_MEMORY_LIST
 #endif
+
+/*
+ * ACPI_PRINTF_LIKE is used to tag functions as "printf-like" because
+ * some compilers can catch printf format string problems
+ */
+#ifndef ACPI_PRINTF_LIKE
+#define ACPI_PRINTF_LIKE(c)
+#endif
+
+/* Some compilers complain about unused variables. Sometimes we don't want to
+ * use all the variables (for example, _AcpiModuleName). This allows us
+ * to to tell the compiler in a per-variable manner that a variable
+ * is unused
+ */
+#ifndef ACPI_UNUSED_VAR
+#define ACPI_UNUSED_VAR
+#endif
+
 
 /* Variable-width type, used instead of clib size_t */
 
