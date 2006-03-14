bee24de101ff73620c4b609b99d9bfa61c0d7f0f
rmoore1 <rmoore1>
2006-03-14 20:55:32 +0000

    Remove NSnode reference count mechanism, not needed

--- source/components/namespace/nsalloc.c
+++ source/components/namespace/nsalloc.c
@@ -1,7 +1,7 @@
 /*******************************************************************************
  *
  * Module Name: nsalloc - Namespace allocation and deletion utilities
- *              $Revision: 1.102 $
+ *              $Revision: 1.103 $
  *
  ******************************************************************************/
 
@@ -124,12 +124,6 @@
 #define _COMPONENT          ACPI_NAMESPACE
         ACPI_MODULE_NAME    ("nsalloc")
 
-/* Local prototypes */
-
-static void
-AcpiNsRemoveReference (
-    ACPI_NAMESPACE_NODE     *Node);
-
 
 /*******************************************************************************
  *
@@ -161,10 +155,8 @@ AcpiNsCreateNode (
 
     ACPI_MEM_TRACKING (AcpiGbl_NsNodeList->TotalAllocated++);
 
-    Node->Name.Integer   = Name;
-    Node->ReferenceCount = 1;
+    Node->Name.Integer = Name;
     ACPI_SET_DESCRIPTOR_TYPE (Node, ACPI_DESC_TYPE_NAMED);
-
     return_PTR (Node);
 }
 
@@ -236,7 +228,7 @@ AcpiNsDeleteNode (
     ACPI_MEM_TRACKING (AcpiGbl_NsNodeList->TotalFreed++);
 
     /*
-     * Detach an object if there is one then delete the node
+     * Detach an object if there is one, then delete the node
      */
     AcpiNsDetachObject (Node);
     (void) AcpiOsReleaseObject (AcpiGbl_NamespaceCache, Node);
@@ -323,17 +315,6 @@ AcpiNsInstallNode (
         AcpiUtGetNodeName (Node), AcpiUtGetTypeName (Node->Type), Node, OwnerId,
         AcpiUtGetNodeName (ParentNode), AcpiUtGetTypeName (ParentNode->Type),
         ParentNode));
-
-    /*
-     * Increment the reference count(s) of all parents up to
-     * the root!
-     */
-    while ((Node = AcpiNsGetParentNode (Node)) != NULL)
-    {
-        Node->ReferenceCount++;
-    }
-
-    return_VOID;
 }
 
 
@@ -356,7 +337,6 @@ AcpiNsDeleteChildren (
 {
     ACPI_NAMESPACE_NODE     *ChildNode;
     ACPI_NAMESPACE_NODE     *NextNode;
-    ACPI_NAMESPACE_NODE     *Node;
     UINT8                   Flags;
 
 
@@ -383,8 +363,8 @@ AcpiNsDeleteChildren (
     {
         /* Get the things we need */
 
-        NextNode    = ChildNode->Peer;
-        Flags       = ChildNode->Flags;
+        NextNode = ChildNode->Peer;
+        Flags = ChildNode->Flags;
 
         /* Grandchildren should have all been deleted already */
 
@@ -406,28 +386,9 @@ AcpiNsDeleteChildren (
          */
         AcpiNsDetachObject (ChildNode);
 
-        /*
-         * Decrement the reference count(s) of all parents up to
-         * the root! (counts were incremented when the node was created)
-         */
-        Node = ChildNode;
-        while ((Node = AcpiNsGetParentNode (Node)) != NULL)
-        {
-            Node->ReferenceCount--;
-        }
-
-        /* There should be only one reference remaining on this node */
-
-        if (ChildNode->ReferenceCount != 1)
-        {
-            ACPI_WARNING ((AE_INFO,
-                "Existing references (%d) on node being deleted (%p)",
-                ChildNode->ReferenceCount, ChildNode));
-        }
-
         /* Now we can delete the node */
 
-        ACPI_FREE (ChildNode);
+        (void) AcpiOsReleaseObject (AcpiGbl_NamespaceCache, ChildNode);
 
         /* And move on to the next child in the list */
 
@@ -481,8 +442,7 @@ AcpiNsDeleteNamespaceSubtree (
     {
         /* Get the next node in this scope (NULL if none) */
 
-        ChildNode = AcpiNsGetNextNode (ACPI_TYPE_ANY, ParentNode,
-                                            ChildNode);
+        ChildNode = AcpiNsGetNextNode (ACPI_TYPE_ANY, ParentNode, ChildNode);
         if (ChildNode)
         {
             /* Found a child node - detach any attached object */
@@ -532,62 +492,6 @@ AcpiNsDeleteNamespaceSubtree (
 
 /*******************************************************************************
  *
- * FUNCTION:    AcpiNsRemoveReference
- *
- * PARAMETERS:  Node           - Named node whose reference count is to be
- *                               decremented
- *
- * RETURN:      None.
- *
- * DESCRIPTION: Remove a Node reference.  Decrements the reference count
- *              of all parent Nodes up to the root.  Any node along
- *              the way that reaches zero references is freed.
- *
- ******************************************************************************/
-
-static void
-AcpiNsRemoveReference (
-    ACPI_NAMESPACE_NODE     *Node)
-{
-    ACPI_NAMESPACE_NODE     *ParentNode;
-    ACPI_NAMESPACE_NODE     *ThisNode;
-
-
-    ACPI_FUNCTION_ENTRY ();
-
-
-    /*
-     * Decrement the reference count(s) of this node and all
-     * nodes up to the root,  Delete anything with zero remaining references.
-     */
-    ThisNode = Node;
-    while (ThisNode)
-    {
-        /* Prepare to move up to parent */
-
-        ParentNode = AcpiNsGetParentNode (ThisNode);
-
-        /* Decrement the reference count on this node */
-
-        ThisNode->ReferenceCount--;
-
-        /* Delete the node if no more references */
-
-        if (!ThisNode->ReferenceCount)
-        {
-            /* Delete all children and delete the node */
-
-            AcpiNsDeleteChildren (ThisNode);
-            AcpiNsDeleteNode (ThisNode);
-        }
-
-        ThisNode = ParentNode;
-    }
-}
-
-
-/*******************************************************************************
- *
  * FUNCTION:    AcpiNsDeleteNamespaceByOwner
  *
  * PARAMETERS:  OwnerId     - All nodes with this owner will be deleted
@@ -618,10 +522,10 @@ AcpiNsDeleteNamespaceByOwner (
         return_VOID;
     }
 
-    ParentNode    = AcpiGbl_RootNode;
-    ChildNode     = NULL;
-    DeletionNode  = NULL;
-    Level         = 1;
+    DeletionNode = NULL;
+    ParentNode = AcpiGbl_RootNode;
+    ChildNode = NULL;
+    Level = 1;
 
     /*
      * Traverse the tree of nodes until we bubble back up
@@ -637,7 +541,8 @@ AcpiNsDeleteNamespaceByOwner (
 
         if (DeletionNode)
         {
-            AcpiNsRemoveReference (DeletionNode);
+            AcpiNsDeleteChildren (DeletionNode);
+            AcpiNsDeleteNode (DeletionNode);
             DeletionNode = NULL;
         }
 
