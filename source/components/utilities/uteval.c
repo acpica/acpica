21771513c0d492481637cca7e1383ca593e28824
rmoore1 <rmoore1>
2007-03-26 22:10:34 +0000

    Update _OSI string list. Latest update for the Windows strings, with comments. Removed unused strings.

--- source/components/utilities/uteval.c
+++ source/components/utilities/uteval.c
@@ -1,7 +1,7 @@
 /******************************************************************************
  *
  * Module Name: uteval - Object evaluation
- *              $Revision: 1.71 $
+ *              $Revision: 1.72 $
  *
  *****************************************************************************/
 
@@ -146,16 +146,13 @@ static const char               *AcpiInterfacesSupported[] =
     /* Operating System Vendor Strings */
 
     "Linux",
-    "Windows 2000",
-    "Windows 2001",
-    "Windows 2001 SP0",
-    "Windows 2001 SP1",
-    "Windows 2001 SP2",
-    "Windows 2001 SP3",
-    "Windows 2001 SP4",
-    "Windows 2001.1",
-    "Windows 2001.1 SP1",   /* Added 03/2006 */
-    "Windows 2006",         /* Added 03/2006 */
+    "Windows 2000",         /* Windows 2000 */
+    "Windows 2001",         /* Windows XP */
+    "Windows 2001 SP1",     /* Windows XP SP1 */
+    "Windows 2001 SP2",     /* Windows XP SP2 */
+    "Windows 2001.1",       /* Windows Server 2003 */
+    "Windows 2001.1 SP1",   /* Windows Server 2003 SP1 - Added 03/2006 */
+    "Windows 2006",         /* Windows Vista - Added 03/2006 */
 
     /* Feature Group Strings */
 
