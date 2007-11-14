489bf8ed679419b77fef067713d7deac475ecdfe
rmoore1 <rmoore1>
2007-11-14 21:05:59 +0000

    Update version to 20071114. Update version to 20071114.

--- source/include/acconfig.h
+++ source/include/acconfig.h
@@ -1,7 +1,7 @@
 /******************************************************************************
  *
  * Name: acconfig.h - Global configuration constants
- *       $Revision: 1.237 $
+ *       $Revision: 1.238 $
  *
  *****************************************************************************/
 
@@ -137,7 +137,7 @@
 
 /* Current ACPICA subsystem version in YYYYMMDD format */
 
-#define ACPI_CA_VERSION                 0x20071019
+#define ACPI_CA_VERSION                 0x20071114
 
 /*
  * OS name, used for the _OS object.  The _OS object is essentially obsolete,
