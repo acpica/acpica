a41245aee8962907c3c354ede17061db97a0b4b2
rmoore1 <rmoore1>
2008-02-13 18:09:17 +0000

    Update ACPICA version to 20080213. Update ACPICA version to 20080213.

--- source/include/acconfig.h
+++ source/include/acconfig.h
@@ -1,7 +1,7 @@
 /******************************************************************************
  *
  * Name: acconfig.h - Global configuration constants
- *       $Revision: 1.241 $
+ *       $Revision: 1.242 $
  *
  *****************************************************************************/
 
@@ -137,7 +137,7 @@
 
 /* Current ACPICA subsystem version in YYYYMMDD format */
 
-#define ACPI_CA_VERSION                 0x20080123
+#define ACPI_CA_VERSION                 0x20080213
 
 /*
  * OS name, used for the _OS object.  The _OS object is essentially obsolete,
