3e5bc07346427ee92d021bf044bbab60057dd71d
Robert Moore <Robert.Moore@intel.com>
2008-08-29 12:06:44 -0700

    Update version to 20080829.
    Update version to 20080829.

--- source/include/acconfig.h
+++ source/include/acconfig.h
@@ -137,7 +137,7 @@
 
 /* Current ACPICA subsystem version in YYYYMMDD format */
 
-#define ACPI_CA_VERSION                 0x20080729
+#define ACPI_CA_VERSION                 0x20080829
 
 /*
  * OS name, used for the _OS object.  The _OS object is essentially obsolete,
