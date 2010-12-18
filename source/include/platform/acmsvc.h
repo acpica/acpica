bb624f47b8b2e1ac41f0b55c38a0cedbbee6e7a6
Robert Moore <Robert.Moore@intel.com>
2010-12-17 17:06:14 -0800

    Update msvc header for VC++ 9 (VS 2008)
    Add a couple new compiler-specific items to eliminate warnings.

--- source/include/platform/acmsvc.h
+++ source/include/platform/acmsvc.h
@@ -116,6 +116,18 @@
 #ifndef __ACMSVC_H__
 #define __ACMSVC_H__
 
+/* Eliminate warnings for "old" (non-secure) versions of clib functions */
+
+#ifndef _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+/* Eliminate warnings for POSIX clib function names (open, write, etc.) */
+
+#ifndef _CRT_NONSTDC_NO_DEPRECATE
+#define _CRT_NONSTDC_NO_DEPRECATE
+#endif
+
 #define COMPILER_DEPENDENT_INT64    __int64
 #define COMPILER_DEPENDENT_UINT64   unsigned __int64
 #define ACPI_INLINE                 __inline
@@ -179,5 +191,9 @@
 
 /* warn C4131: uses old-style declarator (iASL compiler only) */
 #pragma warning(disable:4131)
+
+#if _MSC_VER > 1200 /* Versions above VC++ 6 */
+#pragma warning( disable : 4295 ) /* needed for acpredef.h array */
+#endif
 
 #endif /* __ACMSVC_H__ */
