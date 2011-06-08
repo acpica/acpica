6f0bc401cac4f1a40cd7bdfe5dc7859c45bc0eab
Robert Moore <Robert.Moore@intel.com>
2011-06-08 15:00:21 -0700

    All makefiles: Cleanup use of command-line #defines.
    Common defines now all appear in acenv.h. Simplifies all makefiles.

--- source/include/platform/acenv.h
+++ source/include/platform/acenv.h
@@ -151,7 +151,7 @@
 #define ACPI_SINGLE_THREADED
 #endif
 
-/* AcpiExec and AcpiBin configuration */
+/* AcpiExec configuration. Multithreaded with full AML debugger */
 
 #ifdef ACPI_EXEC_APP
 #define ACPI_APPLICATION
@@ -160,7 +160,21 @@
 #define ACPI_DBG_TRACK_ALLOCATIONS
 #endif
 
-#ifdef ACPI_BIN_APP
+/* AcpiNames configuration. Single threaded with debugger output enabled. */
+
+#ifdef ACPI_NAMES_APP
+#define ACPI_DEBUGGER
+#define ACPI_APPLICATION
+#define ACPI_SINGLE_THREADED
+#endif
+
+/*
+ * AcpiBin/AcpiHelp/AcpiSrc configuration. All single threaded, with
+ * no debug output.
+ */
+#if (defined ACPI_BIN_APP)   || \
+    (defined ACPI_HELP_APP)  || \
+    (defined ACPI_SRC_APP)
 #define ACPI_APPLICATION
 #define ACPI_SINGLE_THREADED
 #endif
