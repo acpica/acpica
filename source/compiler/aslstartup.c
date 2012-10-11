9f0a65f188c06341b392237481369b4d37754ba9
Robert Moore <Robert.Moore@intel.com>
2012-10-11 14:27:14 -0700

    iASL: For -f option (force), allow non-ASCII characters in input.
    If -f is specified, allow an input file with non-ASCII characters.

--- source/compiler/aslstartup.c
+++ source/compiler/aslstartup.c
@@ -302,8 +302,12 @@ AslDetectSourceFileType (
     if (ACPI_FAILURE (Status))
     {
         printf ("Non-ascii input file - %s\n", Info->Filename);
-        Type = ASL_INPUT_TYPE_BINARY;
-        goto Cleanup;
+
+        if (!Gbl_IgnoreErrors)
+        {
+            Type = ASL_INPUT_TYPE_BINARY;
+            goto Cleanup;
+        }
     }
 
     /*
