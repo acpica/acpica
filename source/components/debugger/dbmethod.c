d26da516d7f00cf4e4f8bc6d3e25f14a1de17486
Robert Moore <Robert.Moore@intel.com>
2013-04-18 09:32:31 -0700

    Debugger: Use ACPI_STRLEN instead of just strlen().
    Required for ACPICA configurability.

--- source/components/debugger/dbmethod.c
+++ source/components/debugger/dbmethod.c
@@ -513,6 +513,7 @@ AcpiDbWalkForExecute (
     ACPI_DEVICE_INFO            *ObjInfo;
     ACPI_OBJECT_LIST            ParamObjects;
     ACPI_OBJECT                 Params[ACPI_METHOD_NUM_ARGS];
+    ACPI_OBJECT                 *ThisParam;
     ACPI_BUFFER                 ReturnObj;
     ACPI_STATUS                 Status;
     UINT16                      ArgTypeList;
@@ -563,30 +564,31 @@ AcpiDbWalkForExecute (
          * the actual method defines. If there is a difference, then the
          * method is wrong and a warning will be issued during execution.
          */
+        ThisParam = Params;
         for (i = 0; i < ArgCount; i++)
         {
             ArgType = METHOD_GET_NEXT_TYPE (ArgTypeList);
-            Params[i].Type = ArgType;
+            ThisParam->Type = ArgType;
 
             switch (ArgType)
             {
             case ACPI_TYPE_INTEGER:
-                Params[i].Integer.Value = 1;
+                ThisParam->Integer.Value = 1;
                 break;
 
             case ACPI_TYPE_STRING:
-                Params[i].String.Pointer = "This is the default argument string";
-                Params[i].String.Length = strlen (Params[i].String.Pointer);
+                ThisParam->String.Pointer = "This is the default argument string";
+                ThisParam->String.Length = ACPI_STRLEN (ThisParam->String.Pointer);
                 break;
 
             case ACPI_TYPE_BUFFER:
-                Params[i].Buffer.Pointer = (UINT8 *) Params; /* just a garbage buffer */
-                Params[i].Buffer.Length = 48;
+                ThisParam->Buffer.Pointer = (UINT8 *) Params; /* just a garbage buffer */
+                ThisParam->Buffer.Length = 48;
                 break;
 
              case ACPI_TYPE_PACKAGE:
-                Params[i].Package.Elements = NULL;
-                Params[i].Package.Count = 0;
+                ThisParam->Package.Elements = NULL;
+                ThisParam->Package.Count = 0;
                 break;
 
            default:
@@ -594,6 +596,8 @@ AcpiDbWalkForExecute (
                     Pathname, ArgType);
                 break;
             }
+
+            ThisParam++;
         }
 
         ParamObjects.Count = ArgCount;
