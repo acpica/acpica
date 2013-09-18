57bdfbee675cd2c0618c66882d68a6bdf9f8efc4
Robert Moore <Robert.Moore@intel.com>
2013-09-18 14:17:17 -0700

    Hardcode access width for the reset register.
    The ACPI spec requires the reset register width to be 8, so we
    now hardcode it and ignore the FADT value. This provides/maintains
    compatibility with other ACPI implementations that have allowed
    BIOS code with bad register width values to go unnoticed.
    Matthew Garett, Bob Moore, Lv Zheng.

--- source/components/hardware/hwxface.c
+++ source/components/hardware/hwxface.c
@@ -162,9 +162,15 @@ AcpiReset (
          * For I/O space, write directly to the OSL. This bypasses the port
          * validation mechanism, which may block a valid write to the reset
          * register.
+         *
+         * NOTE:
+         * The ACPI spec requires the reset register width to be 8, so we
+         * hardcode it here and ignore the FADT value. This maintains
+         * compatibility with other ACPI implementations that have allowed
+         * BIOS code with bad register width values to go unnoticed.
          */
         Status = AcpiOsWritePort ((ACPI_IO_ADDRESS) ResetReg->Address,
-                    AcpiGbl_FADT.ResetValue, ResetReg->BitWidth);
+            AcpiGbl_FADT.ResetValue, ACPI_RESET_REGISTER_WIDTH);
     }
     else
     {
--- source/include/actypes.h
+++ source/include/actypes.h
@@ -435,6 +435,7 @@ typedef UINT32                          ACPI_PHYSICAL_ADDRESS;
 #define ACPI_PM1_REGISTER_WIDTH         16
 #define ACPI_PM2_REGISTER_WIDTH         8
 #define ACPI_PM_TIMER_WIDTH             32
+#define ACPI_RESET_REGISTER_WIDTH       8
 
 /* Names within the namespace are 4 bytes long */
 
