24298543de2adf78e08c7a842c2fb95c70ca603d
Robert Moore <Robert.Moore@intel.com>
2013-09-19 07:44:29 -0700

    Update comment: no functional change.
    Adds a comment to clarify reason for using AcpiOsFree directly
    instead of ACPI_FREE.

--- source/components/namespace/nsxfeval.c
+++ source/components/namespace/nsxfeval.c
@@ -221,8 +221,12 @@ AcpiEvaluateObjectTyped (
 
     if (MustFree)
     {
-        /* Caller used ACPI_ALLOCATE_BUFFER, free the return buffer */
-
+        /*
+         * Caller used ACPI_ALLOCATE_BUFFER, free the return buffer.
+         * Note: We use AcpiOsFree here because AcpiOsAllocate was used
+         * to allocate the buffer. This purposefully bypasses the internal
+         * allocation tracking mechanism (if it is enabled).
+         */
         AcpiOsFree (ReturnBuffer->Pointer);
         ReturnBuffer->Pointer = NULL;
     }
