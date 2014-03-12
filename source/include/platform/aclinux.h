21ff7c3ef2a1bbd342ce08347b8f711e38bc7910
Robert Moore <Robert.Moore@intel.com>
2014-03-12 13:12:28 -0700

    Linux-specific header: Add support for PPC64 compilation.
    Adds PPC64 as a 64-bit architecture. Colin Ian King.

--- source/include/platform/aclinux.h
+++ source/include/platform/aclinux.h
@@ -171,7 +171,8 @@
 #define ACPI_FLUSH_CPU_CACHE()
 #define ACPI_CAST_PTHREAD_T(Pthread) ((ACPI_THREAD_ID) (Pthread))
 
-#if defined(__ia64__) || defined(__x86_64__) || defined(__aarch64__)
+#if defined(__ia64__)    || defined(__x86_64__) ||\
+    defined(__aarch64__) || defined(__PPC64__)
 #define ACPI_MACHINE_WIDTH          64
 #define COMPILER_DEPENDENT_INT64    long
 #define COMPILER_DEPENDENT_UINT64   unsigned long
