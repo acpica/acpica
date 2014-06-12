a97e1f769135bdcd31975255e862771a7cb13931
Lv Zheng <lv.zheng@intel.com>
2014-06-12 15:13:10 +0800

    Utilities: Cleanup DEFINE_ACPI_GLOBALS by moving AcpiUtInitGlobal() from utglobal.c to utinit.c.
    The utglobal.c is used to define and initialize global variables.  It makes
    sense if just adding utglobal.o to applications that are using such
    variables. But AcpiUtInitGlobals() is preventing us from doing so as this
    initialization function references other components' initializations code,
    which leads to the requirement that many files should also get linked if
    one wants to link utglobal.o.
    
    By eliminating the stubs for acpiexamples, acpinames, iasl, now it is
    possible to just move AcpiUtInitGlobal() to utinit.c for applications that
    require this function to link.
    
    By linking utglobal.o, we can stop defining DEFINE_ACPI_GLOBALS for
    applications (currently only acpidump is affected). Lv Zheng.
    
    Signed-off-by: Lv Zheng <lv.zheng@intel.com>

--- source/tools/acpidump/acpidump.h
+++ source/tools/acpidump/acpidump.h
@@ -120,7 +120,6 @@
 #ifdef _DECLARE_GLOBALS
 #define EXTERN
 #define INIT_GLOBAL(a,b)        a=b
-#define DEFINE_ACPI_GLOBALS     1
 #else
 #define EXTERN                  extern
 #define INIT_GLOBAL(a,b)        a
