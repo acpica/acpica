75dfbd86151f93df19f23c56ffc7f8f237fdeefc
Jung-uk Kim <jkim@FreeBSD.org>
2014-06-27 18:11:06 -0400

    Work around an ancient GCC bug.
    warning: cast from function call of type 'char *' to non-matching type 'long unsigned int'

--- source/components/utilities/utprint.c
+++ source/components/utilities/utprint.c
@@ -374,6 +374,7 @@ AcpiUtFormatNumber (
     INT32                   Precision,
     UINT8                   Type)
 {
+    char                    *Pos;
     char                    Sign;
     char                    Zero;
     BOOLEAN                 NeedPrefix;
@@ -431,9 +432,8 @@ AcpiUtFormatNumber (
 
     /* Generate full string in reverse order */
 
-    i = ACPI_PTR_DIFF (
-            AcpiUtPutNumber (ReversedString, Number, Base, Upper),
-            ReversedString);
+    Pos = AcpiUtPutNumber (ReversedString, Number, Base, Upper);
+    i = ACPI_PTR_DIFF (Pos, ReversedString);
 
     /* Printing 100 using %2d gives "100", not "00" */
 
