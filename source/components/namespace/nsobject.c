acpica/acpica

commit 694dea635401fda4aef71f9bd160f62d2e14d44e

Namespace: Properly null terminate objects detached from a namespace node
Fixes a bug exposed by commit 6af53b030395bb6bfa7a36812c41e9557a0bb003 where the
failure to null terminate a detached object led to the creation of a circular
linked list (and infinite looping) when the object is reattached.

Reported in acpica bugzilla #1063

Signed-off-by: David E. Box <david.e.box@linux.intel.com>

source/components/namespace/nsobject.c
@@ -337,6 +337,17 @@ AcpiNsDetachObject (
         }
     }
 
+    /*
+     * Detach the object from any data objects (which are still held by
+     * the namespace node)
+     */
+
+    if (ObjDesc->Common.NextObject &&
+       ((ObjDesc->Common.NextObject)->Common.Type == ACPI_TYPE_LOCAL_DATA))
+    {
+       ObjDesc->Common.NextObject = NULL;
+    }
+
     /* Reset the node type to untyped */
 
     Node->Type = ACPI_TYPE_ANY;

commit 7d2477f3b8be27a62e51d0fa8eab9d07ee84b648

Fix a regression for deletion of Alias() objects.
Fixes a regression introduced by commit 694dea6. In the case of
Alias namespace nodes, the node simply points to the aliased node via
the Object field; thus we cannot assume that the object is an
operand object.

Affects core ACPICA and the ACPICA tools including iASL and
acpiexec.

source/components/namespace/nsobject.c
@@ -335,17 +335,16 @@ AcpiNsDetachObject (
         {
             Node->Object = Node->Object->Common.NextObject;
         }
-    }
 
-    /*
-     * Detach the object from any data objects (which are still held by
-     * the namespace node)
-     */
-
-    if (ObjDesc->Common.NextObject &&
-       ((ObjDesc->Common.NextObject)->Common.Type == ACPI_TYPE_LOCAL_DATA))
-    {
-       ObjDesc->Common.NextObject = NULL;
+        /*
+         * Detach the object from any data objects (which are still held by
+         * the namespace node)
+         */
+        if (ObjDesc->Common.NextObject &&
+           ((ObjDesc->Common.NextObject)->Common.Type == ACPI_TYPE_LOCAL_DATA))
+        {
+           ObjDesc->Common.NextObject = NULL;
+        }
     }
 
     /* Reset the node type to untyped */