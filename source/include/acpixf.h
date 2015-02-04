e06b1624b02dc8317d144e9a6fe9d684c5fa2f90
David E. Box <david.e.box@linux.intel.com>
2015-02-04 12:38:00 -0800

    Update version to 20150204.
    Version 20150204.

--- source/include/acpixf.h
+++ source/include/acpixf.h
@@ -118,7 +118,7 @@
 
 /* Current ACPICA subsystem version in YYYYMMDD format */
 
-#define ACPI_CA_VERSION                 0x20141107
+#define ACPI_CA_VERSION                 0x20150204
 
 #include "acconfig.h"
 #include "actypes.h"
