poppoo/acpica-1

MSVC6: Update MSVC 6.0 project files
MSVC 6.0 project files are out of date. This patch updates these files to
enable MSVC 6.0 build environment. Lv Zheng.

Signed-off-by: Lv Zheng <lv.zheng@intel.com>

generate/msvc/AcpiHelp.dsp
@@ -1,24 +1,24 @@
-# Microsoft Developer Studio Project File - Name="AcpiXtract" - Package Owner=<4>
+# Microsoft Developer Studio Project File - Name="AcpiHelp" - Package Owner=<4>
 # Microsoft Developer Studio Generated Build File, Format Version 6.00
 # ** DO NOT EDIT **
 
 # TARGTYPE "Win32 (x86) Console Application" 0x0103
 
-CFG=AcpiXtract - Win32 Debug
+CFG=AcpiHelp - Win32 Debug
 !MESSAGE This is not a valid makefile. To build this project using NMAKE,
 !MESSAGE use the Export Makefile command and run
 !MESSAGE 
-!MESSAGE NMAKE /f "AcpiXtract.mak".
+!MESSAGE NMAKE /f "AcpiHelp.mak".
 !MESSAGE 
 !MESSAGE You can specify a configuration when running NMAKE
 !MESSAGE by defining the macro CFG on the command line. For example:
 !MESSAGE 
-!MESSAGE NMAKE /f "AcpiXtract.mak" CFG="AcpiXtract - Win32 Debug"
+!MESSAGE NMAKE /f "AcpiHelp.mak" CFG="AcpiHelp - Win32 Debug"
 !MESSAGE 
 !MESSAGE Possible choices for configuration are:
 !MESSAGE 
-!MESSAGE "AcpiXtract - Win32 Release" (based on "Win32 (x86) Console Application")
-!MESSAGE "AcpiXtract - Win32 Debug" (based on "Win32 (x86) Console Application")
+!MESSAGE "AcpiHelp - Win32 Release" (based on "Win32 (x86) Console Application")
+!MESSAGE "AcpiHelp - Win32 Debug" (based on "Win32 (x86) Console Application")
 !MESSAGE 
 
 # Begin Project
@@ -28,7 +28,7 @@ CFG=AcpiXtract - Win32 Debug
 CPP=cl.exe
 RSC=rc.exe
 
-!IF  "$(CFG)" == "AcpiXtract - Win32 Release"
+!IF  "$(CFG)" == "AcpiHelp - Win32 Release"
 
 # PROP BASE Use_MFC 0
 # PROP BASE Use_Debug_Libraries 0
@@ -37,12 +37,12 @@ RSC=rc.exe
 # PROP BASE Target_Dir ""
 # PROP Use_MFC 0
 # PROP Use_Debug_Libraries 0
-# PROP Output_Dir "AcpiXtract"
-# PROP Intermediate_Dir "AcpiXtract"
+# PROP Output_Dir "AcpiHelp"
+# PROP Intermediate_Dir "AcpiHelp"
 # PROP Ignore_Export_Lib 0
 # PROP Target_Dir ""
 # ADD BASE CPP /nologo /W3 /GX /O2 /D "WIN32" /D "NDEBUG" /D "_CONSOLE" /D "_MBCS" /YX /FD /c
-# ADD CPP /nologo /Gd /W4 /Gi /Ob1 /Gf /I "..\..\source\include" /D "NDEBUG" /D "WIN32" /D "_MBCS" /D "_CONSOLE" /D "__STDC__" /D "ACPI_XTRACT_APP" /FD /c
+# ADD CPP /nologo /Gd /W4 /Gi /Ob1 /Gf /I "..\..\source\include" /D "NDEBUG" /D "WIN32" /D "_MBCS" /D "_CONSOLE" /D "__STDC__" /D "ACPI_HELP_APP" /FD /c
 # SUBTRACT CPP /Fr /YX
 # ADD BASE RSC /l 0x409 /d "NDEBUG"
 # ADD RSC /l 0x409 /d "NDEBUG"
@@ -58,10 +58,10 @@ SOURCE="$(InputPath)"
 PreLink_Desc=Checking existence of acpica/libraries directory
 PreLink_Cmds=if NOT EXIST ..\..\libraries mkdir ..\..\libraries
 PostBuild_Desc=Copying acpixtract to libraries...
-PostBuild_Cmds=copy acpixtract\acpixtract.exe ..\..\libraries\acpixtract.exe
+PostBuild_Cmds=copy acpihelp\acpihelp.exe ..\..\libraries\acpihelp.exe
 # End Special Build Tool
 
-!ELSEIF  "$(CFG)" == "AcpiXtract - Win32 Debug"
+!ELSEIF  "$(CFG)" == "AcpiHelp - Win32 Debug"
 
 # PROP BASE Use_MFC 0
 # PROP BASE Use_Debug_Libraries 1
@@ -70,18 +70,18 @@ PostBuild_Cmds=copy acpixtract\acpixtract.exe ..\..\libraries\acpixtract.exe
 # PROP BASE Target_Dir ""
 # PROP Use_MFC 0
 # PROP Use_Debug_Libraries 1
-# PROP Output_Dir "AcpiXtractDebug"
-# PROP Intermediate_Dir "AcpiXtractDebug"
+# PROP Output_Dir "AcpiHelpDebug"
+# PROP Intermediate_Dir "AcpiHelpDebug"
 # PROP Ignore_Export_Lib 0
 # PROP Target_Dir ""
 # ADD BASE CPP /nologo /W3 /Gm /GX /ZI /Od /D "WIN32" /D "_DEBUG" /D "_CONSOLE" /D "_MBCS" /YX /FD /GZ /c
-# ADD CPP /nologo /Gd /W4 /Gm /Gi /Zi /Od /Ob1 /Gf /I "..\..\source\include" /D "_DEBUG" /D "WIN32" /D "_MBCS" /D "_CONSOLE" /D "__STDC__" /D "ACPI_XTRACT_APP" /FD /GZ /c
+# ADD CPP /nologo /Gd /W4 /Gm /Gi /Zi /Od /Ob1 /Gf /I "..\..\source\include" /D "_DEBUG" /D "WIN32" /D "_MBCS" /D "_CONSOLE" /D "__STDC__" /D "ACPI_HELP_APP" /FD /GZ /c
 # SUBTRACT CPP /Fr
 # ADD BASE RSC /l 0x409 /d "_DEBUG"
 # ADD RSC /l 0x409 /d "_DEBUG"
 BSC32=bscmake.exe
 # ADD BASE BSC32 /nologo
-# ADD BSC32 /nologo /o"/acpica/generate/msvc/AcpiXtractDebug/AcpiXtract.bsc"
+# ADD BSC32 /nologo /o"/acpica/generate/msvc/AcpiHelpDebug/AcpiHelp.bsc"
 LINK32=link.exe
 # ADD BASE LINK32 kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /nologo /subsystem:console /debug /machine:I386 /pdbtype:sept
 # ADD LINK32 kernel32.lib advapi32.lib /nologo /subsystem:console /incremental:no /map /debug /machine:I386 /pdbtype:sept
@@ -91,44 +91,57 @@ SOURCE="$(InputPath)"
 PreLink_Desc=Checking existence of acpica/libraries directory
 PreLink_Cmds=if NOT EXIST ..\..\libraries mkdir ..\..\libraries
 PostBuild_Desc=Copying acpixtract to libraries...
-PostBuild_Cmds=copy acpixtractdebug\acpixtract.exe ..\..\libraries\acpixtract_dbg.exe
+PostBuild_Cmds=copy acpihelpdebug\acpihelp.exe ..\..\libraries\acpihelp_dbg.exe
 # End Special Build Tool
 
 !ENDIF 
 
 # Begin Target
 
-# Name "AcpiXtract - Win32 Release"
-# Name "AcpiXtract - Win32 Debug"
+# Name "AcpiHelp - Win32 Release"
+# Name "AcpiHelp - Win32 Debug"
 # Begin Group "Source Files"
 
 # PROP Default_Filter "cpp;c;cxx;rc;def;r;odl;idl;hpj;bat"
-# Begin Group "AcpiXtract"
+# Begin Group "AcpiHelp"
 
 # PROP Default_Filter ""
 # Begin Source File
 
-SOURCE=..\..\source\tools\AcpiXtract\acpixtract.c
-
-!IF  "$(CFG)" == "AcpiXtract - Win32 Release"
-
-# ADD CPP /W4 /FR
-
-!ELSEIF  "$(CFG)" == "AcpiXtract - Win32 Debug"
-
-# ADD CPP /W4
-
-!ENDIF 
-
+SOURCE=..\..\source\tools\acpihelp\acpihelp.h
 # End Source File
 # Begin Source File
 
-SOURCE=..\..\source\tools\acpixtract\axmain.c
+SOURCE=..\..\source\tools\acpihelp\ahamlops.c
+# End Source File
+# Begin Source File
+
+SOURCE=..\..\source\tools\acpihelp\ahaslkey.c
+# End Source File
+# Begin Source File
+
+SOURCE=..\..\source\tools\acpihelp\ahaslops.c
+# End Source File
+# Begin Source File
+
+SOURCE=..\..\source\tools\acpihelp\ahdecode.c
+# End Source File
+# Begin Source File
+
+SOURCE=..\..\source\tools\acpihelp\ahmain.c
 # End Source File
 # End Group
 # Begin Group "Common"
 
 # PROP Default_Filter ""
+# Begin Source File
+
+SOURCE=..\..\source\common\ahids.c
+# End Source File
+# Begin Source File
+
+SOURCE=..\..\source\common\ahpredef.c
+# End Source File
 # Begin Source File
 
 SOURCE=..\..\source\common\getopt.c
@@ -163,11 +176,11 @@ SOURCE=..\..\source\components\utilities\utmath.c
 # End Source File
 # Begin Source File
 
-SOURCE=..\..\source\components\utilities\utprint.c
+SOURCE=..\..\source\components\utilities\utpredef.c
 # End Source File
 # Begin Source File
 
-SOURCE=..\..\source\components\utilities\utxferror.c
+SOURCE=..\..\source\components\utilities\utprint.c
 # End Source File
 # End Group
 # End Group

acpica/acpica

generate/msvc/AcpiHelp.dsp
@@ -1,24 +1,24 @@
-# Microsoft Developer Studio Project File - Name="AcpiXtract" - Package Owner=<4>
+# Microsoft Developer Studio Project File - Name="AcpiHelp" - Package Owner=<4>
 # Microsoft Developer Studio Generated Build File, Format Version 6.00
 # ** DO NOT EDIT **
 
 # TARGTYPE "Win32 (x86) Console Application" 0x0103
 
-CFG=AcpiXtract - Win32 Debug
+CFG=AcpiHelp - Win32 Debug
 !MESSAGE This is not a valid makefile. To build this project using NMAKE,
 !MESSAGE use the Export Makefile command and run
 !MESSAGE 
-!MESSAGE NMAKE /f "AcpiXtract.mak".
+!MESSAGE NMAKE /f "AcpiHelp.mak".
 !MESSAGE 
 !MESSAGE You can specify a configuration when running NMAKE
 !MESSAGE by defining the macro CFG on the command line. For example:
 !MESSAGE 
-!MESSAGE NMAKE /f "AcpiXtract.mak" CFG="AcpiXtract - Win32 Debug"
+!MESSAGE NMAKE /f "AcpiHelp.mak" CFG="AcpiHelp - Win32 Debug"
 !MESSAGE 
 !MESSAGE Possible choices for configuration are:
 !MESSAGE 
-!MESSAGE "AcpiXtract - Win32 Release" (based on "Win32 (x86) Console Application")
-!MESSAGE "AcpiXtract - Win32 Debug" (based on "Win32 (x86) Console Application")
+!MESSAGE "AcpiHelp - Win32 Release" (based on "Win32 (x86) Console Application")
+!MESSAGE "AcpiHelp - Win32 Debug" (based on "Win32 (x86) Console Application")
 !MESSAGE 
 
 # Begin Project
@@ -28,7 +28,7 @@ CFG=AcpiXtract - Win32 Debug
 CPP=cl.exe
 RSC=rc.exe
 
-!IF  "$(CFG)" == "AcpiXtract - Win32 Release"
+!IF  "$(CFG)" == "AcpiHelp - Win32 Release"
 
 # PROP BASE Use_MFC 0
 # PROP BASE Use_Debug_Libraries 0
@@ -37,12 +37,12 @@ RSC=rc.exe
 # PROP BASE Target_Dir ""
 # PROP Use_MFC 0
 # PROP Use_Debug_Libraries 0
-# PROP Output_Dir "AcpiXtract"
-# PROP Intermediate_Dir "AcpiXtract"
+# PROP Output_Dir "AcpiHelp"
+# PROP Intermediate_Dir "AcpiHelp"
 # PROP Ignore_Export_Lib 0
 # PROP Target_Dir ""
 # ADD BASE CPP /nologo /W3 /GX /O2 /D "WIN32" /D "NDEBUG" /D "_CONSOLE" /D "_MBCS" /YX /FD /c
-# ADD CPP /nologo /W3 /Gi /Ob1 /Gf /I "..\..\source\include" /D "NDEBUG" /D "WIN32" /D "_MBCS" /D "_CONSOLE" /D "__STDC__" /D "ACPI_XTRACT_APP" /FD /c
+# ADD CPP /nologo /W3 /Gi /Ob1 /Gf /I "..\..\source\include" /D "NDEBUG" /D "WIN32" /D "_MBCS" /D "_CONSOLE" /D "__STDC__" /D "ACPI_HELP_APP" /FD /c
 # SUBTRACT CPP /Fr /YX
 # ADD BASE RSC /l 0x409 /d "NDEBUG"
 # ADD RSC /l 0x409 /d "NDEBUG"
@@ -58,10 +58,10 @@ SOURCE="$(InputPath)"
 PreLink_Desc=Checking existence of acpica/libraries directory
 PreLink_Cmds=if NOT EXIST ..\..\libraries mkdir ..\..\libraries
 PostBuild_Desc=Copying acpixtract to libraries...
-PostBuild_Cmds=copy acpixtract\acpixtract.exe ..\..\libraries\acpixtract.exe
+PostBuild_Cmds=copy acpihelp\acpihelp.exe ..\..\libraries\acpihelp.exe
 # End Special Build Tool
 
-!ELSEIF  "$(CFG)" == "AcpiXtract - Win32 Debug"
+!ELSEIF  "$(CFG)" == "AcpiHelp - Win32 Debug"
 
 # PROP BASE Use_MFC 0
 # PROP BASE Use_Debug_Libraries 1
@@ -70,18 +70,18 @@ PostBuild_Cmds=copy acpixtract\acpixtract.exe ..\..\libraries\acpixtract.exe
 # PROP BASE Target_Dir ""
 # PROP Use_MFC 0
 # PROP Use_Debug_Libraries 1
-# PROP Output_Dir "AcpiXtractDebug"
-# PROP Intermediate_Dir "AcpiXtractDebug"
+# PROP Output_Dir "AcpiHelpDebug"
+# PROP Intermediate_Dir "AcpiHelpDebug"
 # PROP Ignore_Export_Lib 0
 # PROP Target_Dir ""
 # ADD BASE CPP /nologo /W3 /Gm /GX /ZI /Od /D "WIN32" /D "_DEBUG" /D "_CONSOLE" /D "_MBCS" /YX /FD /GZ /c
-# ADD CPP /nologo /W3 /Gm /Gi /Zi /Od /Ob1 /Gf /I "..\..\source\include" /D "_DEBUG" /D "WIN32" /D "_MBCS" /D "_CONSOLE" /D "__STDC__" /D "ACPI_XTRACT_APP" /FD /GZ /c
+# ADD CPP /nologo /W3 /Gm /Gi /Zi /Od /Ob1 /Gf /I "..\..\source\include" /D "_DEBUG" /D "WIN32" /D "_MBCS" /D "_CONSOLE" /D "__STDC__" /D "ACPI_HELP_APP" /FD /GZ /c
 # SUBTRACT CPP /Fr
 # ADD BASE RSC /l 0x409 /d "_DEBUG"
 # ADD RSC /l 0x409 /d "_DEBUG"
 BSC32=bscmake.exe
 # ADD BASE BSC32 /nologo
-# ADD BSC32 /nologo /o"/acpica/generate/msvc/AcpiXtractDebug/AcpiXtract.bsc"
+# ADD BSC32 /nologo /o"/acpica/generate/msvc/AcpiHelpDebug/AcpiHelp.bsc"
 LINK32=link.exe
 # ADD BASE LINK32 kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /nologo /subsystem:console /debug /machine:I386 /pdbtype:sept
 # ADD LINK32 kernel32.lib advapi32.lib /nologo /subsystem:console /incremental:no /map /debug /machine:I386 /pdbtype:sept
@@ -91,44 +91,61 @@ SOURCE="$(InputPath)"
 PreLink_Desc=Checking existence of acpica/libraries directory
 PreLink_Cmds=if NOT EXIST ..\..\libraries mkdir ..\..\libraries
 PostBuild_Desc=Copying acpixtract to libraries...
-PostBuild_Cmds=copy acpixtractdebug\acpixtract.exe ..\..\libraries\acpixtract_dbg.exe
+PostBuild_Cmds=copy acpihelpdebug\acpihelp.exe ..\..\libraries\acpihelp_dbg.exe
 # End Special Build Tool
 
 !ENDIF 
 
 # Begin Target
 
-# Name "AcpiXtract - Win32 Release"
-# Name "AcpiXtract - Win32 Debug"
+# Name "AcpiHelp - Win32 Release"
+# Name "AcpiHelp - Win32 Debug"
 # Begin Group "Source Files"
 
 # PROP Default_Filter "cpp;c;cxx;rc;def;r;odl;idl;hpj;bat"
-# Begin Group "AcpiXtract"
+# Begin Group "AcpiHelp"
 
 # PROP Default_Filter ""
 # Begin Source File
 
-SOURCE=..\..\source\tools\AcpiXtract\acpixtract.c
-
-!IF  "$(CFG)" == "AcpiXtract - Win32 Release"
-
-# ADD CPP /W4 /FR
-
-!ELSEIF  "$(CFG)" == "AcpiXtract - Win32 Debug"
-
-# ADD CPP /W4
-
-!ENDIF 
-
+SOURCE=..\..\source\tools\acpihelp\acpihelp.h
 # End Source File
 # Begin Source File
 
-SOURCE=..\..\source\tools\acpixtract\axmain.c
+SOURCE=..\..\source\tools\acpihelp\ahamlops.c
+# End Source File
+# Begin Source File
+
+SOURCE=..\..\source\tools\acpihelp\ahaslkey.c
+# End Source File
+# Begin Source File
+
+SOURCE=..\..\source\tools\acpihelp\ahaslops.c
+# End Source File
+# Begin Source File
+
+SOURCE=..\..\source\tools\acpihelp\ahdecode.c
+# End Source File
+# Begin Source File
+
+SOURCE=..\..\source\tools\acpihelp\ahmain.c
 # End Source File
 # End Group
 # Begin Group "Common"
 
 # PROP Default_Filter ""
+# Begin Source File
+
+SOURCE=..\..\source\common\ahids.c
+# End Source File
+# Begin Source File
+
+SOURCE=..\..\source\common\ahpredef.c
+# End Source File
+# Begin Source File
+
+SOURCE=..\..\source\common\ahuuids.c
+# End Source File
 # Begin Source File
 
 SOURCE=..\..\source\common\getopt.c
@@ -159,7 +176,15 @@ SOURCE=..\..\source\components\utilities\utglobal.c
 # End Source File
 # Begin Source File
 
+SOURCE=..\..\source\components\utilities\uthex.c
+# End Source File
+# Begin Source File
+
 SOURCE=..\..\source\components\utilities\utmath.c
+# End Source File
+# Begin Source File
+
+SOURCE=..\..\source\components\utilities\utpredef.c
 # End Source File
 # Begin Source File
 
@@ -167,7 +192,7 @@ SOURCE=..\..\source\components\utilities\utprint.c
 # End Source File
 # Begin Source File
 
-SOURCE=..\..\source\components\utilities\utxferror.c
+SOURCE=..\..\source\components\utilities\utuuid.c
 # End Source File
 # End Group
 # End Group