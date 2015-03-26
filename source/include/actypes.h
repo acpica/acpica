b293f602a67da478ae0bec129e68bd99787d9908
Robert Moore <Robert.Moore@intel.com>
2015-03-26 13:52:40 -0700

    Add "Windows 2015" string to _OSI support.
    This change adds this string for Windows 10.

--- source/include/actypes.h
+++ source/include/actypes.h
@@ -1439,6 +1439,7 @@ typedef struct acpi_memory_list
 #define ACPI_OSI_WIN_VISTA_SP2          0x0A
 #define ACPI_OSI_WIN_7                  0x0B
 #define ACPI_OSI_WIN_8                  0x0C
+#define ACPI_OSI_WIN_10                 0x0D
 
 
 /* Definitions of file IO */
