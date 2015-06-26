49c6a6517a906900e9baa51ad5859beeb8a3089f
Lv Zheng <lv.zheng@intel.com>
2015-06-26 14:01:36 +0800

    MSVC: Fix inclusion order issue of <crtdbg.h>.
    The following error logs can be seen for calloc/free/malloc/realloc that
    defined in the stdlib.h:
    ...\stdlib.h(281) : error C2059: syntax error : ','
    ...\stdlib.h(281) : error C2143: syntax error : missing ')' before 'constant'
    ...\stdlib.h(281) : error C2143: syntax error : missing '{' before 'constant'
    ...\stdlib.h(281) : error C2059: syntax error : '<Unknown>'
    ...\stdlib.h(281) : error C2059: syntax error : ')'
    
    This is caused by the wrong inclusion order of stdlib.h/crtdbg.h introduced
    in acenv.h. This patch fixes this breakage. Lv Zheng.
    
    Signed-off-by: Lv Zheng <lv.zheng@intel.com>

--- source/include/platform/acmsvcex.h
+++ source/include/platform/acmsvcex.h
@@ -1,6 +1,6 @@
 /******************************************************************************
  *
- * Name: acmsvc.h - VC specific defines, etc.
+ * Name: acmsvcex.h - Extra VC specific defines, etc.
  *
  *****************************************************************************/
 
@@ -113,149 +113,14 @@
  *
  *****************************************************************************/
 
-#ifndef __ACMSVC_H__
-#define __ACMSVC_H__
-
-/* Note: do not include any C library headers here */
-
-/*
- * Map low I/O functions for MS. This allows us to disable MS language
- * extensions for maximum portability.
- */
-#define open            _open
-#define read            _read
-#define write           _write
-#define close           _close
-#define stat            _stat
-#define fstat           _fstat
-#define mkdir           _mkdir
-#define strlwr          _strlwr
-#define O_RDONLY        _O_RDONLY
-#define O_BINARY        _O_BINARY
-#define O_CREAT         _O_CREAT
-#define O_WRONLY        _O_WRONLY
-#define O_TRUNC         _O_TRUNC
-#define S_IREAD         _S_IREAD
-#define S_IWRITE        _S_IWRITE
-#define S_IFDIR         _S_IFDIR
-
-/* Eliminate warnings for "old" (non-secure) versions of clib functions */
-
-#ifndef _CRT_SECURE_NO_WARNINGS
-#define _CRT_SECURE_NO_WARNINGS
-#endif
-
-/* Eliminate warnings for POSIX clib function names (open, write, etc.) */
-
-#ifndef _CRT_NONSTDC_NO_DEPRECATE
-#define _CRT_NONSTDC_NO_DEPRECATE
-#endif
-
-#define COMPILER_DEPENDENT_INT64    __int64
-#define COMPILER_DEPENDENT_UINT64   unsigned __int64
-#define ACPI_INLINE                 __inline
-
-/*
- * Calling conventions:
- *
- * ACPI_SYSTEM_XFACE        - Interfaces to host OS (handlers, threads)
- * ACPI_EXTERNAL_XFACE      - External ACPI interfaces
- * ACPI_INTERNAL_XFACE      - Internal ACPI interfaces
- * ACPI_INTERNAL_VAR_XFACE  - Internal variable-parameter list interfaces
- */
-#define ACPI_SYSTEM_XFACE           __cdecl
-#define ACPI_EXTERNAL_XFACE
-#define ACPI_INTERNAL_XFACE
-#define ACPI_INTERNAL_VAR_XFACE     __cdecl
-
-#ifndef _LINT
-/*
- * Math helper functions
- */
-#define ACPI_DIV_64_BY_32(n_hi, n_lo, d32, q32, r32) \
-{                           \
-    __asm mov    edx, n_hi  \
-    __asm mov    eax, n_lo  \
-    __asm div    d32        \
-    __asm mov    q32, eax   \
-    __asm mov    r32, edx   \
-}
-
-#define ACPI_SHIFT_RIGHT_64(n_hi, n_lo) \
-{                           \
-    __asm shr    n_hi, 1    \
-    __asm rcr    n_lo, 1    \
-}
-#else
-
-/* Fake versions to make lint happy */
-
-#define ACPI_DIV_64_BY_32(n_hi, n_lo, d32, q32, r32) \
-{                           \
-    q32 = n_hi / d32;       \
-    r32 = n_lo / d32;       \
-}
-
-#define ACPI_SHIFT_RIGHT_64(n_hi, n_lo) \
-{                           \
-    n_hi >>= 1;    \
-    n_lo >>= 1;    \
-}
-#endif
-
-/* warn C4100: unreferenced formal parameter */
-#pragma warning(disable:4100)
-
-/* warn C4127: conditional expression is constant */
-#pragma warning(disable:4127)
-
-/* warn C4706: assignment within conditional expression */
-#pragma warning(disable:4706)
-
-/* warn C4131: uses old-style declarator (iASL compiler only) */
-#pragma warning(disable:4131)
-
-#if _MSC_VER > 1200 /* Versions above VC++ 6 */
-#pragma warning( disable : 4295 ) /* needed for acpredef.h array */
-#endif
-
+#ifndef __ACMSVCEX_H__
+#define __ACMSVCEX_H__
 
 /* Debug support. */
 
 #ifdef _DEBUG
-
-/*
- * Debugging memory corruption issues with windows:
- * Add #include <crtdbg.h> to accommon.h if necessary.
- * Add _ASSERTE(_CrtCheckMemory()); where needed to test memory integrity.
- * This can quickly localize the memory corruption.
- */
-#define ACPI_DEBUG_INITIALIZE() \
-    _CrtSetDbgFlag (\
-        _CRTDBG_CHECK_ALWAYS_DF | \
-        _CRTDBG_ALLOC_MEM_DF | \
-        _CRTDBG_DELAY_FREE_MEM_DF | \
-        _CRTDBG_LEAK_CHECK_DF | \
-        _CrtSetDbgFlag(_CRTDBG_REPORT_FLAG));
-
-#if 0
-/*
- * _CrtSetBreakAlloc can be used to set a breakpoint at a particular
- * memory leak, add to the macro above.
- */
-Detected memory leaks!
-Dumping objects ->
-..\..\source\os_specific\service_layers\oswinxf.c(701) : {937} normal block at 0x002E9190, 40 bytes long.
- Data: <                > 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
-
-_CrtSetBreakAlloc (937);
+#define _CRTDBG_MAP_ALLOC /* Enables specific file/lineno for leaks */
+#include <crtdbg.h>
 #endif
 
-#endif
-
-#if _MSC_VER > 1200 /* Versions above VC++ 6 */
-#define COMPILER_VA_MACRO               1
-#else
-#endif
-
-#endif /* __ACMSVC_H__ */
+#endif /* __ACMSVCEX_H__ */
