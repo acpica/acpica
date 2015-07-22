6d9c827b540837b6e54059e17756a06985e4a196
Robert Moore <Robert.Moore@intel.com>
2015-07-22 12:14:03 -0700

    Disassembler: Remove duplicate code in _PLD processing.
    ACPICA BZ 1176.

--- source/components/utilities/utxface.c
+++ source/components/utilities/utxface.c
@@ -676,7 +676,7 @@ AcpiDecodePldBuffer (
 
     /* Parameter validation */
 
-    if (!InBuffer || !ReturnBuffer || (Length < 16))
+    if (!InBuffer || !ReturnBuffer || (Length < ACPI_PLD_REV1_BUFFER_SIZE))
     {
         return (AE_BAD_PARAMETER);
     }
@@ -728,7 +728,7 @@ AcpiDecodePldBuffer (
     PldInfo->Rotation =             ACPI_PLD_GET_ROTATION (&Dword);
     PldInfo->Order =                ACPI_PLD_GET_ORDER (&Dword);
 
-    if (Length >= ACPI_PLD_BUFFER_SIZE)
+    if (Length >= ACPI_PLD_REV2_BUFFER_SIZE)
     {
         /* Fifth 32-bit DWord (Revision 2 of _PLD) */
 
