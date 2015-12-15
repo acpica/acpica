5923a9be36b8cb1b62afa89fe217783c04c92ad3
Robert Moore <Robert.Moore@intel.com>
2015-12-15 10:44:33 -0800

    Update internal #defines for ObjectType operator. No functional change
    To improve readability. Rename all *_TYPE_* defines related to
    ObjectType to *_OBJECT_TYPE_*

--- source/include/amlcode.h
+++ source/include/amlcode.h
@@ -192,7 +192,7 @@
 #define AML_CREATE_WORD_FIELD_OP    (UINT16) 0x8b
 #define AML_CREATE_BYTE_FIELD_OP    (UINT16) 0x8c
 #define AML_CREATE_BIT_FIELD_OP     (UINT16) 0x8d
-#define AML_TYPE_OP                 (UINT16) 0x8e
+#define AML_OBJECT_TYPE_OP          (UINT16) 0x8e
 #define AML_CREATE_QWORD_FIELD_OP   (UINT16) 0x8f     /* ACPI 2.0 */
 #define AML_LAND_OP                 (UINT16) 0x90
 #define AML_LOR_OP                  (UINT16) 0x91
