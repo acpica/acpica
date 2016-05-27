49a4dd1c54cc1195462520f691fd9b111f1f366f
Robert Moore <Robert.Moore@intel.com>
2016-05-27 09:47:56 -0700

    Revert "Hardware: Fix a regression caused by bit width mask generation"
    This reverts commit c23034a3a09d5ed79f1827d51f43cfbccf68ab64.
    
    Revert until further testing/validation.

--- source/components/hardware/hwregs.c
+++ source/components/hardware/hwregs.c
@@ -315,7 +315,7 @@ AcpiHwRead (
         }
 
         ACPI_SET_BITS (Value, Index * AccessWidth,
-            ACPI_MASK_BITS_ABOVE_32 (AccessWidth), Value32);
+            (1 << AccessWidth) - 1, Value32);
 
         BitWidth -= BitWidth > AccessWidth ? AccessWidth : BitWidth;
         Index++;
@@ -386,7 +386,7 @@ AcpiHwWrite (
     while (BitWidth)
     {
         NewValue32 = ACPI_GET_BITS (&Value, Index * AccessWidth,
-            ACPI_MASK_BITS_ABOVE_32 (AccessWidth));
+            (1 << AccessWidth) - 1);
 
         if (BitOffset > AccessWidth)
         {
