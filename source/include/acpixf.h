3c8c04c2e8a371018b3a29f5cfe27a241caea48d
Robert Moore <Robert.Moore@intel.com>
2016-08-31 09:27:28 -0700

    Update version to 20160831
    Version 20160831

--- source/include/acpixf.h
+++ source/include/acpixf.h
@@ -118,7 +118,7 @@
 
 /* Current ACPICA subsystem version in YYYYMMDD format */
 
-#define ACPI_CA_VERSION                 0x20160729
+#define ACPI_CA_VERSION                 0x20160831
 
 #include "acconfig.h"
 #include "actypes.h"
