2529fa2f568b878fbd1ba28cc68d89ea1afbf40e
Lv Zheng <lv.zheng@intel.com>
2017-01-23 14:22:56 +0800

    Events: Add a new initialization flag to bypass SystemMemory/SystemIo operation region initialization
    According to the spec and the de-facto standard behavior, it is not
    required to execute _REG(SystemMemory, CONNECT) and
    _REG(SystemIo, CONNECT) while it is required to execute
    _REG(PCI_Config, CONNECT) for the operation regions under a non PCI root
    bridge device node.
    This patch adds an additional operation region initialization flag to make
    it possible for OSPMs to bypass SystemIo/SystemMemory _REG evaluations. The
    AcpiEvHasDefaultHandler() is removed accordingly because we actually should
    always execute _REG at this point whatever the handler is installed by
    ACPICA or an external driver and when the handler is installed, _REG
    executions are blocked even though it is a handler from an external driver.
    
    Note that this patch stops evaluating _REG(DataTable, CONNECT), because
    this operation region is not defined by the ACPI specification, but designed
    by ACPICA itself for internal testing purpose and there is no external
    dependencies required for accessing it. Lv Zheng.
    
    Link: https://bugzilla.kernel.org/show_bug.cgi?id=102421
    Signed-off-by: Lv Zheng <lv.zheng@intel.com>

--- source/include/acevents.h
+++ source/include/acevents.h
@@ -304,11 +304,6 @@ AcpiEvFindRegionHandler (
     ACPI_ADR_SPACE_TYPE     SpaceId,
     ACPI_OPERAND_OBJECT     *HandlerObj);
 
-BOOLEAN
-AcpiEvHasDefaultHandler (
-    ACPI_NAMESPACE_NODE     *Node,
-    ACPI_ADR_SPACE_TYPE     SpaceId);
-
 ACPI_STATUS
 AcpiEvInstallRegionHandlers (
     void);
@@ -327,7 +322,7 @@ AcpiEvInstallSpaceHandler (
  */
 ACPI_STATUS
 AcpiEvInitializeOpRegions (
-    void);
+    UINT32                  Flags);
 
 ACPI_STATUS
 AcpiEvAddressSpaceDispatch (
