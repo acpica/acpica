f14e1c69d7894323912f2a0798d0d4e9472bea34
Erik Schmauss <erik.schmauss@intel.com>
2017-02-27 12:48:41 -0800

    Converter: getting rid of useless if statement
    Signed-off-by: Erik Schmauss <erik.schmauss@intel.com>

--- source/compiler/asltree.c
+++ source/compiler/asltree.c
@@ -295,17 +295,14 @@ TrAllocateNode (
         }
 
         Gbl_CommentState.Latest_Parse_Node = Op;
-        if (Gbl_CommentState.Latest_Parse_Node->Asl.ParseOpName)
-        {
-            CvDbgPrint ("trallocatenode=Set latest parse node to this node.\n");
-            CvDbgPrint ("           Op->Asl.ParseOpName = %s\n",
-                Gbl_CommentState.Latest_Parse_Node->Asl.ParseOpName);
-            CvDbgPrint ("           Op->Asl.ParseOpcode = 0x%x\n", ParseOpcode);
+        CvDbgPrint ("trallocatenode=Set latest parse node to this node.\n");
+        CvDbgPrint ("           Op->Asl.ParseOpName = %s\n",
+            Gbl_CommentState.Latest_Parse_Node->Asl.ParseOpName);
+        CvDbgPrint ("           Op->Asl.ParseOpcode = 0x%x\n", ParseOpcode);
 
-            if (Op->Asl.FileChanged)
-            {
-                CvDbgPrint("    file has been changed!\n");
-            }
+        if (Op->Asl.FileChanged)
+        {
+            CvDbgPrint("    file has been changed!\n");
         }
 
         /*
