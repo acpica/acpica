5839d0db67fa20df9a0a7ee671fe61fd9df362af
Jung-uk Kim <jkim@FreeBSD.org>
2017-02-28 13:52:45 -0500

    Remove redefinition of YY_BUFFER_STATE.
    This works around an unnecessary redefinition of typedef "YY_BUFFER_STATE".

--- source/compiler/asltypes.h
+++ source/compiler/asltypes.h
@@ -416,12 +416,11 @@ typedef struct asl_xref_info
 } ASL_XREF_INFO;
 
 
-typedef struct yy_buffer_state *YY_BUFFER_STATE;
 typedef struct asl_file_node
 {
     FILE                    *File;
     UINT32                  CurrentLineNumber;
-    YY_BUFFER_STATE         State;
+    void                    *State;
     char                    *Filename;
     struct asl_file_node    *Next;
 
