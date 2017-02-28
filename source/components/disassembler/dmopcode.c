abf1d02accd4ba809c87bf5a04e75f18fe0fd2e2
Robert Moore <Robert.Moore@intel.com>
2017-02-28 08:47:30 -0800

    Revert "Disassembler: Fix small memory leak for resource tags"
    This reverts commit 20741c9c6e4f86eb90948cfceb55169593b79f0b.
    Attempt to free the wrong object.

--- source/components/disassembler/dmopcode.c
+++ source/components/disassembler/dmopcode.c
@@ -515,7 +515,6 @@ AcpiDmFieldPredefinedDescription (
             ACPI_CAST_PTR (char, Info->Description));
     }
 
-    ACPI_FREE (Tag); /* Tag was allocated in AcpiGetTagPathname */
 #endif
     return;
 }
