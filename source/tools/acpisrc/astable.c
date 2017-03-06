4f12387029c6a561e7792f53caf2e7f1f0ab2bbe
Lv Zheng <lv.zheng@intel.com>
2017-03-06 16:28:59 +0800

    Tables: Add WSMT support
    This patch adds WSMT support, the table can be found at Line [#1].
    The support includes table structure definitions (ACPICA tables) and
    assembly/disassembly (iasl) support. Lv Zheng.
    
    Link: https://msdn.microsoft.com/windows/hardware/drivers/bringup/acpi-system-description-tables#wsmt [1]
    Signed-off-by: Lv Zheng <lv.zheng@intel.com>

--- source/tools/acpisrc/astable.c
+++ source/tools/acpisrc/astable.c
@@ -664,6 +664,7 @@ ACPI_TYPED_IDENTIFIER_TABLE           AcpiIdentifiers[] = {
     {"ACPI_TABLE_WDDT",                     SRC_TYPE_STRUCT},
     {"ACPI_TABLE_WDRT",                     SRC_TYPE_STRUCT},
     {"ACPI_TABLE_WPBT",                     SRC_TYPE_STRUCT},
+    {"ACPI_TABLE_WSMT",                     SRC_TYPE_STRUCT},
     {"ACPI_TABLE_XENV",                     SRC_TYPE_STRUCT},
     {"ACPI_TABLE_XSDT",                     SRC_TYPE_STRUCT},
 
