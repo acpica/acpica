d717eb46f0bd8f691d57efd0a085227780356a2f
Robert Moore <Robert.Moore@intel.com>
2017-03-08 13:43:35 -0800

    acpihelp: Add PPTT table information
    New table for ACPI 6.2

--- source/common/ahtable.c
+++ source/common/ahtable.c
@@ -233,6 +233,7 @@ const AH_TABLE      Gbl_AcpiSupportedTables[] =
     {ACPI_SIG_NFIT, "NVDIMM Firmware Interface Table"},
     {ACPI_SIG_PCCT, "Platform Communications Channel Table"},
     {ACPI_SIG_PMTT, "Platform Memory Topology Table"},
+    {ACPI_SIG_PPTT, "Processor Properties Topology Table"},
     {ACPI_SIG_RASF, "RAS Features Table"},
     {ACPI_RSDP_NAME,"Root System Description Pointer"},
     {ACPI_SIG_RSDT, "Root System Description Table"},
