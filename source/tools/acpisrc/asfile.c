acpica/acpica

commit a2c83e75a595b956d4e32e9603b0c1c9d41e0a88

acpisrc: Add option to check files for non-printable characters
Dirty little option to scan entire file for non-printable ASCII
characters. Useful for debugging garbage output in listing
and text debug files.

source/tools/acpisrc/asfile.c
@@ -167,6 +167,11 @@ AsDetectLoneLineFeeds (
     char                    *Filename,
     char                    *Buffer);
 
+static BOOLEAN
+AsCheckForNonPrintableChars (
+    char                    *FileBuffer,
+    UINT32                  FileSize);
+
 static ACPI_INLINE int
 AsMaxInt (int a, int b)
 {
@@ -636,6 +641,45 @@ AsConvertFile (
     }
 }
 
+/*******************************************************************************
+ *
+ * FUNCTION:    AsCheckForNonPrintableChars
+ *
+ * PARAMETERS:  FileBuffer              - Buffer with contents of entire file
+ *              FileSize                - Size of the file and buffer
+ *
+ * RETURN:      TRUE if there are no non-printable characters
+ *
+ * DESCRIPTION: Scan a file for any non-printable ASCII bytes.
+ *
+ ******************************************************************************/
+
+static BOOLEAN
+AsCheckForNonPrintableChars (
+    char                    *FileBuffer,
+    UINT32                  FileSize)
+{
+    BOOLEAN                 Found = TRUE;
+    UINT8                   Byte;
+    UINT32                  i;
+
+
+    /* Scan entire file for any non-printable characters */
+
+    for (i = 0; i < FileSize; i++)
+    {
+        Byte = FileBuffer[i];
+        if (!isprint (Byte) && !isspace (Byte))
+        {
+            printf ( "Non-printable character (0x%2.2X) "
+                "at file offset: %8u (0x%X)\n", Byte, i, i);
+            Found = FALSE;
+        }
+    }
+
+    return (Found);
+}
+
 
 /******************************************************************************
  *
@@ -680,11 +724,18 @@ AsProcessOneFile (
     }
 
     strcat (Pathname, Filename);
-
     if (AsGetFile (Pathname, &Gbl_FileBuffer, &Gbl_FileSize))
     {
         Status = -1;
         goto Exit1;
+    }
+
+    /* Exit now if simply checking the file for printable ascii chars */
+
+    if (Gbl_CheckAscii)
+    {
+        Status = 0;
+        goto Exit2;
     }
 
     Gbl_HeaderSize = 0;
@@ -872,6 +923,17 @@ AsGetFile (
 
     Buffer [Size] = 0;         /* Null terminate the buffer */
     fclose (File);
+
+    /* This option checks the entire file for non-printable chars */
+
+    if (Gbl_CheckAscii)
+    {
+        if (AsCheckForNonPrintableChars (Buffer, Size))
+        {
+            printf ("File contains only printable ASCII characters\n");
+        }
+        goto ErrorFree;
+    }
 
     /* Check for unix contamination */
 

source/tools/acpisrc/acpisrc.h
@@ -230,6 +230,7 @@ extern BOOLEAN                  Gbl_IgnoreLoneLineFeeds;
 extern BOOLEAN                  Gbl_HasLoneLineFeeds;
 extern BOOLEAN                  Gbl_Cleanup;
 extern BOOLEAN                  Gbl_IgnoreTranslationEscapes;
+extern BOOLEAN                  Gbl_CheckAscii;
 extern void                     *Gbl_StructDefs;
 
 #define PARAM_LIST(pl)          pl

source/tools/acpisrc/asmain.c
@@ -188,6 +188,7 @@ struct stat             Gbl_StatBuf;
 char                    *Gbl_FileBuffer;
 UINT32                  Gbl_FileSize;
 UINT32                  Gbl_FileType;
+BOOLEAN                 Gbl_CheckAscii = FALSE;
 BOOLEAN                 Gbl_VerboseMode = FALSE;
 BOOLEAN                 Gbl_QuietMode = FALSE;
 BOOLEAN                 Gbl_BatchMode = FALSE;
@@ -201,7 +202,7 @@ BOOLEAN                 Gbl_Cleanup = FALSE;
 BOOLEAN                 Gbl_IgnoreTranslationEscapes = FALSE;
 
 #define AS_UTILITY_NAME             "ACPI Source Code Conversion Utility"
-#define AS_SUPPORTED_OPTIONS        "cdhilqsuv^y"
+#define AS_SUPPORTED_OPTIONS        "acdhilqsuv^y"
 
 
 /******************************************************************************
@@ -361,6 +362,7 @@ AsDisplayUsage (
 
     ACPI_USAGE_HEADER ("acpisrc [-c|l|u] [-dsvy] <SourceDir> <DestinationDir>");
 
+    ACPI_OPTION ("-a <file>",   "Check entire file for non-printable characters");
     ACPI_OPTION ("-c",          "Generate cleaned version of the source");
     ACPI_OPTION ("-h",          "Insert dual-license header into all modules");
     ACPI_OPTION ("-i",          "Cleanup macro indentation");
@@ -505,6 +507,11 @@ main (
         Gbl_QuietMode = TRUE;
         break;
 
+    case 'a':
+
+        Gbl_CheckAscii = TRUE;
+        break;
+
     default:
 
         AsDisplayUsage ();
@@ -518,6 +525,14 @@ main (
         printf ("Missing source path\n");
         AsDisplayUsage ();
         return (-1);
+    }
+
+    /* This option checks the entire file for printable ascii chars */
+
+    if (Gbl_CheckAscii)
+    {
+        AsProcessOneFile (NULL, NULL, NULL, 0, SourcePath, FILE_TYPE_SOURCE);
+        return (0);
     }
 
     TargetPath = argv[AcpiGbl_Optind+1];
@@ -558,6 +573,13 @@ main (
     }
     else
     {
+        if (Gbl_CheckAscii)
+        {
+            AsProcessOneFile (NULL, NULL, NULL, 0,
+                SourcePath, FILE_TYPE_SOURCE);
+            return (0);
+        }
+
         /* Process a single file */
 
         /* Differentiate between source and header files */

commit 2383f50553d063d010035a5fa34654c04d866ffd

acpisrc: don't fclose a file twice on an error exit path
The check on Gbl_CheckAscii in function AsProcessOneFile jumps to the
ErrorFree exit path that fcloses a file a second time.  Avoid this
double fclose by moving first fclose to close to the end path of the
function.

Detected by CoverityScan, CID#175021 ("Use after close")

Fixes: a2c83e75a595 ("acpisrc: Add option to check files for non-printable characters")
Signed-off-by: Colin Ian King <colin.king@canonical.com>

source/tools/acpisrc/asfile.c
@@ -922,7 +922,6 @@ AsGetFile (
     }
 
     Buffer [Size] = 0;         /* Null terminate the buffer */
-    fclose (File);
 
     /* This option checks the entire file for non-printable chars */
 
@@ -945,6 +944,7 @@ AsGetFile (
      */
     AsConvertToLineFeeds (Buffer);
 
+    fclose (File);
     *FileBuffer = Buffer;
     *FileSize = Size;
     return (0);