a48cbca637d2db26a7d1952f565bc1fec8a5222d
Robert Moore <Robert.Moore@intel.com>
2017-09-08 13:17:31 -0700

    acpiexec: Update for new loop timeout mechanism
    Adds new option to override a loop timeout, to allow method
    execution to continue.
    Rename -i option to -to (set timeout value).

--- source/tools/acpiexec/aecommon.h
+++ source/tools/acpiexec/aecommon.h
@@ -190,6 +190,7 @@ typedef struct ae_debug_regions
 
 
 extern BOOLEAN              AcpiGbl_IgnoreErrors;
+extern BOOLEAN              AcpiGbl_AbortLoopOnTimeout;
 extern UINT8                AcpiGbl_RegionFillValue;
 extern UINT8                AcpiGbl_UseHwReducedFadt;
 extern BOOLEAN              AcpiGbl_DisplayRegionAccess;
