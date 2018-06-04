4c9fec1e71c5ba3d011048d73919a888788c486e
Erik Schmauss <erik.schmauss@intel.com>
2018-06-04 15:06:24 -0700

    Revert "iasl: fix field unit compilation"
    This reverts commit 512859550948dc663bb45c5d2bccbee4390b2677.

--- source/compiler/aslload.c
+++ source/compiler/aslload.c
@@ -321,8 +321,7 @@ LdLoadFieldElements (
                     return (Status);
                 }
                 else if (Status == AE_ALREADY_EXISTS &&
-                    (Node->Flags & ANOBJ_IS_EXTERNAL) &&
-                    Node->OwnerId != WalkState->OwnerId)
+                    (Node->Flags & ANOBJ_IS_EXTERNAL))
                 {
                     Node->Type = (UINT8) ACPI_TYPE_LOCAL_REGION_FIELD;
                 }
