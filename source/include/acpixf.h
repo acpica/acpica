aace846bb8f9b5877e100017f37fefc09dd2fde6
Robert Moore <Robert.Moore@intel.com>
2019-01-08 13:03:54 -0800

    Update version to 20190108
    Version 20190108

--- source/include/acpixf.h
+++ source/include/acpixf.h
@@ -154,7 +154,7 @@
 
 /* Current ACPICA subsystem version in YYYYMMDD format */
 
-#define ACPI_CA_VERSION                 0x20181213
+#define ACPI_CA_VERSION                 0x20190108
 
 #include "acconfig.h"
 #include "actypes.h"
