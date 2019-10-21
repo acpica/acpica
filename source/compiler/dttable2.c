acpica/acpica

commit c22c0b1df98c344f087b0fb0e3f55e3d828f3472

Data table compiler: Add error messages for SLIT locality buffers Check for: 1) Invalid number of initializers in a locality buffer 2) Invalid number of locality buffer declarations

source/compiler/dttable2.c
@@ -1621,7 +1621,9 @@ DtCompileSlit (
     DT_SUBTABLE             *ParentTable;
     DT_FIELD                **PFieldList = (DT_FIELD **) List;
     DT_FIELD                *FieldList;
+    DT_FIELD                *EndOfFieldList = NULL;
     UINT32                  Localities;
+    UINT32                  LocalityListLength;
     UINT8                   *LocalityBuffer;
 
 
@@ -1637,6 +1639,7 @@ DtCompileSlit (
 
     Localities = *ACPI_CAST_PTR (UINT32, Subtable->Buffer);
     LocalityBuffer = UtLocalCalloc (Localities);
+    LocalityListLength = 0;
 
     /* Compile each locality buffer */
 
@@ -1646,9 +1649,20 @@ DtCompileSlit (
         DtCompileBuffer (LocalityBuffer,
             FieldList->Value, FieldList, Localities);
 
+        LocalityListLength++;
         DtCreateSubtable (LocalityBuffer, Localities, &Subtable);
         DtInsertSubtable (ParentTable, Subtable);
+        EndOfFieldList = FieldList;
         FieldList = FieldList->Next;
+    }
+
+    if (LocalityListLength != Localities)
+    {
+        sprintf(AslGbl_MsgBuffer,
+            "Found %u entries, must match LocalityCount: %u",
+            LocalityListLength, Localities);
+        DtError (ASL_ERROR, ASL_MSG_ENTRY_LIST, EndOfFieldList, AslGbl_MsgBuffer);
+        return (AE_LIMIT);
     }
 
     ACPI_FREE (LocalityBuffer);

source/compiler/aslmessages.c
@@ -388,7 +388,9 @@ const char                      *AslTableCompilerMsgs [] =
 /*    ASL_MSG_UNKNOWN_SUBTABLE */           "Unknown subtable type",
 /*    ASL_MSG_UNKNOWN_TABLE */              "Unknown ACPI table signature",
 /*    ASL_MSG_ZERO_VALUE */                 "Value must be non-zero",
-/*    ASL_MSG_INVALID_LABEL */              "Invalid field label detected"
+/*    ASL_MSG_INVALID_LABEL */              "Invalid field label detected",
+/*    ASL_MSG_BUFFER_LIST */                "Invalid buffer initializer list",
+/*    ASL_MSG_ENTRY_LIST */                 "Invalid entry initializer list"
 };
 
 /* Preprocessor */

source/compiler/aslmessages.h
@@ -388,6 +388,8 @@ typedef enum
     ASL_MSG_UNKNOWN_TABLE,
     ASL_MSG_ZERO_VALUE,
     ASL_MSG_INVALID_LABEL,
+    ASL_MSG_BUFFER_LIST,
+    ASL_MSG_ENTRY_LIST,
 
     /* These messages are used by the Preprocessor only */
 

source/compiler/dtfield.c
@@ -576,6 +576,14 @@ DtCompileBuffer (
 
     StringValue = DtNormalizeBuffer (StringValue, &Count);
     Substring = StringValue;
+    if (Count != ByteLength)
+    {
+        sprintf(AslGbl_MsgBuffer,
+            "Found %u values, must match expected count: %u",
+            Count, ByteLength);
+        DtError (ASL_ERROR, ASL_MSG_BUFFER_LIST, Field, AslGbl_MsgBuffer);
+        goto Exit;
+    }
 
     /* Each element of StringValue is now three chars (2 hex + 1 space) */
 

commit 1e145c624628d7a127072c4ec15c3c110e0c002f

iASL: DT: allocate from cache to facilitate memory reclamation
Fixes: c22c0b1 ("Data table compiler: Add error messages for SLIT locality buffers")
Reported-by: Colin Ian King <colin.king@canonical.com>
Signed-off-by: Erik Schmauss <erik.schmauss@intel.com>

source/compiler/dttable2.c
@@ -1638,7 +1638,7 @@ DtCompileSlit (
     DtInsertSubtable (ParentTable, Subtable);
 
     Localities = *ACPI_CAST_PTR (UINT32, Subtable->Buffer);
-    LocalityBuffer = UtLocalCalloc (Localities);
+    LocalityBuffer = (UINT8 *) UtLocalCacheCalloc (Localities);
     LocalityListLength = 0;
 
     /* Compile each locality buffer */