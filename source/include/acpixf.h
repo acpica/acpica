28cb42013541950cf378582a5a5a5587061498ca
Robert Moore <Robert.Moore@intel.com>
2021-01-05 10:10:20 -0800

    Update version to 20210105
    Version 20210105.

--- source/include/acpixf.h
+++ source/include/acpixf.h
@@ -154,7 +154,7 @@
 
 /* Current ACPICA subsystem version in YYYYMMDD format */
 
-#define ACPI_CA_VERSION                 0x20201217
+#define ACPI_CA_VERSION                 0x20210105
 
 #include "acconfig.h"
 #include "actypes.h"
