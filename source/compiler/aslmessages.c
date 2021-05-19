acpica/acpica

commit 81eb9c383e6dee0f1b6620e91e5c3dbb48234831

iASL: Add support for the BDAT ACPI table Includes: Table compiler, disassembler, and template generator.

source/compiler/aslmessages.c
@@ -400,7 +400,7 @@ const char                      *AslTableCompilerMsgs [] =
 /*    ASL_MSG_INVALID_FIELD_NAME */         "Invalid Field Name",
 /*    ASL_MSG_INVALID_HEX_INTEGER */        "Invalid hex integer constant",
 /*    ASL_MSG_OEM_TABLE */                  "OEM table - unknown contents",
-/*    ASL_MSG_RESERVED_VALUE */             "Reserved field",
+/*    ASL_MSG_RESERVED_FIELD */             "Reserved field",
 /*    ASL_MSG_UNKNOWN_LABEL */              "Label is undefined",
 /*    ASL_MSG_UNKNOWN_SUBTABLE */           "Unknown subtable type",
 /*    ASL_MSG_UNKNOWN_TABLE */              "Unknown ACPI table signature",
@@ -409,6 +409,7 @@ const char                      *AslTableCompilerMsgs [] =
 /*    ASL_MSG_BUFFER_LIST */                "Invalid buffer initializer list",
 /*    ASL_MSG_ENTRY_LIST */                 "Invalid entry initializer list",
 /*    ASL_MSG_UNKNOWN_FORMAT */             "Unknown format value"
+/*    ASL_MSG_RESERVED_VALUE */             "Value for field is reserved or unknown",
 };
 
 /* Preprocessor */

source/common/ahtable.c
@@ -200,6 +200,7 @@ AcpiAhGetTableInfo (
 const AH_TABLE      AcpiGbl_SupportedTables[] =
 {
     {ACPI_SIG_ASF,  "Alert Standard Format Table"},
+    {ACPI_SIG_BDAT, "BIOS Data ACPI Table"},
     {ACPI_SIG_BERT, "Boot Error Record Table"},
     {ACPI_SIG_BGRT, "Boot Graphics Resource Table"},
     {ACPI_SIG_BOOT, "Simple Boot Flag Table"},

source/common/dmtable.c
@@ -532,6 +532,7 @@ static const char           *AcpiDmGasAccessWidth[] =
 const ACPI_DMTABLE_DATA     AcpiDmTableData[] =
 {
     {ACPI_SIG_ASF,  NULL,                   AcpiDmDumpAsf,  DtCompileAsf,   TemplateAsf},
+    {ACPI_SIG_BDAT, AcpiDmTableInfoBdat,    NULL,           NULL,           TemplateBdat},
     {ACPI_SIG_BERT, AcpiDmTableInfoBert,    NULL,           NULL,           TemplateBert},
     {ACPI_SIG_BGRT, AcpiDmTableInfoBgrt,    NULL,           NULL,           TemplateBgrt},
     {ACPI_SIG_BOOT, AcpiDmTableInfoBoot,    NULL,           NULL,           TemplateBoot},

source/common/dmtbinfo1.c
@@ -297,6 +297,19 @@ ACPI_DMTABLE_INFO           AcpiDmTableInfoAsf4[] =
 
 /*******************************************************************************
  *
+ * BDAT -  BIOS Data ACPI Table
+ *
+ ******************************************************************************/
+
+ACPI_DMTABLE_INFO           AcpiDmTableInfoBdat[] =
+{
+    {ACPI_DMT_GAS,      ACPI_BDAT_OFFSET (Gas),                     "BDAT Generic Address", 0},
+    ACPI_DMT_TERMINATOR
+};
+
+
+/*******************************************************************************
+ *
  * BERT -  Boot Error Record table
  *
  ******************************************************************************/

source/compiler/aslmessages.h
@@ -402,7 +402,7 @@ typedef enum
     ASL_MSG_INVALID_FIELD_NAME,
     ASL_MSG_INVALID_HEX_INTEGER,
     ASL_MSG_OEM_TABLE,
-    ASL_MSG_RESERVED_VALUE,
+    ASL_MSG_RESERVED_FIELD,
     ASL_MSG_UNKNOWN_LABEL,
     ASL_MSG_UNKNOWN_SUBTABLE,
     ASL_MSG_UNKNOWN_TABLE,
@@ -411,6 +411,7 @@ typedef enum
     ASL_MSG_BUFFER_LIST,
     ASL_MSG_ENTRY_LIST,
     ASL_MSG_UNKNOWN_FORMAT,
+    ASL_MSG_RESERVED_VALUE,
 
     /* These messages are used by the Preprocessor only */
 

source/compiler/dtcompiler.h
@@ -751,6 +751,7 @@ DtGetGenericTableInfo (
 
 extern const unsigned char  TemplateAsf[];
 extern const unsigned char  TemplateBoot[];
+extern const unsigned char  TemplateBdat[];
 extern const unsigned char  TemplateBert[];
 extern const unsigned char  TemplateBgrt[];
 extern const unsigned char  TemplateCedt[];

source/compiler/dtfield.c
@@ -434,14 +434,14 @@ DtCompileInteger (
         {
             if (Value != 1)
             {
-                DtError (ASL_WARNING, ASL_MSG_RESERVED_VALUE, Field,
+                DtError (ASL_WARNING, ASL_MSG_RESERVED_FIELD, Field,
                     "Must be one, setting to one");
                 Value = 1;
             }
         }
         else if (Value != 0)
         {
-            DtError (ASL_WARNING, ASL_MSG_RESERVED_VALUE, Field,
+            DtError (ASL_WARNING, ASL_MSG_RESERVED_FIELD, Field,
                 "Must be zero, setting to zero");
             Value = 0;
         }

source/compiler/dttemplate.h
@@ -174,6 +174,16 @@ const unsigned char TemplateAsf[] =
     0x01,0x00                                 /* 00000070    ".."       */
 };
 
+const unsigned char TemplateBdat[] =
+{
+    0x42,0x44,0x41,0x54,0x30,0x00,0x00,0x00,  /* 00000000    "BDAT0..." */
+    0x01,0xED,0x49,0x4E,0x54,0x45,0x4C,0x20,  /* 00000008    "..INTEL " */
+    0x54,0x65,0x6D,0x70,0x6C,0x61,0x74,0x65,  /* 00000010    "Template" */
+    0x01,0x00,0x00,0x00,0x49,0x4E,0x54,0x4C,  /* 00000018    "....INTL" */
+    0x31,0x03,0x21,0x20,0x01,0x08,0x00,0x01,  /* 00000020    "1.! ...." */
+    0xF0,0xDE,0xBC,0x9A,0x78,0x56,0x34,0x12   /* 00000028    "....xV4." */
+};
+
 const unsigned char TemplateBgrt[] =
 {
     0x42,0x47,0x52,0x54,0x38,0x00,0x00,0x00,  /* 00000000    "BGRT8..." */

commit 3f8c79fc22fd64b739f51268654a6783a874520e

iASL: Fix a missing comma between two literal strings
A recent commit added a new literal string to the AslTableCompilerMsgs[]
array but the previous string is missing a comma and so the two strings
are ending up being concatenated. Fix this by adding the missing comma.

Addresses-Coverity: ("Missing comma in a string array initialization")
Fixes: 81eb9c383e6d ("iASL: Add support for the BDAT ACPI table")
Signed-off-by: Colin Ian King <colin.king@canonical.com>

source/compiler/aslmessages.c
@@ -408,7 +408,7 @@ const char                      *AslTableCompilerMsgs [] =
 /*    ASL_MSG_INVALID_LABEL */              "Invalid field label detected",
 /*    ASL_MSG_BUFFER_LIST */                "Invalid buffer initializer list",
 /*    ASL_MSG_ENTRY_LIST */                 "Invalid entry initializer list",
-/*    ASL_MSG_UNKNOWN_FORMAT */             "Unknown format value"
+/*    ASL_MSG_UNKNOWN_FORMAT */             "Unknown format value",
 /*    ASL_MSG_RESERVED_VALUE */             "Value for field is reserved or unknown",
 };
 