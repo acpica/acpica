e75074d84d1207339a048486c2d06ecb935d0092
Robert Moore <Robert.Moore@intel.com>
2021-07-01 09:30:45 -0700

    iASL: Add support for the AEST table (data compiler) Includes support in the table compiler and the disassembler.

--- source/compiler/dtutils.c
+++ source/compiler/dtutils.c
@@ -586,6 +586,10 @@ DtGetFieldLength (
     case ACPI_DMT_RGRT:
     case ACPI_DMT_SDEV:
     case ACPI_DMT_SRAT:
+    case ACPI_DMT_AEST:
+    case ACPI_DMT_AEST_RES:
+    case ACPI_DMT_AEST_XFACE:
+    case ACPI_DMT_AEST_XRUPT:
     case ACPI_DMT_ASF:
     case ACPI_DMT_HESTNTYP:
     case ACPI_DMT_FADTPM:
@@ -616,6 +620,8 @@ DtGetFieldLength (
         break;
 
     case ACPI_DMT_UINT32:
+    case ACPI_DMT_AEST_CACHE:
+    case ACPI_DMT_AEST_GIC:
     case ACPI_DMT_NAME4:
     case ACPI_DMT_SIG:
     case ACPI_DMT_LPIT:
