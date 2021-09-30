acpica/acpica

commit 0084a2ede3ed9d6382552de1d340d489c080b192

Headers: Avoid NULL pointer arithmetic
There are some very old macros for doing an open-coded offsetof() and
cast between pointer and integer in ACPI headers. clang-14 now complains
about these:

drivers/acpi/acpica/tbfadt.c:86:3: error: performing pointer subtraction with a null pointer has undefined behavior [-Werror,-Wnull-pointer-subtraction]
         ACPI_FADT_OFFSET(pm_timer_block),
         ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
include/acpi/actbl.h:376:47: note: expanded from macro 'ACPI_FADT_OFFSET'
 #define ACPI_FADT_OFFSET(f)             (u16) ACPI_OFFSET (struct acpi_table_fadt, f)
                                              ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
include/acpi/actypes.h:511:41: note: expanded from macro 'ACPI_OFFSET'
 #define ACPI_OFFSET(d, f)               ACPI_PTR_DIFF (&(((d *) 0)->f), (void *) 0)
                                        ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
include/acpi/actypes.h:505:79: note: expanded from macro 'ACPI_PTR_DIFF'
 #define ACPI_PTR_DIFF(a, b)             ((acpi_size) (ACPI_CAST_PTR (u8, (a)) - ACPI_CAST_PTR (u8, (b))))
                                                                              ^ ~~~~~~~~~~~~~~~~~~~~~~~
Convert them to the modern equivalents.

Signed-off-by: Arnd Bergmann <arnd@arndb.de>
[ rjw: Linux kernel patch conversion into the upstream ACPICA format ]
Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

source/include/actypes.h
@@ -658,8 +658,8 @@ typedef UINT64                          ACPI_INTEGER;
 /* Pointer/Integer type conversions */
 
 #define ACPI_TO_POINTER(i)              ACPI_CAST_PTR (void, (ACPI_SIZE) (i))
-#define ACPI_TO_INTEGER(p)              ACPI_PTR_DIFF (p, (void *) 0)
-#define ACPI_OFFSET(d, f)               ACPI_PTR_DIFF (&(((d *) 0)->f), (void *) 0)
+#define ACPI_TO_INTEGER(p)              ((uintptr_t)(p))
+#define ACPI_OFFSET(d, f)               offsetof(d, f)
 #define ACPI_PHYSADDR_TO_PTR(i)         ACPI_TO_POINTER(i)
 #define ACPI_PTR_TO_PHYSADDR(i)         ACPI_TO_INTEGER(i)
 

commit 5ed3af35d804ae9150c833f0077f1d1d4ccaccda

Headers: Fix build when ACPI_USE_SYSTEM_INTTYPES is not set
Commit 0084a2ede3ed ("Headers: Avoid NULL pointer arithmetic")
introduced build issues that occur when ACPI_USE_SYSTEM_INTTYPES
is not set, because the uintptr_t type and the offsetof() macro
are not defined then.

Fix this by providing uintptr_t and offsetof() definitions for the
failing case.

Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

source/include/actypes.h
@@ -239,6 +239,9 @@ typedef short                           INT16;
 typedef COMPILER_DEPENDENT_UINT64       UINT64;
 typedef COMPILER_DEPENDENT_INT64        INT64;
 
+typedef unsigned long                   uintptr_t;
+#define offsetof(TYPE, MEMBER)          ((size_t)&((TYPE *)0)->MEMBER)
+
 #endif /* ACPI_USE_SYSTEM_INTTYPES */
 
 /*