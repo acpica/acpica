dd4acd9614d133f2639b928acc0cdbcd6aaa0d35
Robert Moore <Robert.Moore@intel.com>
2021-12-15 07:44:39 -0800

    acpisrc: Add suppoprt for AGDI table Add conversion info for AGDI.

--- source/tools/acpisrc/astable.c
+++ source/tools/acpisrc/astable.c
@@ -642,6 +642,7 @@ ACPI_TYPED_IDENTIFIER_TABLE           AcpiIdentifiers[] = {
      * Acpi table definition names.
      */
     {"ACPI_TABLE_AEST",                     SRC_TYPE_STRUCT},
+    {"ACPI_TABLE_AGDI",                     SRC_TYPE_STRUCT},
     {"ACPI_TABLE_ASF",                      SRC_TYPE_STRUCT},
     {"ACPI_TABLE_BDAT",                     SRC_TYPE_STRUCT},
     {"ACPI_TABLE_BERT",                     SRC_TYPE_STRUCT},
