7e27ab1e6f9a69875fc7b2da5d71e903a6568f4d
Elyes Haouas <ehaouas@noos.fr>
2023-02-07 06:59:17 +0100

    Remove trailing whitespace
    Signed-off-by: Elyes Haouas <ehaouas@noos.fr>

--- source/components/utilities/utcksum.c
+++ source/components/utilities/utcksum.c
@@ -172,7 +172,7 @@
  *
  * DESCRIPTION: Verifies that the table checksums to zero. Optionally returns
  *              exception on bad checksum.
- *              Note: We don't have to check for a CDAT here, since CDAT is 
+ *              Note: We don't have to check for a CDAT here, since CDAT is
  *              not in the RSDT/XSDT, and the CDAT table is never installed
  *              via ACPICA.
  *
