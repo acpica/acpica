acpica/acpica

commit 36dcfd8818a4e0e1e0c7a81d4c731049cc79fa35

Events: Make it possible to enable runtime GPEs earlier
Runtime GPEs have corresponding _Lxx/_Exx methods and are enabled
automatically during the initialization of the ACPI subsystem through
AcpiUpdateAllGpes() with the assumption that AcpiSetupGpeForWake()
will be called in advance for all of the GPEs pointed to by _PRW
objects in the namespace that may be affected by AcpiUpdateAllGpes().
That is, AcpiEvInitializeGpeBlock() can only be called for a GPE
block after AcpiSetupGpeForWake() has been called for all of the
_PRW (wakeup) GPEs in it.

The platform firmware on some systems, however, expects GPEs to be
enabled before the enumeration of devices which is when
AcpiSetupGpeForWake() is called and that goes against the above
assumption.

For this reason, introduce a new flag to be set by
AcpiEvInitializeGpeBlock() when automatically enabling a GPE
to indicate to AcpiSetupGpeForWake() that it needs to drop the
reference to the GPE coming from AcpiEvInitializeGpeBlock()
and modify AcpiSetupGpeForWake() accordingly.  These changes
allow AcpiSetupGpeForWake() and AcpiEvInitializeGpeBlock()
to be invoked in any order.

Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
Signed-off-by: Mika Westerberg <mika.westerberg@linux.intel.com>

source/components/events/evxfgpe.c
@@ -609,6 +609,16 @@ AcpiSetupGpeForWake (
         GpeEventInfo->Flags =
             (ACPI_GPE_DISPATCH_NOTIFY | ACPI_GPE_LEVEL_TRIGGERED);
     }
+    else if (GpeEventInfo->Flags & ACPI_GPE_AUTO_ENABLED)
+    {
+        /*
+         * A reference to this GPE has been added during the GPE block
+         * initialization, so drop it now to prevent the GPE from being
+         * permanently enabled and clear its ACPI_GPE_AUTO_ENABLED flag.
+         */
+        (void) AcpiEvRemoveGpeReference (GpeEventInfo);
+        GpeEventInfo->Flags &= ~~ACPI_GPE_AUTO_ENABLED;
+    }
 
     /*
      * If we already have an implicit notify on this GPE, add

source/components/events/evgpeblk.c
@@ -648,6 +648,8 @@ AcpiEvInitializeGpeBlock (
                 continue;
             }
 
+            GpeEventInfo->Flags |= ACPI_GPE_AUTO_ENABLED;
+
             if (EventStatus & ACPI_EVENT_FLAG_STATUS_SET)
             {
                 ACPI_INFO (("GPE 0x%02X active on init",

source/include/actypes.h
@@ -906,7 +906,7 @@ typedef UINT32                          ACPI_EVENT_STATUS;
  *   |  | | |  +-- Type of dispatch:to method, handler, notify, or none
  *   |  | | +----- Interrupt type: edge or level triggered
  *   |  | +------- Is a Wake GPE
- *   |  +--------- Is GPE masked by the software GPE masking mechanism
+ *   |  +--------- Has been enabled automatically at init time
  *   +------------ <Reserved>
  */
 #define ACPI_GPE_DISPATCH_NONE          (UINT8) 0x00
@@ -922,6 +922,7 @@ typedef UINT32                          ACPI_EVENT_STATUS;
 #define ACPI_GPE_XRUPT_TYPE_MASK        (UINT8) 0x08
 
 #define ACPI_GPE_CAN_WAKE               (UINT8) 0x10
+#define ACPI_GPE_AUTO_ENABLED           (UINT8) 0x20
 
 /*
  * Flags for GPE and Lock interfaces

commit 8affc87afd283607d7a12df38169bb91ac4f035e

events/evxfgpe: correctly clear GPE_AUTO_ENABLED in AcpiSetupGpeForWake
Because of a typo we would inverse the mask twice, thus producing a
bogus result.

Fixes: 36dcfd8818a ("Events: Make it possible to enable runtime GPEs earlier")
Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

source/components/events/evxfgpe.c
@@ -636,7 +636,7 @@ AcpiSetupGpeForWake (
          * permanently enabled and clear its ACPI_GPE_AUTO_ENABLED flag.
          */
         (void) AcpiEvRemoveGpeReference (GpeEventInfo);
-        GpeEventInfo->Flags &= ~~ACPI_GPE_AUTO_ENABLED;
+        GpeEventInfo->Flags &= ~ACPI_GPE_AUTO_ENABLED;
     }
 
     /*