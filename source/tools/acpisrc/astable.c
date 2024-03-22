c18edfb1abfb50521a363f811bc8e1e64213bd00
Saket Dumbre <saket.dumbre@intel.com>
2024-03-21 20:23:36 -0700

    Update the copyright year in the license header from 2023 to 2024

--- source/tools/acpisrc/astable.c
+++ source/tools/acpisrc/astable.c
@@ -8,7 +8,7 @@
  *
  * 1. Copyright Notice
  *
- * Some or all of this work - Copyright (c) 1999 - 2023, Intel Corp.
+ * Some or all of this work - Copyright (c) 1999 - 2024, Intel Corp.
  * All rights reserved.
  *
  * 2. License
@@ -1242,7 +1242,7 @@ ACPI_CONVERSION_TABLE       LicenseConversionTable =
 
 ACPI_STRING_TABLE           CustomReplacements[] =
 {
-    {"(c) 1999 - 2021",     "(c) 1999 - 2023",         REPLACE_WHOLE_WORD}, /* Main ACPICA source */
+    {"(c) 1999 - 2021",     "(c) 1999 - 2024",         REPLACE_WHOLE_WORD}, /* Main ACPICA source */
     {"(c) 2006 - 2021",     "(c) 2006 - 2023",         REPLACE_WHOLE_WORD}, /* Test suites */
 
 #if 0
