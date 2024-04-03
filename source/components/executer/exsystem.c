acpica/acpica

commit 05ba545ce7859392250b18c10081db25c90ed8d7

executer/exsystem: Inform users about ACPI spec violation
Values greater than 100 microseconds violate the ACPI specification, so
warn users about it.

From ACPI Specification version 6.2 Errata A, 19.6.128 *Stall (Stall for
a Short Time)*:

> The implementation of Stall is OS-specific, but must not relinquish
> control of the processor. Because of this, delays longer than 100
> microseconds must use Sleep instead of Stall.

source/components/executer/exsystem.c
@@ -302,6 +302,11 @@ AcpiExSystemDoStall (
     }
     else
     {
+        if (HowLongUS > 100)
+	{
+            ACPI_WARNING ((AE_INFO,
+                "Time parameter %u us > 100 us violating ACPI spec, please fix the firmware.", HowLongUs));
+	}
         AcpiOsStall (HowLongUs);
     }
 

commit 129b75516fc49fe1fd6b8c5798f86c13854630b3

executer/exsystem: Don't nag user about every Stall() violating the spec
Stop nagging user about every Stall() that violates the spec

On my Dell XPS 15 7590 I get hundreds of these warnings after few hours of
uptime:

$ dmesg | grep "fix the firmware" | wc -l
261

I cannot fix the firmware and I doubt that Dell cares about 4 year old
laptop either

Fixes: 05ba545ce785 ("executer/exsystem: Inform users about ACPI spec violation")
Signed-off-by: Vasily Khoruzhick <anarsoul@gmail.com>

source/components/executer/exsystem.c
@@ -296,7 +296,7 @@ AcpiExSystemDoStall (
          * (ACPI specifies 100 usec as max, but this gives some slack in
          * order to support existing BIOSs)
          */
-        ACPI_ERROR ((AE_INFO,
+        ACPI_ERROR_ONCE ((AE_INFO,
             "Time parameter is too large (%u)", HowLongUs));
         Status = AE_AML_OPERAND_VALUE;
     }
@@ -304,7 +304,7 @@ AcpiExSystemDoStall (
     {
         if (HowLongUs > 100)
         {
-            ACPI_WARNING ((AE_INFO,
+            ACPI_WARNING_ONCE ((AE_INFO,
                 "Time parameter %u us > 100 us violating ACPI spec, please fix the firmware.", HowLongUs));
         }
         AcpiOsStall (HowLongUs);