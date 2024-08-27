86c762afe5bf915e8101a0455513368e2a60dd80
Saket Dumbre <saket.dumbre@intel.com>
2024-08-27 11:24:10 -0700

    Setup for the new ACPICA release
    Signed-off-by: Saket Dumbre <saket.dumbre@intel.com>

--- source/include/acpixf.h
+++ source/include/acpixf.h
@@ -154,7 +154,7 @@
 
 /* Current ACPICA subsystem version in YYYYMMDD format */
 
-#define ACPI_CA_VERSION                 0x20240322
+#define ACPI_CA_VERSION                 0x20240827
 
 #include "acconfig.h"
 #include "actypes.h"
