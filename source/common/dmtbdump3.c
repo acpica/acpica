acpica/acpica

commit 1672c9dc644620016e9030b1203ac0c4a2dff5d6

Correct dumping of SLIC tables
When dumping the SLIC table, the code was not starting at the proper
offset.  Set the offset to the first byte after the header.

Signed-off-by: Al Stone <ahs3@ahs3.net>

source/common/dmtbdump3.c
@@ -177,7 +177,8 @@ AcpiDmDumpSlic (
     ACPI_TABLE_HEADER       *Table)
 {
 
-    (void) AcpiDmDumpTable (Table->Length, sizeof (ACPI_TABLE_HEADER), Table,
+    (void) AcpiDmDumpTable (Table->Length, sizeof (ACPI_TABLE_HEADER),
+        (void *) (Table + sizeof (*Table)),
         Table->Length - sizeof (*Table), AcpiDmTableInfoSlic);
 }
 

commit 747578fcdc0ad5eb806d9843aa0cb5d846bebc51

Fix pointer offset into SLIC table
Currently the offset into the SLIC table is being computing by adding
an offset onto the Table pointer but because Table is a ACPI_TABLE_HEADER
pointer type the offset is actually in multiples of the size of the
ACPI_TABLE_HEADER struct. Fix this by casting Table to a UINT8 * to
make the pointer arithmetic work in terms of octects (bytes).

Detected when integrating ACPICA into the Firmware Test Suite and I noticed
the SLIC table data was being fetched from a bogus location.

Fixes: 1672c9dc6446 ("Correct dumping of SLIC tables")

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

source/common/dmtbdump3.c
@@ -178,7 +178,7 @@ AcpiDmDumpSlic (
 {
 
     (void) AcpiDmDumpTable (Table->Length, sizeof (ACPI_TABLE_HEADER),
-        (void *) (Table + sizeof (*Table)),
+        (void *) ((UINT8 *)Table + sizeof (*Table)),
         Table->Length - sizeof (*Table), AcpiDmTableInfoSlic);
 }
 