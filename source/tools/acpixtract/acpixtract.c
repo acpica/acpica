acpica/acpica

commit ad5755dd80718b02d797b184ba290789ea89f274

acpixtract: don't abort AxExtractTables for AX_OPTIONAL_TABLES
Otherwise this makes the utility return an error for invocations like
"acpixtract <FILE>", where FILE contains a DSDT but no SSDTs.

Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

source/tools/acpixtract/acpixtract.c
@@ -213,7 +213,7 @@ AxExtractTables (
         AxNormalizeSignature (UpperSignature);
         Instances = AxCountTableInstances (InputPathname, UpperSignature);
 
-        if (Instances < MinimumInstances)
+        if (Instances < MinimumInstances || MinimumInstances == AX_OPTIONAL_TABLES)
         {
             printf ("Table [%s] was not found in %s\n",
                 UpperSignature, InputPathname);

commit 0f0b704ef72c8e0f822faf6322398f920f211e8f

tools/acpixtract: handle AX_OPTIONAL_TABLES correctly
The previous commit introduced a regression while attempting to make
acpixtract not exit with -1 in case of a missing optional table.

Add an extra condition that checks that the number of detected instances
was 0 before checking AX_OPTIONAL_TABLES.

Fixes: ad5755dd807 ("acpixtract: don't abort AxExtractTables for AX_OPTIONAL_TABLES")
Resolves: #978
Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

source/tools/acpixtract/acpixtract.c
@@ -213,7 +213,8 @@ AxExtractTables (
         AxNormalizeSignature (UpperSignature);
         Instances = AxCountTableInstances (InputPathname, UpperSignature);
 
-        if (Instances < MinimumInstances || MinimumInstances == AX_OPTIONAL_TABLES)
+        if (Instances < MinimumInstances ||
+            (Instances == 0 && MinimumInstances == AX_OPTIONAL_TABLES))
         {
             printf ("Table [%s] was not found in %s\n",
                 UpperSignature, InputPathname);