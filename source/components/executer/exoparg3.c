acpica/acpica

commit d516c7758ba645f2a0b1eb5f5e775e6bf116271d

Abort AML bytecode execution when executing AML_FATAL_OP
The ACPI specification states that when executing AML_FATAL_OP,
the OS should log the fatal error event and shutdown in a timely
fashion. Windows complies with this requirement by immediatly
entering a BSoD, effectively aborting the execution of the AML
bytecode in question.

ACPICA however might continue with the AML bytecode execution
should AcpiOsSignal() simply return AE_OK. This will cause issues
because ACPI BIOS implementations might assume that the Fatal()
operator does not return.

Fix this by aborting the AML bytecode execution in such a case
by returning AE_ERROR. Also turn ACPI_SIGNAL_FATAL_INFO into a
local variable because of its small size (12 bytes) and to ensure
that AcpiOsSignal() always receives valid information about the
fatal ACPI BIOS error.

Signed-off-by: Armin Wolf <W_Armin@gmx.de>

source/components/executer/exoparg3.c
@@ -153,6 +153,7 @@
 #include "acpi.h"
 #include "accommon.h"
 #include "acinterp.h"
+#include "acoutput.h"
 #include "acparser.h"
 #include "amlcode.h"
 
@@ -201,8 +202,7 @@ AcpiExOpcode_3A_0T_0R (
     ACPI_WALK_STATE         *WalkState)
 {
     ACPI_OPERAND_OBJECT     **Operand = &WalkState->Operands[0];
-    ACPI_SIGNAL_FATAL_INFO  *Fatal;
-    ACPI_STATUS             Status = AE_OK;
+    ACPI_SIGNAL_FATAL_INFO  Fatal;
 
 
     ACPI_FUNCTION_TRACE_STR (ExOpcode_3A_0T_0R,
@@ -213,29 +213,23 @@ AcpiExOpcode_3A_0T_0R (
     {
     case AML_FATAL_OP:          /* Fatal (FatalType  FatalCode  FatalArg) */
 
-        ACPI_DEBUG_PRINT ((ACPI_DB_INFO,
-            "FatalOp: Type %X Code %X Arg %X "
-            "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n",
-            (UINT32) Operand[0]->Integer.Value,
-            (UINT32) Operand[1]->Integer.Value,
-            (UINT32) Operand[2]->Integer.Value));
+        Fatal.Type = (UINT32) Operand[0]->Integer.Value;
+        Fatal.Code = (UINT32) Operand[1]->Integer.Value;
+        Fatal.Argument = (UINT32) Operand[2]->Integer.Value;
 
-        Fatal = ACPI_ALLOCATE (sizeof (ACPI_SIGNAL_FATAL_INFO));
-        if (Fatal)
-        {
-            Fatal->Type = (UINT32) Operand[0]->Integer.Value;
-            Fatal->Code = (UINT32) Operand[1]->Integer.Value;
-            Fatal->Argument = (UINT32) Operand[2]->Integer.Value;
-        }
+        ACPI_BIOS_ERROR ((AE_INFO,
+            "Fatal ACPI BIOS error (Type 0x%X Code 0x%X Arg 0x%X)\n",
+            Fatal.Type, Fatal.Code, Fatal.Argument));
 
         /* Always signal the OS! */
 
-        Status = AcpiOsSignal (ACPI_SIGNAL_FATAL, Fatal);
+        AcpiOsSignal (ACPI_SIGNAL_FATAL, &Fatal);
 
-        /* Might return while OS is shutting down, just continue */
-
-        ACPI_FREE (Fatal);
-        goto Cleanup;
+        /*
+         * Might return while OS is shutting down, so abort the AML execution
+         * by returning an error.
+         */
+        return_ACPI_STATUS (AE_ERROR);
 
     case AML_EXTERNAL_OP:
         /*
@@ -247,22 +241,16 @@ AcpiExOpcode_3A_0T_0R (
          * wrong if an external opcode ever gets here.
          */
         ACPI_ERROR ((AE_INFO, "Executed External Op"));
-        Status = AE_OK;
-        goto Cleanup;
+
+        return_ACPI_STATUS (AE_OK);
 
     default:
 
         ACPI_ERROR ((AE_INFO, "Unknown AML opcode 0x%X",
             WalkState->Opcode));
 
-        Status = AE_AML_BAD_OPCODE;
-        goto Cleanup;
+        return_ACPI_STATUS (AE_AML_BAD_OPCODE);
     }
-
-
-Cleanup:
-
-    return_ACPI_STATUS (Status);
 }
 
 

commit 428b3410c490f258e016d47e517fb70c2c6e190f

Fix asltests using the Fatal() opcode
Some asltests test the behavior of the Fatal() opcode and thus require
that said opcode does not return an error when called.

Introduce a compile-time option called ACPI_CONTINUE_ON_FATAL to
instruct the executor to continue the execution of AML bytecode when
encountering a Fatal() opcode. Also update the asltest to use this
new option.

Fixes: d516c7758ba6 ("Abort AML bytecode execution when executing AML_FATAL_OP")
Signed-off-by: Armin Wolf <W_Armin@gmx.de>

source/components/executer/exoparg3.c
@@ -225,11 +225,18 @@ AcpiExOpcode_3A_0T_0R (
 
         AcpiOsSignal (ACPI_SIGNAL_FATAL, &Fatal);
 
+#ifndef ACPI_CONTINUE_ON_FATAL
         /*
          * Might return while OS is shutting down, so abort the AML execution
          * by returning an error.
          */
         return_ACPI_STATUS (AE_ERROR);
+#else
+	/*
+	 * The alstests require that the Fatal() opcode does not return an error.
+	 */
+	return_ACPI_STATUS (AE_OK);
+#endif
 
     case AML_EXTERNAL_OP:
         /*

tests/aslts.sh
@@ -88,9 +88,10 @@ build_acpi_tools() {
 	if [ "x$REBUILD_TOOLS" = "xyes" ]; then
 		jobs=`nproc`
 		make clean
-		make iasl ASLTS=TRUE -j$jobs
-		make acpibin ASLTS=TRUE -j$jobs
-		make acpiexec ASLTS=TRUE -j$jobs
+		# We need to disable the Fatal() opcode in order to test it
+		make iasl ASLTS=TRUE OPT_CFLAGS="-DACPI_CONTINUE_ON_FATAL" -j$jobs
+		make acpibin ASLTS=TRUE OPT_CFLAGS="-DACPI_CONTINUE_ON_FATAL" -j$jobs
+		make acpiexec ASLTS=TRUE OPT_CFLAGS="-DACPI_CONTINUE_ON_FATAL" -j$jobs
 	fi
 
 	if [ -d "bin" ] && [ -f "bin/iasl" ]; then

tests/aslts/HOW_TO_INSTALL
@@ -51,7 +51,7 @@ ENVIRONMENT
 
    For test execution, three additional variables are required:
 
-        acpiexec - path to acpiexec utility: (example)
+        acpiexec - path to acpiexec utility: (example, needs to be build with ACPI_CONTINUE_ON_FATAL)
 
             > export acpiexec="c:/acpica/libraries/acpiexec.exe"
 

tests/aslts/HOW_TO_USE
@@ -28,7 +28,7 @@ How to execute the run-time tests
 
             > export ASL="c:/acpica/libraries/iasl.exe"
 
-        acpiexec - path to acpiexec utility: (example)
+        acpiexec - path to acpiexec utility: (example, needs to be build with ACPI_CONTINUE_ON_FATAL)
 
             > export acpiexec="c:/acpica/libraries/acpiexec.exe"
 

tests/aslts/README
@@ -19,7 +19,9 @@ more common functionality.
    The testing is performed in "hardware-independent" mode without any
 access to ACPI subsystem hardware. In this purpose the AcpiExec utility
 is used which includes the entire ACPICA subsystem and allows to execute
-the AML code and thus verify functionality of ACPICA subsystem.
+the AML code and thus verify functionality of ACPICA subsystem. Please keep
+in mind that AcpiExec needs to be build with ACPI_CONTINUE_ON_FATAL being
+defined in order to run tests dealing with the Fatal() operator.
 
    The ASL source code is compiled to AML code and then passed to
 AcpiExec utility. In this relation one more type tests are provided,