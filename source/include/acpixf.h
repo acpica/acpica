232ff3f8ae1a4da11c709f61d9154482cfe8e6df
Saket Dumbre <saket.dumbre@intel.com>
2026-04-08 09:31:35 -0700

    Update version to 20260408
    Signed-off-by: Saket Dumbre <saket.dumbre@intel.com>

--- source/include/acpixf.h
+++ source/include/acpixf.h
@@ -154,7 +154,7 @@
 
 /* Current ACPICA subsystem version in YYYYMMDD format */
 
-#define ACPI_CA_VERSION                 0x20251212 
+#define ACPI_CA_VERSION                 0x20260408 
 
 #include "acconfig.h"
 #include "actypes.h"
