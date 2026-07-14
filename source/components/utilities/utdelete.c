28c9f3e956704e134175c662f3e6260311062b92
Saket Dumbre <saket.dumbre@intel.com>
2026-07-14 21:15:55 +0000

    utilities: guard notify list traversal against invalid pointers

--- source/components/utilities/utdelete.c
+++ source/components/utilities/utdelete.c
@@ -698,7 +698,11 @@ AcpiUtUpdateObjectReference (
                 PrevObject = Object->CommonNotify.NotifyList[i];
                 while (PrevObject)
                 {
-                    if (PrevObject->Common.Type != ACPI_TYPE_LOCAL_NOTIFY)
+                    /* Stop if list entry is not a valid notify descriptor */
+                    if (((ACPI_SIZE) PrevObject < sizeof (ACPI_OPERAND_OBJECT)) ||
+                        (ACPI_GET_DESCRIPTOR_TYPE (PrevObject) !=
+                            ACPI_DESC_TYPE_OPERAND) ||
+                        (PrevObject->Common.Type != ACPI_TYPE_LOCAL_NOTIFY))
                     {
                         break;
                     }
