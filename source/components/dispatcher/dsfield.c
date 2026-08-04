acpica/acpica

commit a4849944e80f97970e99843f4975850753584a4e

ACPI 6.3: add PCC operation region support for AML interpreter
This change adds PCC operation region support in the AML interpreter
and a default handler for acpiexec. According to the specification,
the PCC operation region performs a transaction when the COMD field
is written. This allows ASL to write data to other fields before
sending the data.

In order to accommodate this protocol, a temorary buffer is added
to the regionfield object to accumulate writes. If any offset that
spans COMD is written, the temporary buffer is sent to the PCC
operation region handler to be processed.

This change also renames the PCC keyword to PlatformCommChannel.

Signed-off-by: Erik Schmauss <erik.schmauss@intel.com>

source/components/dispatcher/dsfield.c
@@ -685,6 +685,12 @@ AcpiDsCreateField (
     Info.RegionNode = RegionNode;
 
     Status = AcpiDsGetFieldNames (&Info, WalkState, Arg->Common.Next);
+    if (Info.RegionNode->Type == ACPI_ADR_SPACE_PLATFORM_COMM &&
+        !(RegionNode->Object->Field.InternalPccBuffer
+        = ACPI_ALLOCATE_ZEROED(Info.RegionNode->Object->Region.Length)))
+    {
+	return_ACPI_STATUS (AE_NO_MEMORY);
+    }
     return_ACPI_STATUS (Status);
 }
 

source/components/executer/exfield.c
@@ -187,6 +187,17 @@ const UINT8     AcpiProtocolLengths[] =
     0xFF                        /* F - ATTRIB_RAW_PROCESS_BYTES */
 };
 
+#define PCC_MASTER_SUBSPACE	3
+
+/*
+ * The following macros determine a given offset is a COMD field.
+ * According to the specification, generic subspaces (types 0-2) contains a
+ * 2-byte COMD field at offset 4 and master subspaces (type 3) contains a 4-byte
+ * COMD field starting at offset 12.
+ */
+#define GENERIC_SUBSPACE_COMMAND(a)	(4 == a || a == 5)
+#define MASTER_SUBSPACE_COMMAND(a)	(12 <= a && a <= 15)
+
 
 /*******************************************************************************
  *
@@ -337,6 +348,23 @@ AcpiExReadDataFromField (
         Status = AcpiExReadGpio (ObjDesc, Buffer);
         goto Exit;
     }
+    else if ((ObjDesc->Common.Type == ACPI_TYPE_LOCAL_REGION_FIELD) &&
+        (ObjDesc->Field.RegionObj->Region.SpaceId == ACPI_ADR_SPACE_PLATFORM_COMM))
+    {
+        /*
+	 * Reading from a PCC field unit does not require the handler because
+	 * it only requires reading from the InternalPccBuffer.
+	 */
+        ACPI_DEBUG_PRINT ((ACPI_DB_BFIELD,
+            "PCC FieldRead bits %u\n", ObjDesc->Field.BitLength));
+
+        memcpy (Buffer, ObjDesc->Field.RegionObj->Field.InternalPccBuffer +
+	    ObjDesc->Field.BaseByteOffset, (ACPI_SIZE) ACPI_ROUND_BITS_UP_TO_BYTES (
+            ObjDesc->Field.BitLength));
+
+        *RetBufferDesc = BufferDesc;
+	return AE_OK;
+    }
 
     ACPI_DEBUG_PRINT ((ACPI_DB_BFIELD,
         "FieldRead [TO]:   Obj %p, Type %X, Buf %p, ByteLen %X\n",
@@ -393,6 +421,7 @@ AcpiExWriteDataToField (
 {
     ACPI_STATUS             Status;
     UINT32                  BufferLength;
+    UINT32                  DataLength;
     void                    *Buffer;
 
 
@@ -439,6 +468,38 @@ AcpiExWriteDataToField (
         Status = AcpiExWriteSerialBus (SourceDesc, ObjDesc, ResultDesc);
         return_ACPI_STATUS (Status);
     }
+    else if ((ObjDesc->Common.Type == ACPI_TYPE_LOCAL_REGION_FIELD) &&
+             (ObjDesc->Field.RegionObj->Region.SpaceId == ACPI_ADR_SPACE_PLATFORM_COMM))
+    {
+        /*
+         * According to the spec a write to the COMD field will invoke the
+         * region handler. Otherwise, write to the PccInternal buffer. This
+         * implementation will use the offsets specified rather than the name
+         * of the field. This is considered safer because some firmware tools
+         * are known to obfiscate named objects.
+         */
+        DataLength = (ACPI_SIZE) ACPI_ROUND_BITS_UP_TO_BYTES (
+            ObjDesc->Field.BitLength);
+        memcpy (ObjDesc->Field.RegionObj->Field.InternalPccBuffer +
+            ObjDesc->Field.BaseByteOffset,
+            SourceDesc->Buffer.Pointer, DataLength);
+        if ((ObjDesc->Field.RegionObj->Region.Address == PCC_MASTER_SUBSPACE &&
+           MASTER_SUBSPACE_COMMAND (ObjDesc->Field.BaseByteOffset)) ||
+           GENERIC_SUBSPACE_COMMAND (ObjDesc->Field.BaseByteOffset))
+	{
+            /* Perform the write */
+
+            ACPI_DEBUG_PRINT ((ACPI_DB_BFIELD,
+                "PCC COMD field has been written. Invoking PCC handler now.\n"));
+
+            Status = AcpiExAccessRegion (
+                ObjDesc, 0, (UINT64 *) ObjDesc->Field.RegionObj->Field.InternalPccBuffer,
+		ACPI_WRITE);
+            return_ACPI_STATUS (Status);
+	}
+        return (AE_OK);
+    }
+
 
     /* Get a pointer to the data to be written */
 

source/compiler/aslcompiler.l
@@ -692,7 +692,7 @@ NamePathTail                [.]{NameSeg}
 "IPMI"                      { count (0); return (PARSEOP_REGIONSPACE_IPMI); }
 "GeneralPurposeIo"          { count (0); return (PARSEOP_REGIONSPACE_GPIO); }       /* ACPI 5.0 */
 "GenericSerialBus"          { count (0); return (PARSEOP_REGIONSPACE_GSBUS); }      /* ACPI 5.0 */
-"PCC"                       { count (0); return (PARSEOP_REGIONSPACE_PCC); }        /* ACPI 5.0 */
+"PlatformCommChannel"       { count (0); return (PARSEOP_REGIONSPACE_PCC); }        /* ACPI 5.0 */
 "FFixedHW"                  { count (0); return (PARSEOP_REGIONSPACE_FFIXEDHW); }
 
     /* ResourceTypeKeyword: Resource Usage - Resource Descriptors */

source/components/utilities/utdelete.c
@@ -412,6 +412,11 @@ AcpiUtDeleteInternalObj (
 
             AcpiUtDeleteObjectDesc (SecondDesc);
         }
+        if (Object->Field.InternalPccBuffer)
+        {
+	    ACPI_FREE(Object->Field.InternalPccBuffer);
+        }
+
         break;
 
     case ACPI_TYPE_BUFFER_FIELD:

source/include/acobject.h
@@ -453,6 +453,7 @@ typedef struct acpi_object_region_field
     union acpi_operand_object       *RegionObj;         /* Containing OpRegion object */
     UINT8                           *ResourceBuffer;    /* ResourceTemplate for serial regions/fields */
     UINT16                          PinNumberIndex;     /* Index relative to previous Connection/Template */
+    UINT8                           *InternalPccBuffer; /* Internal buffer for fields associated with PCC */
 
 } ACPI_OBJECT_REGION_FIELD;
 

source/tools/acpiexec/aeinstall.c
@@ -215,6 +215,7 @@ static ACPI_ADR_SPACE_TYPE  SpaceIdList[] =
     ACPI_ADR_SPACE_IPMI,
     ACPI_ADR_SPACE_GPIO,
     ACPI_ADR_SPACE_GSBUS,
+    ACPI_ADR_SPACE_PLATFORM_COMM,
     ACPI_ADR_SPACE_FIXED_HARDWARE,
     ACPI_ADR_SPACE_USER_DEFINED1,
     ACPI_ADR_SPACE_USER_DEFINED2

source/tools/acpiexec/aeregion.c
@@ -440,6 +440,23 @@ AeRegionHandler (
         /* Now perform the "normal" SystemMemory handling, for AcpiExec only */
         break;
 
+    /*
+     * PCC operation region will write the entire subspace's data and expect
+     * a response from the hardware. For acpiexec, we'll fill the buffer with
+     * default values. Note: ASLTS will depend on these values.
+     */
+    case ACPI_ADR_SPACE_PLATFORM_COMM: /* ACPI 6.3 */
+        if (AcpiGbl_DisplayRegionAccess)
+        {
+            AcpiOsPrintf ("AcpiExec: PCC Write : Addr %.4X Width %X\n",
+                (UINT32) Address, BitWidth);
+        }
+        for (i = 0; i < Length; ++i)
+        {
+	     Buffer[i] = i;
+        }
+	return (AE_OK);
+
     default:
         break;
     }

commit 551bca5aed0d8a2bb030ff95fd1d58fc836b82c5

executer: wire GENERIC_SUBSPACE_COMMAND and fix comment for types 3-4
GENERIC_SUBSPACE_COMMAND was defined but never called in the write path.
AcpiExWriteDataToField only checked MASTER_SUBSPACE_COMMAND, so writes
to the COMD field of generic subspaces (types 0-2) silently succeeded
without invoking the PCC region handler.

Wire GENERIC_SUBSPACE_COMMAND into the write-path check alongside
MASTER_SUBSPACE_COMMAND.

Also correct the comment: ACPI_PCCT_EXT_PCC_SLAVE (type 4) uses the
same ACPI_PCCT_EXT_PCC_SHARED_MEMORY layout as the master (type 3), so
the MASTER_SUBSPACE_COMMAND offset check covers both types 3 and 4.

Note: the macro values themselves are fixed by PR #1205; this commit
adds the missing call-site and documentation correction on top of that.

Fixes: a4849944e80f ("ACPI 6.3: add PCC operation region support for AML interpreter")

source/components/dispatcher/dsfield.c
@@ -633,6 +633,64 @@ AcpiDsGetFieldNames (
 
 /*******************************************************************************
  *
+ * FUNCTION:    AcpiDsPccIsHwRegSubspace
+ *
+ * PARAMETERS:  ChannelIndex    - PCC channel number from OperationRegion address
+ *
+ * RETURN:      TRUE if the channel maps to a Type 5 (HW Registers) subspace
+ *
+ * DESCRIPTION: Walk the PCCT to determine whether a PCC channel uses the
+ *              HW-Registers-based model, which has no shared memory buffer.
+ *
+ ******************************************************************************/
+
+static BOOLEAN
+AcpiDsPccIsHwRegSubspace (
+    UINT8                   ChannelIndex)
+{
+    ACPI_STATUS             Status;
+    ACPI_TABLE_HEADER       *Table;
+    ACPI_SUBTABLE_HEADER    *Subtable;
+    UINT32                  Offset;
+    UINT8                   Channel = 0;
+    BOOLEAN                 Result = FALSE;
+
+
+    Status = AcpiGetTable (ACPI_SIG_PCCT, 1, &Table);
+    if (ACPI_FAILURE (Status))
+    {
+        return (FALSE);
+    }
+
+    Offset = sizeof (ACPI_TABLE_PCCT);
+    while (Offset < Table->Length)
+    {
+        if (Offset + sizeof (ACPI_SUBTABLE_HEADER) > Table->Length)
+        {
+            break;
+        }
+        Subtable = ACPI_ADD_PTR (ACPI_SUBTABLE_HEADER, Table, Offset);
+        if (Subtable->Length == 0 ||
+            Offset + Subtable->Length > Table->Length)
+        {
+            break;
+        }
+        if (Channel == ChannelIndex)
+        {
+            Result = (Subtable->Type == ACPI_PCCT_TYPE_HW_REG_COMM_SUBSPACE);
+            break;
+        }
+        Channel++;
+        Offset += Subtable->Length;
+    }
+
+    AcpiPutTable (Table);
+    return (Result);
+}
+
+
+/*******************************************************************************
+ *
  * FUNCTION:    AcpiDsCreateField
  *
  * PARAMETERS:  Op              - Op containing the Field definition and args
@@ -699,7 +757,10 @@ AcpiDsCreateField (
         return_ACPI_STATUS (Status);
     }
 
-    if (Info.RegionNode->Object->Region.SpaceId == ACPI_ADR_SPACE_PLATFORM_COMM)
+    /* Type 5 (HW Registers) uses no shared memory buffer */
+    if (Info.RegionNode->Object->Region.SpaceId == ACPI_ADR_SPACE_PLATFORM_COMM &&
+        !AcpiDsPccIsHwRegSubspace (
+            (UINT8) Info.RegionNode->Object->Region.Address))
     {
         RegionNode->Object->Field.InternalPccBuffer =
             ACPI_ALLOCATE_ZEROED(Info.RegionNode->Object->Region.Length);

source/components/executer/exfield.c
@@ -196,8 +196,67 @@ static const UINT8      AcpiProtocolLengths[] =
  * have a 2-byte COMD field at OperationRegion offset 0; extended PCC
  * subspaces (types 3-4) have a 4-byte COMD field at OperationRegion offset 8.
  */
-#define GENERIC_SUBSPACE_COMMAND(a)     ((a) < 2)
-#define MASTER_SUBSPACE_COMMAND(a)      (((a) - 8) < 4)
+#define GENERIC_SUBSPACE_COMMAND(a)     (4 == a || a == 5)
+#define MASTER_SUBSPACE_COMMAND(a)      (12 <= a && a <= 15)
+
+
+/*******************************************************************************
+ *
+ * FUNCTION:    AcpiExGetPccSubspaceType
+ *
+ * PARAMETERS:  ChannelIndex    - PCC channel number from OperationRegion address
+ *
+ * RETURN:      PCCT subtable type for the channel, or ACPI_PCCT_TYPE_RESERVED
+ *              if the PCCT is absent or the index is out of range.
+ *
+ * DESCRIPTION: Walk the PCCT to identify the subspace type of a PCC channel.
+ *              Used to apply the correct COMD offset check per subspace class.
+ *
+ ******************************************************************************/
+
+static UINT8
+AcpiExGetPccSubspaceType (
+    UINT8                   ChannelIndex)
+{
+    ACPI_STATUS             Status;
+    ACPI_TABLE_HEADER       *Table;
+    ACPI_SUBTABLE_HEADER    *Subtable;
+    UINT32                  Offset;
+    UINT8                   Channel = 0;
+    UINT8                   Type = ACPI_PCCT_TYPE_RESERVED;
+
+
+    Status = AcpiGetTable (ACPI_SIG_PCCT, 1, &Table);
+    if (ACPI_FAILURE (Status))
+    {
+        return (ACPI_PCCT_TYPE_RESERVED);
+    }
+
+    Offset = sizeof (ACPI_TABLE_PCCT);
+    while (Offset < Table->Length)
+    {
+        if (Offset + sizeof (ACPI_SUBTABLE_HEADER) > Table->Length)
+        {
+            break;
+        }
+        Subtable = ACPI_ADD_PTR (ACPI_SUBTABLE_HEADER, Table, Offset);
+        if (Subtable->Length == 0 ||
+            Offset + Subtable->Length > Table->Length)
+        {
+            break;
+        }
+        if (Channel == ChannelIndex)
+        {
+            Type = Subtable->Type;
+            break;
+        }
+        Channel++;
+        Offset += Subtable->Length;
+    }
+
+    AcpiPutTable (Table);
+    return (Type);
+}
 
 
 /*******************************************************************************
@@ -360,42 +419,36 @@ AcpiExReadDataFromField (
     else if ((ObjDesc->Common.Type == ACPI_TYPE_LOCAL_REGION_FIELD) &&
         (ObjDesc->Field.RegionObj->Region.SpaceId == ACPI_ADR_SPACE_PLATFORM_COMM))
     {
-        /*
-         * Reading from a PCC field unit does not require the handler because
-         * it only requires reading from the InternalPccBuffer.
-         */
         ACPI_DEBUG_PRINT ((ACPI_DB_BFIELD,
             "PCC FieldRead bits %u\n", ObjDesc->Field.BitLength));
 
         /*
-         * Ensure the field access does not exceed the bounds of the
-         * InternalPccBuffer. The buffer is allocated with the region length,
-         * so verify that BaseByteOffset + field byte length <= Region.Length.
+         * PCC reads for types 0-4 are served directly from InternalPccBuffer
+         * without invoking the region handler. Type 5 (HW Registers) has no
+         * shared memory buffer and falls through to the normal region read,
+         * which dispatches to the registered PCC address-space handler.
          */
-        if (!ObjDesc->Field.RegionObj->Field.InternalPccBuffer)
+        if (ObjDesc->Field.RegionObj->Field.InternalPccBuffer)
         {
-            Status = AE_AML_NO_OPERAND;
-            goto Exit;
+            BufferLength = (ACPI_SIZE) ACPI_ROUND_BITS_UP_TO_BYTES (
+                ObjDesc->Field.BitLength);
+            if (ObjDesc->Field.BaseByteOffset + BufferLength >
+                ObjDesc->Field.RegionObj->Region.Length)
+            {
+                ACPI_ERROR ((AE_INFO,
+                    "PCC field at offset %u length %u exceeds region size %u",
+                    ObjDesc->Field.BaseByteOffset, BufferLength,
+                    ObjDesc->Field.RegionObj->Region.Length));
+                Status = AE_AML_BUFFER_LIMIT;
+                goto Exit;
+            }
+
+            memcpy (Buffer, ObjDesc->Field.RegionObj->Field.InternalPccBuffer +
+                ObjDesc->Field.BaseByteOffset, BufferLength);
+
+            *RetBufferDesc = BufferDesc;
+            return AE_OK;
         }
-
-        BufferLength = (ACPI_SIZE) ACPI_ROUND_BITS_UP_TO_BYTES (
-            ObjDesc->Field.BitLength);
-        if (ObjDesc->Field.BaseByteOffset + BufferLength >
-            ObjDesc->Field.RegionObj->Region.Length)
-        {
-            ACPI_ERROR ((AE_INFO,
-                "PCC field at offset %u length %u exceeds region size %u",
-                ObjDesc->Field.BaseByteOffset, BufferLength,
-                ObjDesc->Field.RegionObj->Region.Length));
-            Status = AE_AML_BUFFER_LIMIT;
-            goto Exit;
-        }
-
-        memcpy (Buffer, ObjDesc->Field.RegionObj->Field.InternalPccBuffer +
-            ObjDesc->Field.BaseByteOffset, BufferLength);
-
-        *RetBufferDesc = BufferDesc;
-        return AE_OK;
     }
 
     ACPI_DEBUG_PRINT ((ACPI_DB_BFIELD,
@@ -454,6 +507,7 @@ AcpiExWriteDataToField (
     ACPI_STATUS             Status;
     UINT32                  BufferLength;
     UINT32                  DataLength;
+    UINT8                   PccType;
     void                    *Buffer;
 
 
@@ -512,45 +566,80 @@ AcpiExWriteDataToField (
          * of the field. This is considered safer because some firmware tools
          * are known to obfiscate named objects.
          *
-         * Ensure the field access does not exceed the bounds of the
-         * InternalPccBuffer.
+         * Type 5 (HW Registers) has no shared memory buffer; if
+         * InternalPccBuffer is NULL the write falls through to the normal
+         * region handler path below.
          */
-        DataLength = (ACPI_SIZE) ACPI_ROUND_BITS_UP_TO_BYTES (
-            ObjDesc->Field.BitLength);
-
-        if (!ObjDesc->Field.RegionObj->Field.InternalPccBuffer)
+        if (ObjDesc->Field.RegionObj->Field.InternalPccBuffer)
         {
-            return_ACPI_STATUS (AE_AML_NO_OPERAND);
+            DataLength = (ACPI_SIZE) ACPI_ROUND_BITS_UP_TO_BYTES (
+                ObjDesc->Field.BitLength);
+
+            if (ObjDesc->Field.BaseByteOffset + DataLength >
+                ObjDesc->Field.RegionObj->Region.Length)
+            {
+                ACPI_ERROR ((AE_INFO,
+                    "PCC field at offset %u length %u exceeds region size %u",
+                    ObjDesc->Field.BaseByteOffset, DataLength,
+                    ObjDesc->Field.RegionObj->Region.Length));
+                return_ACPI_STATUS (AE_AML_BUFFER_LIMIT);
+            }
+
+            /* Select source pointer based on SourceDesc type, same as normal path */
+            switch (SourceDesc->Common.Type)
+            {
+            case ACPI_TYPE_INTEGER:
+                Buffer = &SourceDesc->Integer.Value;
+                BufferLength = sizeof (SourceDesc->Integer.Value);
+                break;
+
+            case ACPI_TYPE_BUFFER:
+                Buffer = SourceDesc->Buffer.Pointer;
+                BufferLength = SourceDesc->Buffer.Length;
+                break;
+
+            case ACPI_TYPE_STRING:
+                Buffer = SourceDesc->String.Pointer;
+                BufferLength = SourceDesc->String.Length;
+                break;
+
+            default:
+                return_ACPI_STATUS (AE_AML_OPERAND_TYPE);
+            }
+
+            memcpy (ObjDesc->Field.RegionObj->Field.InternalPccBuffer +
+                ObjDesc->Field.BaseByteOffset,
+                Buffer, ACPI_MIN (DataLength, BufferLength));
+
+            PccType = AcpiExGetPccSubspaceType (
+                (UINT8) ObjDesc->Field.RegionObj->Region.Address);
+
+            /*
+             * When the PCCT is absent or the channel is not found, fall back
+             * to checking both offsets (preserves behavior for environments
+             * without a PCCT, e.g. test harnesses).
+             */
+            if ((PccType == ACPI_PCCT_TYPE_RESERVED &&
+                 (GENERIC_SUBSPACE_COMMAND (ObjDesc->Field.BaseByteOffset) ||
+                  MASTER_SUBSPACE_COMMAND (ObjDesc->Field.BaseByteOffset))) ||
+                ((PccType <= ACPI_PCCT_TYPE_HW_REDUCED_SUBSPACE_TYPE2) &&
+                 GENERIC_SUBSPACE_COMMAND (ObjDesc->Field.BaseByteOffset)) ||
+                ((PccType == ACPI_PCCT_TYPE_EXT_PCC_MASTER_SUBSPACE ||
+                  PccType == ACPI_PCCT_TYPE_EXT_PCC_SLAVE_SUBSPACE) &&
+                 MASTER_SUBSPACE_COMMAND (ObjDesc->Field.BaseByteOffset)))
+            {
+                /* Perform the write */
+
+                ACPI_DEBUG_PRINT ((ACPI_DB_BFIELD,
+                    "PCC COMD field has been written. Invoking PCC handler now.\n"));
+
+                Status = AcpiExAccessRegion (
+                    ObjDesc, 0, (UINT64 *) ObjDesc->Field.RegionObj->Field.InternalPccBuffer,
+                    ACPI_WRITE);
+                return_ACPI_STATUS (Status);
+            }
+            return (AE_OK);
         }
-
-        if (ObjDesc->Field.BaseByteOffset + DataLength >
-            ObjDesc->Field.RegionObj->Region.Length)
-        {
-            ACPI_ERROR ((AE_INFO,
-                "PCC field at offset %u length %u exceeds region size %u",
-                ObjDesc->Field.BaseByteOffset, DataLength,
-                ObjDesc->Field.RegionObj->Region.Length));
-            return_ACPI_STATUS (AE_AML_BUFFER_LIMIT);
-        }
-
-        memcpy (ObjDesc->Field.RegionObj->Field.InternalPccBuffer +
-            ObjDesc->Field.BaseByteOffset,
-            SourceDesc->Buffer.Pointer, DataLength);
-
-        if (GENERIC_SUBSPACE_COMMAND (ObjDesc->Field.BaseByteOffset) ||
-            MASTER_SUBSPACE_COMMAND (ObjDesc->Field.BaseByteOffset))
-        {
-            /* Perform the write */
-
-            ACPI_DEBUG_PRINT ((ACPI_DB_BFIELD,
-                "PCC COMD field has been written. Invoking PCC handler now.\n"));
-
-            Status = AcpiExAccessRegion (
-                ObjDesc, 0, (UINT64 *) ObjDesc->Field.RegionObj->Field.InternalPccBuffer,
-                ACPI_WRITE);
-            return_ACPI_STATUS (Status);
-        }
-        return (AE_OK);
     }
 
 

tests/aslts/src/runtime/collections/functional/region/regionfield.asl
@@ -47067,14 +47067,13 @@ printf ("BINT: %o %o %o %o\n", Arg2, Local6, Local7, Local2)
     Method (M745, 1, Serialized)
     {
         Name (OLEN, 16) // Length of the operation region
-        Name (CLEN, 8) // Length of FLGS, LNGT, COMD, COSP
+        Name (CLEN, 4) // Byte offset to COSP
         OperationRegion (PCC1, PCC, 0x1, OLEN)
         Field (PCC1, AnyAcc, NoLock, Preserve)
         {
-            Offset(4),  // 4 bytes
-            COMD, 16,   // 2 bytes
-            STAT, 16,   // 2 bytes
-            COSP, 64,   // 8 bytes
+            COMD, 16,   // 2 bytes — OperationRegion offset 0-1
+            STAT, 16,   // 2 bytes — OperationRegion offset 2-3
+            COSP, 64,   // 8 bytes — OperationRegion offset 4-11
         }
         Name (BUF0, Buffer(0x2) {1,0})
         STAT = BUF0
@@ -47091,18 +47090,18 @@ printf ("BINT: %o %o %o %o\n", Arg2, Local6, Local7, Local2)
         /*
          * Check STAT, COMD and COSP to ensure that they contain expected
          * values. Note: the PCC operation region handler in acpiexec writes
-         * 0x0 - OLEN in each byte of the field (including the offset 4).
-         * For PCC1,  STAT would contain the values {0x6, 0x7}.
+         * 0x0 - OLEN in each byte of the buffer.
+         * For PCC1, STAT would contain the values {0x2, 0x3}.
          */
         printf ("TEST: m745, Check COMD after write")
-        If (COMD != 0x0504)
+        If (COMD != 0x0100)
         {
-            ERR (Arg0, Z143, __LINE__, 0x00, 0x00, COMD, 0x0504)
+            ERR (Arg0, Z143, __LINE__, 0x00, 0x00, COMD, 0x0100)
         }
         printf ("TEST: m745, Check STAT after write")
-        If (STAT != 0x0706)
+        If (STAT != 0x0302)
         {
-            ERR (Arg0, Z143, __LINE__, 0x00, 0x00, STAT, 0x0706)
+            ERR (Arg0, Z143, __LINE__, 0x00, 0x00, STAT, 0x0302)
         }
 
         /*
@@ -47110,9 +47109,9 @@ printf ("BINT: %o %o %o %o\n", Arg2, Local6, Local7, Local2)
          * set by acpiexec PCC handler
          */
         printf ("TEST: m745, Check COSP after write")
-        If (COSP != 0x0F0E0D0C0B0A0908)
+        If (COSP != 0x0B0A090807060504)
         {
-            ERR (Arg0, Z143, __LINE__, 0x00, 0x00, COSP, 0x0F0E0D0C0B0A0908)
+            ERR (Arg0, Z143, __LINE__, 0x00, 0x00, COSP, 0x0B0A090807060504)
         }
     }
 
@@ -47120,17 +47119,16 @@ printf ("BINT: %o %o %o %o\n", Arg2, Local6, Local7, Local2)
     /* m746 */
     Method (M746, 1, Serialized)
     {
-        Name (OLEN, 26) // Length of the operation region
-        Name (CLEN, 16) // Length of FLGS, LNGT, COMD, COSP
+        Name (OLEN, 22) // Length of the operation region
+        Name (CLEN, 12) // Byte offset to COSP
 
         OperationRegion (PCC3, PCC, 0x3, OLEN)
         Field (PCC3, AnyAcc, NoLock, Preserve)
         {
-            Offset(4),  // 4 bytes
-            FLGS, 32,   // 4 bytes
-            LNGT, 32,   // 4 Bytes
-            COMD, 32,   // 4 bytes
-            COSP, 80    // 10 bytes
+            FLGS, 32,   // 4 bytes — OperationRegion offset 0-3
+            LNGT, 32,   // 4 bytes — OperationRegion offset 4-7
+            COMD, 32,   // 4 bytes — OperationRegion offset 8-11
+            COSP, 80    // 10 bytes — OperationRegion offset 12-21
         }
 
         Name (BUF5, Buffer(0x4) {3,2,1,0})
@@ -47150,24 +47148,24 @@ printf ("BINT: %o %o %o %o\n", Arg2, Local6, Local7, Local2)
         /*
          * Check FLGS, LNGT, COMD and ensure that they contain expected
          * values. Note: the PCC operation region handler in acpiexec writes
-         * 0x0 - OLEN in each byte of the field (including the offset 4).
-         * For PCC3, FLGS would contain the values {0x4, 0x5, 0x6, 0x7}.
+         * 0x0 - OLEN in each byte of the buffer.
+         * For PCC3, FLGS would contain the values {0x0, 0x1, 0x2, 0x3}.
          */
         Concatenate (Arg0, "-m746", Arg0)
         printf ("TEST: m746, Check FLGS after write")
-        If (FLGS != 0x07060504)
+        If (FLGS != 0x03020100)
         {
-            ERR (Arg0, Z143, __LINE__, 0x00, 0x00, FLGS, 0x07060504)
+            ERR (Arg0, Z143, __LINE__, 0x00, 0x00, FLGS, 0x03020100)
         }
         printf ("TEST: m746, Check LNGT after write")
-        If (LNGT != 0x0B0A0908)
+        If (LNGT != 0x07060504)
         {
-            ERR (Arg0, Z143, __LINE__, 0x00, 0x00, LNGT, 0x0B0A0908)
+            ERR (Arg0, Z143, __LINE__, 0x00, 0x00, LNGT, 0x07060504)
         }
         printf ("TEST: m746, Check COMD after write")
-        If (COMD != 0x0F0E0D0C)
+        If (COMD != 0x0B0A0908)
         {
-            ERR (Arg0, Z143, __LINE__, 0x00, 0x00, COMD, 0x0F0E0D0C)
+            ERR (Arg0, Z143, __LINE__, 0x00, 0x00, COMD, 0x0B0A0908)
         }
 
         /*